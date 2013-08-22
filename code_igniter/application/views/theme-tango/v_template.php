2e08470539ab87d4fb44fa19f3d49f9da4e489a3
Mark Unwin <marku@opmantek.com>
2013-08-22 10:46:54 +1000

    code cleanup.

--- code_igniter/application/views/theme-tango/v_template.php
+++ code_igniter/application/views/theme-tango/v_template.php
@@ -3,7 +3,8 @@ include "v_lang.php";
 $file_path = base_url() . 'theme-' . $user_theme . '/' . $user_theme . '-files/';
 if (!isset($sortcolumn)) { $sortcolumn = '0';}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
