8ed574570dcb485ee193df3fedaddc5fba7b5110
Mark Unwin <marku@opmantek.com>
2013-09-27 13:02:42 +1000

    Revised session timeout to only occur when user closes browser. This way if a user is in OAE but has not looked at an OAC page, they will not need to log back in if the session timeout was set.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -251,8 +251,9 @@ $config['encryption_key'] = "openaudit";
 */
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
+$config['sess_expiration']		= 0;
 $config['sess_encrypt_cookie']	= TRUE;
-$config['sess_use_database']	= FALSE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'oa_user_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
