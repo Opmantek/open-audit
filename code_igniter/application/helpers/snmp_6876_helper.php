a4416766b692a2efe436435ed8c609b6f9a78f4b
Mark Unwin <marku@opmantek.com>
2014-01-29 15:33:28 +1000

    SNMP cleanup of VMware.

--- code_igniter/application/helpers/snmp_6876_helper.php
+++ code_igniter/application/helpers/snmp_6876_helper.php
@@ -41,15 +41,11 @@ if (!function_exists('get_oid_details')) {
 		$version = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.1.2.0" ));
 		$details->model = $model . " (" . $version . ")";
 		$details->model =str_replace("\"", "", $details->model);
-		$details->man_model = $details->model;
 		$details->os_group = "VMware";
-		$details->man_os_group = "VMware";
 		$details->os_family = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.1.1.0" ));
 		$details->os_family = str_replace("\"", "", $details->os_family);
-		$details->man_os_family = $details->os_family;
 		$details->os_name = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.2.1.1.1.0" ));
 		$details->os_name =str_replace("\"", "", $details->os_name);
-		$details->man_os_name = $details->os_name;
 		$details->type = 'computer';
 		$details->device_type = 'computer';
 		$details->man_class = 'hypervisor';
