cd6a33d5eeb5bb0b65463a96c9fc8fe2b2142aa1
Mark Unwin <marku@opmantek.com>
2014-06-05 10:28:53 +1000

    OMK-473 Added RSS feed to bottom of main page. Can be disabled or altered in standard config.

--- code_igniter/application/views/theme-tango/v_main.php
+++ code_igniter/application/views/theme-tango/v_main.php
@@ -179,3 +179,91 @@ function dynamic_search( group )
 	return false;
 }
 </script>
+
+<?php if ($config->rss_enable == 'y') { ?>
+<style>
+li a {text-decoration: underline; color: #729FCF;}
+</style>
+
+<table cellspacing="1" class="tablesorter">
+	<tr>
+		<td><ul class="fade" name="newsfeed" id="newsfeed" style="list-style-type: none;"><li>&nbsp;&nbsp;&nbsp;<img src="<?php echo $image_path;?>16_nmis.png" />&nbsp;&nbsp;Open-AudIT Community: from the <a href="https://community.opmantek.com/display/OA/Home">Open-AudIT wiki</a>.</li></ul></td>
+	</tr>
+</table>
+
+<style>
+li a {text-decoration: none;}
+</style>
+
+
+<script>
+    (function (e) {
+        e.fn.inewsticker = function (t) {
+            var n = {
+                speed: 200,
+                effect: "fade",
+                dir: "ltr",
+                font_size: null,
+                color: null,
+                font_family: null,
+                delay_after: 3e3
+            };
+            e.extend(n, t);
+            var r = e(this);
+            var i = r.children();
+            i.not(":first").hide();
+            r.css("direction", t.dir);
+            r.css("font-size", t.font_size);
+            r.css("color", t.color);
+            r.css("font-family", t.font_family);
+            setInterval(function () {
+                var e = r.children();
+                e.not(":first").hide();
+                var n = e.eq(0);
+                var i = e.eq(1);
+                if (t.effect == "fade") {
+                    n.fadeOut(function () {
+                        i.fadeIn();
+                        n.remove().appendTo(r)
+                    })
+                }
+            }, t.speed);
+
+        }
+    })(jQuery)
+
+var rssurl = "<?php echo $config->rss_url; ?>"
+var rssfeed = $.get(rssurl, function(data) {
+    var $xml = $(data);
+    $xml.find("entry").each(function() {
+        var $this = $(this),
+            item = {
+                title: $this.find("title").text(),
+                link: $this.find("link").text(),
+                description: $this.find("description").text(),
+                pubDate: $this.find("pubDate").text(),
+                author: $this.find("author").text()
+            }
+            var li = document.createElement("li");
+            var updateDate = new Date($this.find("updated").text());
+            var updatedDate = updateDate.getFullYear() + "/" + updateDate.getMonth() + "/" + updateDate.getDate(); 
+            li.style.display="none";
+            li.innerHTML = "&nbsp;&nbsp;&nbsp;<img src=\"<?php echo $image_path;?>16_nmis.png\" />&nbsp;&nbsp;Open-AudIT Community: <a href='" + $this.find("link").attr("href") + "'>" + $this.find("title").text() + "</a> by " + $this.find("author").text() + " on " + updatedDate + ".";
+            document.getElementById("newsfeed").appendChild(li);
+    });
+});
+
+$(document).ready(function() {
+    $('.fade').inewsticker({
+        speed       : 5000,
+        effect      : 'fade',
+        dir         : 'ltr',
+        font_size   : 13,
+        color       : '#000',
+        font_family : 'arial',
+        delay_after : 1000      
+    });
+});
+</script>
+
+<?php } ?>
