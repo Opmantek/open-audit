97e8cad92367863f1742faaef05388c2f2b2c668
Mark Unwin <marku@opmantek.com>
2014-10-28 09:51:50 +1000

    Version increase in system controller.

--- code_igniter/application/controllers/system.php
+++ code_igniter/application/controllers/system.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * The is the admin controller for Open-AudIT
+ * The is the system controller for Open-AudIT
  * 
  * @category  Controller
  * @package   Open-AudIT
  * @author    Mark Unwin <marku@opmantek.com>
  * @copyright 2014 Opmantek
  * @license   http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
- * @version   1.2
+ * @version   1.5
  * @link      http://www.open-audit.org
  */
 
