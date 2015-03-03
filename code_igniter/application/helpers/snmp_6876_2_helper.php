4dcecea6cd5a5eef275afd5959e35a6e7d2207af
Mark Unwin <marku@opmantek.com>
2015-03-04 09:03:26 +1000

    PSR2 snmp_6876_2_helper - vmware guests.

--- code_igniter/application/helpers/snmp_6876_2_helper.php
+++ code_igniter/application/helpers/snmp_6876_2_helper.php
@@ -1,4 +1,6 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php  if (!defined('BASEPATH')) {
+     exit('No direct script access allowed');
+ }
 #
 #  Copyright 2003-2015 Opmantek Limited (www.opmantek.com)
 #
@@ -7,7 +9,7 @@
 #  This file is part of Open-AudIT.
 #
 #  Open-AudIT is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as published 
+#  it under the terms of the GNU Affero General Public License as published
 #  by the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
@@ -25,7 +27,7 @@
 #
 # *****************************************************************************
 
-/**
+/*
  * @package Open-AudIT
  * @author Mark Unwin <marku@opmantek.com>
  * @version 1.5.6
@@ -46,35 +48,35 @@
 
 $guest_name = @snmp2_real_walk($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.2");
 if (is_array($guest_name) and count($guest_name) > 0) {
-	$guests = array();
-	foreach ($guest_name as $key => $value) {
-		$guest = new stdClass();
-		$guest->name = snmp_clean($value);
-		$guest->vm_id = str_replace('.1.3.6.1.4.1.6876.2.1.1.2.', '', $key);
-		$guest->net = array();
-		$guest_mac = snmp2_real_walk($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.4.1.7." . $guest->vm_id);
-		if (isset($guest_mac) and is_array($guest_mac) and count($guest_mac) > 0) {
-			if (isset($guest_mac[0]) and $guest_mac[0] != '') {
-				foreach ($guest_mac[0] as $k => $v) {
-					if ($v != '') {
-						$mk = str_replace(".1.3.6.1.4.1.6876.2.4.1.7." . $guest['id'] . '.', '', $k);
-						$guest->net[$mk]['mac'] = snmp_clean($v);
-						$guest->net[$mk]['mac'] = strtolower(str_replace(" ", ":", $guest['net'][$mk]['mac']));
-						$guest->net[$mk]['desc'] = snmp_clean(snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.4.1.4.".$guest->vm_id.".".$mk));
-					}
-				}
-			}
-		}
-		$guest->uuid = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.10." . $guest->vm_id));
-		$guest->memory = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.5." . $guest->vm_id));
-		$guest->cpu = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.9." . $guest->vm_id));
-		$guest->status = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.6." . $guest->vm_id));
-		$guest->config_file = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.3." . $guest->vm_id));
-		$guest->vm_group = '';
-		$guests[] = $guest;
-		unset($guest);
-		unset($guest_mac);
-	}
+    $details->class = 'hypervisor';
+    $guests = array();
+    foreach ($guest_name as $key => $value) {
+        $guest = new stdClass();
+        $guest->name = snmp_clean($value);
+        $guest->vm_id = str_replace('.1.3.6.1.4.1.6876.2.1.1.2.', '', $key);
+        $guest->net = array();
+        $guest_mac = snmp2_real_walk($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.4.1.7.".$guest->vm_id);
+        if (isset($guest_mac) and is_array($guest_mac) and count($guest_mac) > 0) {
+            if (isset($guest_mac[0]) and $guest_mac[0] != '') {
+                foreach ($guest_mac[0] as $k => $v) {
+                    if ($v != '') {
+                        $mk = str_replace(".1.3.6.1.4.1.6876.2.4.1.7.".$guest['id'].'.', '', $k);
+                        $guest->net[$mk]['mac'] = snmp_clean($v);
+                        $guest->net[$mk]['mac'] = strtolower(str_replace(" ", ":", $guest['net'][$mk]['mac']));
+                        $guest->net[$mk]['desc'] = snmp_clean(snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.4.1.4.".$guest->vm_id.".".$mk));
+                    }
+                }
+            }
+        }
+        $guest->uuid = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.10.".$guest->vm_id));
+        $guest->memory = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.5.".$guest->vm_id));
+        $guest->cpu = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.9.".$guest->vm_id));
+        $guest->status = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.6.".$guest->vm_id));
+        $guest->config_file = snmp_clean(@snmp2_get($details->man_ip_address, $details->snmp_community, "1.3.6.1.4.1.6876.2.1.1.3.".$guest->vm_id));
+        $guest->vm_group = '';
+        $guests[] = $guest;
+        unset($guest);
+        unset($guest_mac);
+    }
 }
 unset($guest_name);
-	
