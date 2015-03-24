9da553c652fa39912fbe861e944b34d7a0aa9ca9
Mark Unwin <marku@opmantek.com>
2015-03-25 09:00:23 +1000

    OA-224 followup. Apparently it's not enough to use @ in CodeIgniter. Manual set and restore error_reporting to silence warning required.

--- code_igniter/application/models/m_oa_user.php
+++ code_igniter/application/models/m_oa_user.php
@@ -422,7 +422,7 @@ class M_oa_user extends MY_Model
             $ad_ldap_connect = 'ldap://'.$CI->config->item('ad_server');
             $ad_user = $username.'@'.$CI->config->item('ad_domain');
             $ad_secret = $password;
-            $ad = ldap_connect($ad_ldap_connect);
+            $ad = @ldap_connect($ad_ldap_connect);
             if (!$ad) {
                 // log the failed attempt to connect to AD and fall through for local validation by Open-AudIT
                 $log_details->severity = 5;
