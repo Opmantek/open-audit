30258014d85d754a7f469b733b69cee2cbc4eb9a
Mark Unwin <marku@opmantek.com>
2015-04-15 12:18:37 +1000

    Version string bump in all files to 1.6.4.

--- code_igniter/application/controllers/test.php
+++ code_igniter/application/controllers/test.php
@@ -28,7 +28,7 @@
 /**
  * @author Mark Unwin <marku@opmantek.com>
  *
- * @version 1.6.2
+ * @version 1.6.4
  *
  * @copyright Copyright (c) 2014, Opmantek
  * @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
