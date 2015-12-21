bf3122f34cc5f269e9e62e2acabf2d9ab03d6d6e
Mark Unwin <marku@opmantek.com>
2015-12-21 11:50:59 +1000

    Tweaked the config to use the request URI instead of guessing.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -59,7 +59,7 @@ $config['index_page'] = "index.php";
 | 'ORIG_PATH_INFO'	Uses the ORIG_PATH_INFO
 |
 */
-$config['uri_protocol']    = "AUTO";
+$config['uri_protocol']    = "REQUEST_URI";
 
 /*
 |--------------------------------------------------------------------------
