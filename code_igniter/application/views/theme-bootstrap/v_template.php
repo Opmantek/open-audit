6d652e82e99b0a4593ef6dcb364d96833bc7450b
Mark Unwin <marku@opmantek.com>
2016-04-15 15:11:02 +1000

    Moved the CSS for the menu dropdown into an external file to make for a cleaner html template in the Bootstrap theme.

--- code_igniter/application/views/theme-bootstrap/v_template.php
+++ code_igniter/application/views/theme-bootstrap/v_template.php
@@ -45,72 +45,20 @@ include "v_lang.php";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-
+    <link rel="stylesheet" href="<?php echo $this->config->config['oa_web_folder']; ?>/css/bootstrap-dropdown.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table.min.js"></script>
-
     <style>
         body {margin: 8px;}
-        /* below if to re-enable sub menu dropdowns in BS3 */
-        .dropdown-submenu {
-            position: relative;
-        }
-        .dropdown-submenu>.dropdown-menu {
-            top: 0;
-            left: 100%;
-            margin-top: -6px;
-            margin-left: -1px;
-            -webkit-border-radius: 0 6px 6px 6px;
-            -moz-border-radius: 0 6px 6px;
-            border-radius: 0 6px 6px 6px;
-        }
-        .dropdown-submenu:hover>.dropdown-menu {
-            display: block;
-        }
-        .dropdown-submenu>a:after {
-            display: block;
-            content: " ";
-            float: right;
-            width: 0;
-            height: 0;
-            border-color: transparent;
-            border-style: solid;
-            border-width: 5px 0 5px 5px;
-            border-left-color: #ccc;
-            margin-top: 5px;
-            margin-right: -10px;
-        }
-        .dropdown-submenu:hover>a:after {
-            border-left-color: #fff;
-        }
-        .dropdown-submenu.pull-left {
-            float: none;
-        }
-        .dropdown-submenu.pull-left>.dropdown-menu {
-            left: -100%;
-            margin-left: 10px;
-            -webkit-border-radius: 6px 0 6px 6px;
-            -moz-border-radius: 6px 0 6px 6px;
-            border-radius: 6px 0 6px 6px;
-        }
     </style>
-
-
-
 </head>
 <body>
 <div class="container-fluid">
-	<?php 
-    include "include_header.php";
-    if (isset($error->message) and $error->message != '') {
-        echo '<div class="alert alert-danger" role="alert"><pre>';
-        print_r($error);
-        echo '</pre></div>';
-     } else {
-        include($include.'.php');
-    }
-    ?>
+<?php 
+include "include_header.php";
+include($include.'.php');
+?>
 </div>
 </body>
 </html>
