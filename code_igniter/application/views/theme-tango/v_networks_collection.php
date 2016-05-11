9842f544c28fd053d9d49231cd7f999887df9ab2
Mark Unwin <marku@opmantek.com>
2016-05-11 10:04:23 +1000

    Removed link to delete a network on the view page. Because these are global and deleting one could cause audit submissions to be rejected, removed initially. Ability to delete may be added with v2.

--- code_igniter/application/views/theme-tango/v_networks_collection.php
+++ code_igniter/application/views/theme-tango/v_networks_collection.php
@@ -52,7 +52,7 @@ if (!empty($this->response->error)) {
 			<th><?php echo __('Edited By')?></th>
 			<th><?php echo __('Edited Date')?></th>
 			<th align='center'><?php echo __('Edit')?></th>
-			<th align='center'><?php echo __('Delete')?></th>
+			<!-- <th align='center'><?php echo __('Delete')?></th> -->
 		</tr>
 	</thead>
 	<tbody>
@@ -72,7 +72,7 @@ if (!empty($this->response->error)) {
 			<td><?php echo htmlentities($item->edited_by)?></td>
 			<td><?php echo htmlentities($item->edited_date)?></td>
 			<td align='center'><?php echo $edit_pic?></td>
-			<td align='center'><?php echo $delete_pic?></td>
+			<!-- <td align='center'><?php echo $delete_pic?></td> -->
 		</tr>
 		<?php endforeach; ?>
 	</tbody>
