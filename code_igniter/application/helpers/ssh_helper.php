d4530e2595e0feebd3bf9df6aff61aff8066ae7e
Mark Unwin <marku@opmantek.com>
2016-05-19 09:15:23 +1000

    Version string (@version) increased to 1.14 in all files.

--- code_igniter/application/helpers/ssh_helper.php
+++ code_igniter/application/helpers/ssh_helper.php
@@ -31,7 +31,8 @@ if (!defined('BASEPATH')) {
 /*
  * @package Open-AudIT
  * @author Mark Unwin <marku@opmantek.com>
- * @version 1.12.6
+ * 
+@version 1.14
  * @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
  */
 if (! function_exists('ssh_connect')) {
