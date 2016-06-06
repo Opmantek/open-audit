e80652b891267b2d60f54f77edccc3c6f2fb406b
Mark Unwin <marku@opmantek.com>
2016-06-06 10:45:22 -0400

    Change  to .

--- code_igniter/application/helpers/report_helper.php
+++ code_igniter/application/helpers/report_helper.php
@@ -48,7 +48,7 @@ if (! function_exists('refresh_report_definitions')) {
         # get the list of report XML definitions
         $report_files = array();
         $paths = array('c:\omk\reports',
-            $this->config->item('base_path') . '/code_igniter/application/controllers/reports',
+            $CI->config->item('base_path') . '/code_igniter/application/controllers/reports',
             '/usr/local/omk/reports',
             '/usr/local/opmojo/reports');
         foreach ($paths as $path) {
