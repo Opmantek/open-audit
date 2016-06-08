2b7930024a21ce6e6f02006924a9f04164b783ca
Mark Unwin <marku@opmantek.com>
2016-06-08 09:52:05 -0400

    Add to last commit to fix PHP7. Missed commenting out a line.

--- code_igniter/system/database/drivers/mysql/mysql_result.php
+++ code_igniter/system/database/drivers/mysql/mysql_result.php
@@ -171,7 +171,7 @@ class CI_DB_mysql_result extends CI_DB_result {
 	 */
 	function _fetch_object()
 	{
-		return mysql_fetch_object($this->result_id);
+		#return mysql_fetch_object($this->result_id);
 		return mysqli_fetch_object($this->result_id);
 	}
 
