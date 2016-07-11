d49ca52b26e994c73bb5d28310dde6352efc08eb
Mark Unwin <marku@opmantek.com>
2016-07-11 11:26:01 +1000

    Revision to allow for jsonapi format.

--- code_igniter/application/models/m_scripts.php
+++ code_igniter/application/models/m_scripts.php
@@ -75,10 +75,11 @@ class M_scripts extends MY_Model
         $CI = & get_instance();
         $return_data = array();
         $sql = "SELECT * FROM scripts WHERE id = ?";
-        $data = array(intval($CI->response->id));
+        $data = array(intval($CI->response->meta->id));
         $result = $this->run_sql($sql, $data);
-        $return_data['scripts'] = $result;
-        return($return_data);
+        $result[0]->options = json_decode($result[0]->options);
+        $result = $this->format_data($result, 'scripts');
+        return($result);
     }
 
     public function create()
@@ -86,18 +87,18 @@ class M_scripts extends MY_Model
         $CI = & get_instance();
         # check to see if we already have a script with the same name
         $sql = "SELECT COUNT(id) AS count FROM `scripts` WHERE `name` = ?";
-        $data = array($CI->response->received_data['name']);
+        $data = array($CI->response->meta->received_data->name);
         $result = $this->run_sql($sql, $data);
         if (intval($result[0]->count) != 0) {
             log_error('ERR-0010', 'm_scripts::create_script');
             return false;
         }
         $sql = "INSERT INTO `scripts` VALUES (NULL, ?, ?, ?, ?, ?, ?, NOW())";
-        $data = array(  $CI->response->received_data['name'], 
-                        json_encode($CI->response->received_data['options']), 
-                        $CI->response->received_data['description'], 
-                        $CI->response->received_data['based_on'], 
-                        '', 
+        $data = array(  $CI->response->meta->received_data->name,
+                        json_encode($CI->response->meta->received_data->options),
+                        $CI->response->meta->received_data->description,
+                        $CI->response->meta->received_data->based_on,
+                        '',
                         $CI->user->full_name);
         $this->run_sql($sql, $data);
         return $this->db->insert_id();
@@ -165,20 +166,20 @@ class M_scripts extends MY_Model
 
     public function update()
     {
-        // $CI = & get_instance();
-        // $sql = '';
-        // $fields = ' name description ';
-        // foreach ($CI->response->received_data as $key => $value) {
-        //     if (strpos($fields, ' '.$key.' ') !== false) {
-        //         if ($sql == '') {
-        //             $sql = "SET `" . $key . "` = '" . $value . "'";
-        //         } else {
-        //             $sql .= ", `" . $key . "` = '" . $value . "'";
-        //         }
-        //     }
-        // }
-        // $sql = "UPDATE `files` " . $sql . ", `edited_by` = '" . $CI->user->full_name . "', `edited_date` = NOW() WHERE id = " . intval($CI->response->id);
-        // $this->run_sql($sql, array());
+        $CI = & get_instance();
+        $sql = '';
+        $fields = ' name description ';
+        foreach ($CI->response->meta->received_data->attributes as $key => $value) {
+            if (strpos($fields, ' '.$key.' ') !== false) {
+                if ($sql == '') {
+                    $sql = "SET `" . $key . "` = '" . $value . "'";
+                } else {
+                    $sql .= ", `" . $key . "` = '" . $value . "'";
+                }
+            }
+        }
+        $sql = "UPDATE `scripts` " . $sql . ", `edited_by` = '" . $CI->user->full_name . "', `edited_date` = NOW() WHERE id = " . intval($CI->response->meta->id);
+        $this->run_sql($sql, array());
         return;
     }
 
@@ -186,44 +187,9 @@ class M_scripts extends MY_Model
     {
         $CI = & get_instance();
         $sql = "DELETE FROM `scripts` WHERE id = ?";
-        $data = array(intval($CI->response->id));
+        $data = array(intval($CI->response->meta->id));
         $this->run_sql($sql, $data);
         return;
-    }
-
-    private function run_sql($sql, $data = array())
-    {
-        $CI = & get_instance();
-        if ($sql == '') {
-            return;
-        }
-        $trace = debug_backtrace();
-        $caller = $trace[1];
-        // clean our SQL (usually adding the running model, etc)
-        $sql = $this->clean_sql($sql);
-        // store the current setting of db_debug
-        $temp_debug = $this->db->db_debug;
-        // set the db_debug setting to FALSE - this prevents the default CI error page and allows us
-        // to output a nice formatted page with the $error object
-        $this->db->db_debug = FALSE;
-        // run the query
-        $query = $this->db->query($sql, $data);
-        // if we have debug set to TRUE, store the last run query
-        if ($CI->response->meta->debug) {
-            $CI->response->meta->sql = $this->db->last_query();
-        }
-        // restore the origin setting to db_debug
-        $this->db->db_debug = $temp_debug;
-        // do we have an error?
-        if ($this->db->_error_message()) {
-            log_error('ERR-0009', strtolower(@$caller['class'] . '::' . @$caller['function']));
-            $CI->response->errors[count($CI->response->errors)-1]->detail_specific = $this->db->_error_message();
-            return false;
-        }
-        // no error, so get the result
-        $result = $query->result();
-        // return what we have
-        return ($result);
     }
 
     private function count_data($result)
