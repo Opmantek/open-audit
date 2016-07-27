0f40c68210a66247446a167046fff2450b7f4dd0
Mark Unwin <marku@opmantek.com>
2016-07-27 13:36:03 +1000

    Improved error handling in /credentials when creating a new set.

--- code_igniter/application/models/m_credentials.php
+++ code_igniter/application/models/m_credentials.php
@@ -208,17 +208,75 @@ class M_credentials extends MY_Model
     public function create()
     {
         $CI = & get_instance();
+        $log = new stdClass();
+        $log->severity = 7;
+        $log->file = 'system';
+
+        if (!empty($CI->response->meta->received_data->type)) {
+            $type = $CI->response->meta->received_data->type;
+            $log->message = "Using type from received_data.";
+            stdlog($log);
+        } else {
+            $type = $CI->response->meta->collection;
+            $log->message = "Using type from collection.";
+            stdlog($log);
+        }
+
+        # Required
+        if (!empty($CI->response->meta->received_data->attributes->credentials)) {
+            $credentials = (string)$this->encrypt->encode(json_encode($CI->response->meta->received_data->attributes->credentials));
+            $log->message = "Using credentials from received_data.";
+            stdlog($log);
+        } else {
+            $log->message = "Credentials not supplied - exiting." . json_encode($CI->response->meta);
+            stdlog($log);
+            return false;
+        }
+
+        # Required
+        if (!empty($CI->response->meta->received_data->attributes->name)) {
+            $name = (string)$CI->response->meta->received_data->attributes->name;
+            $log->message = "Using name from received_data.";
+            stdlog($log);
+        } else {
+            $log->message = "Name not supplied - exiting.";
+            stdlog($log);
+            return false;
+        }
+
+        # Optional
+        if (!empty($CI->response->meta->received_data->attributes->description)) {
+            $description = (string)$CI->response->meta->received_data->attributes->description;
+            $log->message = "Using description from received_data.";
+            stdlog($log);
+        } else {
+            $description = '';
+        }
+
         # check to see if we already have a set of credentials with the same name
         $sql = "SELECT COUNT(id) AS count FROM `credentials` WHERE `name` = ?";
-        $data = array($CI->response->meta->received_data->attributes->name);
+        $data = array($name);
         $result = $this->run_sql($sql, $data);
         if (intval($result[0]->count) != 0) {
+            $log->message = "Duplicate name found - exiting.";
+            stdlog($log);
             log_error('ERR-0010', 'm_credentials::create');
             return false;
         }
+
+        # Insert the new item
         $sql = "INSERT INTO `credentials` (id, name, description, type, credentials, org_id, edited_by, edited_date) VALUES (NULL, ?, ?, ?, ?, ?, ?, NOW())";
-        $data = array($CI->response->meta->received_data->attributes->name, $CI->response->meta->received_data->attributes->description, $CI->response->meta->received_data->attributes->type, (string)$this->encrypt->encode(json_encode($CI->response->meta->received_data->attributes->credentials)), "0", $CI->user->full_name);
+        $data = array($name, $description, $type, $credentials, "0", $CI->user->full_name);
         $this->run_sql($sql, $data);
-        return $this->db->insert_id();
+        $id = @$this->db->insert_id();
+        if (!empty($id)) {
+            $log->message = "Credentials created.";
+            stdlog($log);
+            return $id;
+        } else {
+            $log->message = "Credentials NOT created.";
+            stdlog($log);
+            return false;
+        }
     }
 }
