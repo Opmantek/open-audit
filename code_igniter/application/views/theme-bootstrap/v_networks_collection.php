2a002bafd97689260aa7be5ae15b774282c12932
Mark Unwin <marku@opmantek.com>
2016-07-28 13:38:46 +1000

    Allow for 0 networks to be sent and don't try to enumerate them on the networks_collection template.

--- code_igniter/application/views/theme-bootstrap/v_networks_collection.php
+++ code_igniter/application/views/theme-bootstrap/v_networks_collection.php
@@ -55,20 +55,22 @@
 		</thead>
 		<tbody>
 			<?php
-	        foreach ($this->response->data as $item):
-                $edit_pic = '<a class="btn btn-sm btn-info" href="networks/'.intval($item->id).'?action=update"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>';
-                $delete_pic = '<button type="button" class="btn btn-sm btn-danger" aria-label="Left Align" ><span class="glyphicon glyphicon-trash delete_link" data-id="' . intval($item->id) . '" data-name="' . htmlentities($item->attributes->name) . '" aria-hidden="true"></span></button>';
-	            ?>
-			<tr>
-                <td style='text-align:center;'><a class="btn btn-sm btn-success" href="<?php echo htmlentities($item->links->self); ?>"><?php echo htmlentities($item->id); ?></a></td>
-				<td><?php echo htmlentities($item->attributes->name)?></td>
-				<td><?php echo htmlentities($item->attributes->description)?></td>
-				<td><?php echo htmlentities($item->attributes->edited_by)?></td>
-				<td><?php echo htmlentities($item->attributes->edited_date)?></td>
-				<td style='text-align:center;'><?php echo $edit_pic?></td>
-				<td style='text-align:center;'><?php echo $delete_pic?></td>
-			</tr>
-			<?php endforeach; ?>
+            if (!empty($this->response->data)) {
+    	        foreach ($this->response->data as $item):
+                    $edit_pic = '<a class="btn btn-sm btn-info" href="networks/'.intval($item->id).'?action=update"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>';
+                    $delete_pic = '<button type="button" class="btn btn-sm btn-danger" aria-label="Left Align" ><span class="glyphicon glyphicon-trash delete_link" data-id="' . intval($item->id) . '" data-name="' . htmlentities($item->attributes->name) . '" aria-hidden="true"></span></button>';
+    	            ?>
+    			<tr>
+                    <td style='text-align:center;'><a class="btn btn-sm btn-success" href="<?php echo htmlentities($item->links->self); ?>"><?php echo htmlentities($item->id); ?></a></td>
+    				<td><?php echo htmlentities($item->attributes->name)?></td>
+    				<td><?php echo htmlentities($item->attributes->description)?></td>
+    				<td><?php echo htmlentities($item->attributes->edited_by)?></td>
+    				<td><?php echo htmlentities($item->attributes->edited_date)?></td>
+    				<td style='text-align:center;'><?php echo $edit_pic?></td>
+    				<td style='text-align:center;'><?php echo $delete_pic?></td>
+    			</tr>
+    			<?php endforeach; ?>
+            <?php } ?>
 		</tbody>
 	</table>
   </div>
