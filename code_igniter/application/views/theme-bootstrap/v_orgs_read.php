885ed67f2d8bd50bb8aad6736712c17b51066be6
Mark Unwin <marku@opmantek.com>
2016-09-05 11:46:10 +1000

    Code indent formatting.

--- code_igniter/application/views/theme-bootstrap/v_orgs_read.php
+++ code_igniter/application/views/theme-bootstrap/v_orgs_read.php
@@ -86,10 +86,10 @@ $item = $this->response->data[0];
                 <div class="col-sm-8 input-group">
                     <select class="data_type form-control" id="parent_id" name="parent_id" disabled>
                         <option value='' label=' '></option>
-                    <?php foreach ($this->response->included as $org) {
+                        <?php foreach ($this->response->included as $org) {
                         if ($org->type == 'orgs' and $org->attributes->id != $item->attributes->id) { ?>
                             <option value="<?php echo intval($org->attributes->id); ?>" <?php if ($org->attributes->id == $item->attributes->parent_id) { echo "selected"; } ?>><?php echo htmlentities($org->attributes->name); ?></option>
-                    <?php } } ?>
+                        <?php } } ?>
                     </select>
                     <?php if (!empty($edit)) { ?>
                     <span class="input-group-btn">
