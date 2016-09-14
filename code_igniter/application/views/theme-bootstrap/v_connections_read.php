7022f8ad0c73f0f9687d34b703bc4fc94ea72797
Mark Unwin <marku@opmantek.com>
2016-09-14 21:38:16 +1000

    Fixed type (edited_on should have been edited_date).

--- code_igniter/application/views/theme-bootstrap/v_connections_read.php
+++ code_igniter/application/views/theme-bootstrap/v_connections_read.php
@@ -164,7 +164,7 @@ $item = $this->response->data[0];
                     <div class="form-group">
                         <label for="edited_date" class="col-sm-3 control-label">Edited Date</label>
                         <div class="col-sm-8 input-group">
-                            <input type="text" class="form-control" id="edited_date" name="edited_date" value="<?php echo htmlentities($item->attributes->edited_by); ?>" disabled>
+                            <input type="text" class="form-control" id="edited_date" name="edited_date" value="<?php echo htmlentities($item->attributes->edited_date); ?>" disabled>
                         </div>
                     </div>
 
