fa019567d80555dff79558c704785194176374d9
Mark Unwin <marku@opmantek.com>
2016-10-05 19:55:37 +1000

    Added dummy controllers that redirect to old controlers and functions for groups, queries and logs.

---
+++ code_igniter/application/controllers/queries.php
@@ -0,0 +1,76 @@
+<?php
+#
+#  Copyright 2003-2015 Opmantek Limited (www.opmantek.com)
+#
+#  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
+#
+#  This file is part of Open-AudIT.
+#
+#  Open-AudIT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Open-AudIT is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Open-AudIT (most likely in a file named LICENSE).
+#  If not, see <http://www.gnu.org/licenses/>
+#
+#  For further information on Open-AudIT or for a license other than AGPL please see
+#  www.opmantek.com or email contact@opmantek.com
+#
+# *****************************************************************************
+
+/**
+ * @author Mark Unwin <marku@opmantek.com>
+ *
+ * 
+ * @version 1.12.8
+ *
+ * @copyright Copyright (c) 2014, Opmantek
+ * @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+ */
+
+/**
+ * Base Object Queries.
+ *
+ * @access   public
+ *
+ * @category Object
+ *
+ * @author   Mark Unwin <marku@opmantek.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+ *
+ * @link     http://www.open-audit.org
+ *
+ * @return   Admin
+ */
+class queries extends CI_Controller
+{
+    /**
+     * Constructor.
+     *
+     * @access    public
+     *
+     * @category  Constructor
+     *
+     * @author    Mark Unwin <marku@opmantek.com>
+     * @license   http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+     *
+     * @link      http://www.open-audit.org
+     *
+     * @return Admin
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        redirect('admin_report/list_reports');
+    }
+}
+// End of file queries.php
+// Location: ./controllers/queries.php
