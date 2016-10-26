87678ef79b632c5b929d3b119f64027d4a86f136
Mark Unwin <marku@opmantek.com>
2016-10-26 10:00:23 +1000

    Change the size on the dashboard icons because they were too large on Chrome on Ubuntu. Might need to revert.

--- code_igniter/application/views/theme-bootstrap/v_dashboards_collection.php
+++ code_igniter/application/views/theme-bootstrap/v_dashboards_collection.php
@@ -45,7 +45,7 @@
         $i = 0;
         foreach ($this->response->included as $endpoint) {
             $i++;
-            echo '<div class="col-sm-1 text-center">' . __($endpoint['name']) . '<br /><a class="btn btn-app" href="' . $endpoint['collection'] . '"><span class="badge">' . $endpoint['count'] . '</span><i class="fa fa-' . $endpoint['icon']  . ' fa-3x fa-fw" style="font-size: 2.5vw;"></i></a></div>';
+            echo '<div class="col-sm-1 text-center">' . __($endpoint['name']) . '<br /><a class="btn btn-app" href="' . $endpoint['collection'] . '"><span class="badge">' . $endpoint['count'] . '</span><i class="fa fa-' . $endpoint['icon']  . ' fa-3x fa-fw" style="font-size: 2vw;"></i></a></div>';
             if ($i == 12) {
                 echo "</div><br /><br /><div class=\"row\">";
             }
