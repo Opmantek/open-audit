b974a74bdf3a1066d19d58b6ef36931c3fc49e0c
Mark Unwin <marku@opmantek.com>
2016-11-30 12:58:26 +1000

    Version bump display_version in blank SQL schema.

--- other/openaudit_mysql.sql
+++ other/openaudit_mysql.sql
@@ -297,7 +297,7 @@ INSERT INTO `configuration` VALUES (NULL, 'discovery_serial_match','y','y','syst
 INSERT INTO `configuration` VALUES (NULL, 'discovery_update_groups','y','y','system',NOW(),'Should Open-AudIT update the device groups after discovering a device.');
 INSERT INTO `configuration` VALUES (NULL, 'discovery_use_ipmi','y','y','system',NOW(),'Should we use ipmitool for discovering management ports if ipmitool is installed.');
 INSERT INTO `configuration` VALUES (NULL, 'discovery_use_dns','y','y','system',NOW(),'Should we use DNS for looking up the hostname and domain.');
-INSERT INTO `configuration` VALUES (NULL, 'display_version','1.12.10','n','system',NOW(),'The version shown on the web pages.');
+INSERT INTO `configuration` VALUES (NULL, 'display_version','1.14','n','system',NOW(),'The version shown on the web pages.');
 INSERT INTO `configuration` VALUES (NULL, 'distinct_groups','y','y','system',NOW(),'Display Groups on the homepage, separated into the type of each Group.');
 INSERT INTO `configuration` VALUES (NULL, 'download_reports','download','y','system',NOW(),'Tells Open-AudIT to advise the browser to download as a file or display the csv, xml, json reports. Valid values are download and display.');
 INSERT INTO `configuration` VALUES (NULL, 'maps_api_key','AIzaSyAhAUqssRASeC0Pfyx1TW1DXRmboG5bdG0','y','system',NOW(),'The API key for Google Maps.');
