19fbb9032ed3f496cd989aeed37cfd878eeea429
Mark Unwin <marku@opmantek.com>
2016-12-19 13:25:57 +1000

    Initial work on manual device creation form.

--- code_igniter/application/views/theme-bootstrap/v_devices_create_form.php
+++ code_igniter/application/views/theme-bootstrap/v_devices_create_form.php
@@ -33,6 +33,13 @@
  * @copyright Copyright (c) 2014, Opmantek
  * @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
  */
+
+if (php_uname('s') != 'Windows NT') {
+    include(str_replace('views/theme-bootstrap/v_devices_create_form.php', 'controllers/include_device_types.php', __FILE__));
+} else {
+    include(str_replace('views\\theme-bootstrap\\v_devices_create_form.php', 'controllers\\include_device_types.php', __FILE__));
+}
+
 ?>
 <div class="panel panel-default">
   <div class="panel-heading">
@@ -43,17 +50,97 @@
   </div>
   <div class="panel-body">
     <form class="form-horizontal" id="form_update" method="post" action="<?php echo $this->response->links->self; ?>">
+
         <div class="form-group">
-            <label for="form_systemXML" class="col-sm-2 control-label">XML Input</label>
+            <label for="data[attributes][type]" class="col-sm-3 control-label">Type</label>
+            <div class="col-sm-8 input-group">
+                <select id="data[attributes][type]" name="data[attributes][type]" class="form-control">
+                    <option value=''></option>
+                <?php
+                foreach ($device_types as $key => $value) {
+                    if ($key != '') {
+                        echo "<option value='$key'>".__("$value")."</option>";
+                    }
+                }
+                ?>
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][hostname]" class="col-sm-3 control-label">Hostname</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][hostname]" name="data[attributes][hostname]">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][ip]" class="col-sm-3 control-label">IP</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][ip]" name="data[attributes][ip]">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][manufacturer]" class="col-sm-3 control-label">Manufacturer</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][manufacturer]" name="data[attributes][manufacturer]">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][model]" class="col-sm-3 control-label">Model</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][model]" name="data[attributes][model]">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][serial]" class="col-sm-3 control-label">Serial</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][serial]" name="data[attributes][serial]">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][org_id]" class="col-sm-3 control-label">Organisation</label>
+            <div class="col-sm-8 input-group">
+                <select class="form-control" id="data[attributes][org_id]" name="data[attributes][org_id]">
+                <?php
+                foreach ($this->response->included as $item) {
+                    if ($item->type == 'orgs') { ?>     <option value="<?php echo intval($item->id); ?>"><?php echo htmlspecialchars($item->attributes->name, REPLACE_FLAGS, CHARSET); ?></option>
+                <?php
+                    }
+                } ?></select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][location_id]" class="col-sm-3 control-label">Location</label>
+            <div class="col-sm-8 input-group">
+                <select class="form-control" id="data[attributes][location_id]" name="data[attributes][location_id]">
+                    <option value="" label=" "></option>
+                <?php
+                foreach ($this->response->included as $item) {
+                    if ($item->type == 'locations') { ?>        <option value="<?php echo intval($item->id); ?>"><?php echo htmlspecialchars($item->attributes->name, REPLACE_FLAGS, CHARSET); ?></option>
+                <?php
+                    }
+                } ?></select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="data[attributes][asset_number]" class="col-sm-3 control-label">Asset Number</label>
+            <div class="col-sm-8 input-group">
+                <input type="text" class="form-control" id="data[attributes][asset_number]" name="data[attributes][asset_number]">
+            </div>
+        </div>
+
+
+        <hr />
+        <div class="form-group">
+            <label for="form_systemXML" class="col-sm-3 control-label">XML Input</label>
             <div class="col-sm-4">
                 <div class="col-sm-12 input-group">
                     <textarea class="form-control" rows="10" id="form_systemXML"></textarea>
                 </div>
             </div>
         </div>
-
+        <hr />
         <div class="form-group">
-            <label for="upload_file" class="col-sm-2 control-label">File Input</label>
+            <label for="upload_file" class="col-sm-3 control-label">File Input</label>
             <div class="col-sm-4">
                 <div class="col-sm-12 input-group">
                     <input type="file" id="upload_file">
@@ -65,7 +152,6 @@
             <label for="submit" class="col-sm-2 control-label"></label>
             <div class="col-sm-4">
                 <div class="col-sm-8 input-group">
-                    <input type="hidden" value="credentials" id="data[type]" name="data[type]" />
                     <button id="submit" name="submit" type="submit" class="btn btn-default">Submit</button>
                 </div>
             </div>
