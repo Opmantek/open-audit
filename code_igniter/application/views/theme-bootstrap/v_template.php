1dd73d3edc0dc70ca30467b2dbdadcc32d173011
Mark Unwin <marku@opmantek.com>
2017-03-20 17:07:05 +1000

    Allow a flashdata of danger to match the bootstrap names.

--- code_igniter/application/views/theme-bootstrap/v_template.php
+++ code_igniter/application/views/theme-bootstrap/v_template.php
@@ -94,6 +94,10 @@ $temp = @$this->session->flashdata('error');
 if (!empty($temp)) {
     echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' . $this->session->flashdata('error') . "</div>\n";
 }
+$temp = @$this->session->flashdata('danger');
+if (!empty($temp)) {
+    echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' . $this->session->flashdata('danger') . "</div>\n";
+}
 $temp = @$this->session->flashdata('success');
 if (!empty($temp)) {
     echo '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' . $this->session->flashdata('success') . "</div>\n";
