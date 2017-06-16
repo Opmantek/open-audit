f0ee2874a2adf878b12e8b21b12e564132012a61
Mark Unwin <marku@opmantek.com>
2017-06-16 11:47:44 +1000

    Button style change on collection panel header.

--- code_igniter/application/views/theme-bootstrap/include_collection_panel_header.php
+++ code_igniter/application/views/theme-bootstrap/include_collection_panel_header.php
@@ -6,24 +6,29 @@ if (strpos($this->response->links->self, '?') !== false) {
     $export_link .= '?';
 }
 ?>
-        <div class="panel-title">
-            <span class="text-left"><?php echo ucwords(str_replace('_', ' ', ($this->response->meta->collection))); ?></span>
-            <div style="padding-left:10px;" class="pull-right btn-group" role="group" aria-label="...">
-                <a href="<?php echo $export_link; ?>format=csv" role="button" class="btn btn-xs btn-default">csv</a>
-                <a href="<?php echo $export_link; ?>format=html" role="button" class="btn btn-xs btn-default">html</a>
-                <a href="<?php echo $export_link; ?>format=json" role="button" class="btn btn-xs btn-default">json</a>
-                <a href="<?php echo $export_link; ?>format=xml" role="button" class="btn btn-xs btn-default">xml</a>
+<div class="panel-title clearfix">
+    <?php echo ucwords(str_replace('_', ' ', ($this->response->meta->collection))); ?>
+
+    <div class="pull-right">
+        <div class="btn-group" role="group" aria-label="...">
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">
+                    Export <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="<?php echo $export_link; ?>format=csv">CSV</a></li>
+                    <li><a href="<?php echo $export_link; ?>format=html">HTML</a></li>
+                    <li><a href="<?php echo $export_link; ?>format=json">JSON</a></li>
+                    <li><a href="<?php echo $export_link; ?>format=xml">XML</a></li>
+                </ul>
             </div>
-            <?php
-            if ($this->m_users->get_user_permission('', $this->response->meta->collection, 'c') and 
-                $this->response->meta->collection != 'configuration' and 
-                $this->response->meta->collection != 'database' and
-                $this->response->meta->collection != 'logs') { ?>
-                <span class="pull-right" style="padding-left:10px;">
-                    <a class="btn btn-xs btn-primary" href="<?php echo $this->response->meta->collection; ?>/create" role="button"><?php echo __('Create'); ?></a>
-                </span>
-            <?php } ?>
-            <span class="pull-right">
-                <?php echo $this->response->meta->filtered . ' of ' . $this->response->meta->total . ' results'; ?>&nbsp;
-            </span>
-        </div>
+    <?php
+    if ($this->m_users->get_user_permission('', $this->response->meta->collection, 'c') and
+        $this->response->meta->collection != 'configuration' and
+        $this->response->meta->collection != 'database' and
+        $this->response->meta->collection != 'logs') { ?>
+            <a class="btn btn-default btn-sm" role="button" href="<?php echo $this->response->meta->collection; ?>/create" role="button"><?php echo __('Create'); ?></a>
+    <?php } ?>
+        &nbsp;&nbsp;&nbsp;<?php echo $this->response->meta->filtered . ' of ' . $this->response->meta->total . ' results'; ?>
+    </div>
+</div>
