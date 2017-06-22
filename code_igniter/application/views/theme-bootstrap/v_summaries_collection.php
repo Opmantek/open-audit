292552d481b8c222c5a46fafc2ae47fd1163c319
Mark Unwin <marku@opmantek.com>
2017-06-22 16:04:46 +1000

    Add search to the panel headere. Rearrange and recolour buttons in list.

--- code_igniter/application/views/theme-bootstrap/v_summaries_collection.php
+++ code_igniter/application/views/theme-bootstrap/v_summaries_collection.php
@@ -38,6 +38,20 @@
         <div class="panel-heading clearfix">
             <div class="panel-title clearfix">
                 Resources
+
+
+      <form id="search_form" name="search_form" class="navbar-form navbar-right" style="margin-top:0px; margin-bottom:0px;" action="<?php echo $this->config->config['oa_web_folder']; ?>/index.php/search" method="post">
+        <div class="form-group">
+          <input type="text"   id="data[attributes][value]"   name="data[attributes][value]"   class="form-control input-sm" placeholder="Name or IP">
+          <input type="hidden" id="data[attributes][tables]"  name="data[attributes][tables]" value='["system"]' />
+          <input type="hidden" id="data[attributes][columns]" name="data[attributes][columns]" value='["name","ip"]' />
+        </div>
+        <button type="submit" class="btn btn-default btn-sm">Submit</button>
+        <button type="button" class="btn btn-default btn-sm" aria-label="Left Align" data-container="body" data-toggle="popover" data-placement="left" title="Device Search" data-content="Search the following fields: name, hostname, dns_hostname, sysName, domain, dns_domain, ip.">
+            <span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+        </button>
+      </form>
+
             </div>
         </div>
         <div class="panel-body">
@@ -47,6 +61,7 @@
             foreach ($this->response->included as $endpoint) {
                 if (!empty($endpoint->type) and $endpoint->type == 'collection') {
                     $i++;
+                    $endpoint->attributes->name = str_replace('Ldap Servers', 'LDAP', $endpoint->attributes->name);
                     echo '<div class="col-sm-1 text-center">' . __($endpoint->attributes->name) . '<br /><a class="btn btn-app" href="' . $endpoint->attributes->collection . '"><span class="badge">' . $endpoint->attributes->count . '</span><i class="fa fa-' . $endpoint->attributes->icon  . ' fa-3x fa-fw" style="font-size: 2vw;"></i></a></div>';
                     if ($i == 12) {
                         echo "</div><br /><br /><div class=\"row\">";
@@ -68,20 +83,20 @@
             <table class="table table-striped table-hover">
                 <thead>
                     <tr>
-                        <th class="col-xs-1 text-center"><?php echo __('Execute'); ?></th>
-                        <th class="col-xs-1 text-center"><?php echo __('View')?></th>
+                        <th class="sorter-false col-xs-1 text-center"><?php echo __('Execute'); ?></th>
+                        <th class="sorter-false col-xs-1 text-center"><?php echo __('View')?></th>
                         <th><?php echo __('Name')?></th>
-                        <th class="col-xs-1 text-center"><?php echo __('Count'); ?></th>
+                        <th class="sorter-false col-xs-1 text-center"><?php echo __('Count'); ?></th>
                         <?php if ($this->m_users->get_user_permission('', 'summaries', 'd')) { ?>
-                        <th class="col-xs-1 text-center"><?php echo __('Delete')?></th>
+                        <th class="sorter-false col-xs-1 text-center"><?php echo __('Delete')?></th>
                         <?php } ?>
                     </tr>
                 </thead>
                 <tbody>
                 <?php foreach ($this->response->data as $item) : ?>
                     <tr>
-                        <td class="text-center"><a class="btn btn-sm btn-primary" href="summaries/<?php echo intval($item->id); ?>?action=execute"><span class="glyphicon glyphicon-play" aria-hidden="true"></span></a></td>
-                        <td class="text-center"><a class="btn btn-sm btn-success" href="summaries/<?php echo intval($item->attributes->id); ?>"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a></td>
+                        <td class="text-center"><a class="btn btn-sm btn-success" href="summaries/<?php echo intval($item->id); ?>?action=execute"><span class="glyphicon glyphicon-play" aria-hidden="true"></span></a></td>
+                        <td class="text-center"><a class="btn btn-sm btn-primary" href="summaries/<?php echo intval($item->attributes->id); ?>"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></a></td>
                         <td><?php echo ucwords($item->attributes->name)?></td>
                         <td class="text-center"><?php echo ucwords($item->attributes->count)?></td>
                         <?php if ($this->m_users->get_user_permission('', 'summaries', 'd')) { ?>
