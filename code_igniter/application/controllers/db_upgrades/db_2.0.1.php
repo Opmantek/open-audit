b6a1a1631b4eccda0b78a1fb00517e1a7ae0df7e
Mark Unwin <marku@opmantek.com>
2017-06-25 19:29:20 +1000

    Insert new data into summaries (display_menu).

--- code_igniter/application/controllers/db_upgrades/db_2.0.1.php
+++ code_igniter/application/controllers/db_upgrades/db_2.0.1.php
@@ -32,6 +32,63 @@ $this->log_db('Upgrade database to 2.0.1 commenced');
 # summaries
 $this->alter_table('summaries', 'expose', "`menu_display` enum('y','n') NOT NULL DEFAULT 'y' AFTER `menu_category`");
 
+$sql = = "DELETE FROM `summaries`";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (1,'Device Classes',1,'Device','y','system','class','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (2,'Device Status',1,'Device','y','system','status','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (3,'Device Types',1,'Device','y','system','type','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (4,'DNS Domains',1,'Network','y','system','dns_domain','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (5,'Form Factors',1,'Device','y','system','form_factor','system.form_factor,system.class','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (6,'Manufacturers',1,'Device','y','system','manufacturer','system.model','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (7,'Operating System Families',1,'Device','y','system','os_family','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (8,'Server Types',1,'Server','y','server','type','server.name,server.full_name,server.version,server.status','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (9,'Services',1,'Software','y','service','name','service.name,service.state','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (10,'Software',1,'Software','y','software','name','software.name,software.version','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (11,'Software Keys',1,'Software','y','software_key','name','software_key.name,software_key.string,software_key.rel,software_key.edition','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (12,'Active Directory OU\'s',1,'Network','y','windows','active_directory_ou','windows.active_directory_ou,windows.client_site_name','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+$sql = "INSERT INTO `summaries` VALUES (13,'Operating System Names',1,'Device','y','system','os_name','','system','2000-01-01 00:00:00')";
+$this->db->query($sql);
+$this->log_db($this->db->last_query());
+
+
 # A config item for other Opmantek installed modules
 $sql = "INSERT INTO `configuration` VALUES (NULL, 'modules', '', 'n', 'system','2000-01-01 00:00:00', 'The list of installed Opmantek modules.')";
 $this->db->query($sql);
