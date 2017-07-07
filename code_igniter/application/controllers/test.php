62dd8c0aec8777938d4533f87227b4857000a480
Mark Unwin <marku@opmantek.com>
2017-07-07 15:55:44 +1000

    Added a function to compare the SQL in the database with the SQL file.

--- code_igniter/application/controllers/test.php
+++ code_igniter/application/controllers/test.php
@@ -64,6 +64,67 @@ class test extends CI_Controller
         redirect('/');
     }
 
+    public function db_compare()
+    {
+        $this->load->helper('diff');
+        $tables = array('attachment','attributes','audit_log','bios','change_log','chart','cluster','configuration','connections','credential','credentials','discoveries','discovery_log','disk','dns','edit_log','field','fields','file','files','graph','groups','invoice','invoice_item','ip','ldap_groups','ldap_servers','licenses','locations','log','logs','maps','memory','module','monitor','motherboard','netstat','network','networks','nmap','notes','oa_asset_select','oa_change','oa_temp','oa_user_sessions','optical','orgs','pagefile','partition','print_queue','processor','queries','roles','route','san','scripts','scsi','server','server_item','service','share','software','software_key','sound','summaries','system','task','tasks','user','user_group','users','variable','video','vm','warranty','windows');
+
+        if ((string) php_uname('s') === 'Windows NT') {
+            $sql_file = file('c:\\xamplite\\open-audit\\other\\openaudit_mysql.sql');
+        } else {
+            $sql_file = file('/usr/local/open-audit/other/openaudit_mysql.sql');
+        }
+
+        echo "<style>";
+        echo ".diff td {\n  vertical-align: top;\n  white-space: pre;\n  white-space: pre-wrap;\n  font-family: monospace;\n}\n";
+        echo ".diffDeleted span {\n  border: 1px solid rgb(255,192,192);\n  background: rgb(255,224,224);\n}\n";
+        echo ".diffInserted span {\n  border: 1px solid rgb(192,255,192);\n  background: rgb(224,255,224);\n}\n";
+        echo "</style>\n";
+
+        foreach ($tables as $table) {
+            # From the DB
+            $query = $this->db->query("SHOW CREATE TABLE `$table`");
+            $result = $query->result();
+            $db_schema = preg_replace('/AUTO_INCREMENT=\d+ /', '', $result[0]->{'Create Table'});
+
+            # From the file
+            for ($i=0; $i < count($sql_file); $i++) { 
+                if (strpos($sql_file[$i], "CREATE TABLE `$table`") !== false) {
+                    $file_schema = $sql_file[$i];
+                    for ($j=$i+1; $j < count($sql_file); $j++) {
+                        if (strpos($sql_file[$j], '/*!') === false) {
+                            $file_schema .= $sql_file[$j];
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+            $file_schema = preg_replace('/AUTO_INCREMENT=\d+ /', '', $file_schema);
+            $file_schema = str_replace(";\n", '', $file_schema);
+
+            # Count the differences
+            $diff = Diff::compare($file_schema, $db_schema);
+            $count_del = 0; $count_ins = 0;
+            foreach ($diff as $line) {
+                if ($line[1] == 1) { $count_del += 1; }
+                if ($line[1] == 2) { $count_ins += 1; }
+            }
+            if ($count_ins != 0) {
+                $count_ins = "<span style=\"color:limegreen;\">" . $count_ins . "</span>";
+            }
+            if ($count_del != 0) {
+                $count_del = "<span style=\"color:red;\">" . $count_del . "</span>";
+            }
+            # Output
+            echo "<h2>$table (file -> database)</h2>";
+            echo "<strong>Del: $count_del Ins: $count_ins</strong>\n";
+            echo Diff::toTable(Diff::compare($file_schema, $db_schema));
+            echo "=======================================\n";
+        }
+
+    }
+
     public function nettest()
     {
         $this->load->helper('network');
