b0eaee42b5931f821620e3771623fb8df20e1601
Mark Unwin <marku@opmantek.com>
2017-09-25 09:07:15 +1000

    Remove the special case for network cards.

--- code_igniter/application/models/m_devices.php
+++ code_igniter/application/models/m_devices.php
@@ -294,12 +294,11 @@ class M_devices extends MY_Model
             $sql = "/* m_devices::read_sub_resource */ " . "SELECT asset_number, purchase_invoice, purchase_order_number, purchase_cost_center, purchase_vendor, purchase_date, purchase_service_contract_number, lease_expiry_date, purchase_amount, warranty_duration, warranty_expires, warranty_type FROM system WHERE id = ?";
             $data = array($id);
         } elseif ($sub_resource == 'discovery_log') {
-            #$sql = "/* m_devices::read_sub_resource */ " . "SELECT `id`, `timestamp`, `file`, `function`, `message`, `command_status`, `command_output`, `command_time_to_execute`, `command` AS `time` FROM discovery_log WHERE system_id = ? " . $limit;
             $sql = "/* m_devices::read_sub_resource */ " . "SELECT `id`, `timestamp`, `file`, `function`, `message`, `command_status`, `command_output`, `command_time_to_execute`, `command` FROM discovery_log WHERE system_id = ? " . $limit;
             $data = array($id);
-        } elseif ($sub_resource == 'network') {
-            $sql = "SELECT ip.ip,  network.*, floor((system.sysuptime - network.iflastchange) /60/60/24/100) as days_since_changed, IF((network.ifadminstatus = 'down') OR (network.ifadminstatus = 'up' AND (network.ip_enabled != 'up' AND network.ip_enabled != 'dormant') AND (((system.sysuptime - network.iflastchange) > 60480000) OR (system.sysuptime < network.iflastchange))), 'available', 'used') AS available  FROM network LEFT JOIN system ON (network.system_id = system.id AND network.current = 'y') LEFT JOIN ip ON (ip.system_id = network.system_id and ip.net_index = network.net_index and ip.current = 'y') WHERE system.id = ? ";
-            $data = array($id);
+        #} elseif ($sub_resource == 'network') {
+            #$sql = "SELECT ip.ip,  network.*, floor((system.sysuptime - network.iflastchange) /60/60/24/100) as days_since_changed, IF((network.ifadminstatus = 'down') OR (network.ifadminstatus = 'up' AND (network.ip_enabled != 'up' AND network.ip_enabled != 'dormant') AND (((system.sysuptime - network.iflastchange) > 60480000) OR (system.sysuptime < network.iflastchange))), 'available', 'used') AS available  FROM network LEFT JOIN system ON (network.system_id = system.id AND network.current = 'y') LEFT JOIN ip ON (ip.system_id = network.system_id and ip.net_index = network.net_index and ip.current = 'y') WHERE system.id = ? ";
+            #$data = array($id);
         // } elseif ($sub_resource == 'fields') {
         //     $sql = "/* m_devices::read_sub_resource */ " . "SELECT fields.id as `fields.id`, fields.name AS `fields.name`, fields.type AS `fields.type`, fields.values AS `fields.values`, fields.placement AS `fields.placement`, field.* FROM fields LEFT JOIN field ON (field.fields_id = fields.id AND (field.system_id = ? OR field.system_id IS NULL))";
         //     $data = array($id);
