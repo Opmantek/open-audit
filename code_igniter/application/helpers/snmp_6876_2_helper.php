6cc97363861875499d8e1845f0ac2e992c7330d8
Mark Unwin <marku@opmantek.com>
2017-12-12 09:52:29 +1000

    Change to use vm_ident, not vm_id. Add esx type to vm guest section.

--- code_igniter/application/helpers/snmp_6876_2_helper.php
+++ code_igniter/application/helpers/snmp_6876_2_helper.php
@@ -55,9 +55,9 @@ if (is_array($guest_name) and count($guest_name) > 0) {
     $details->class = 'hypervisor';
     foreach ($guest_name as $key => $value) {
         $guest = new stdClass();
-        $guest->vm_id = str_replace('.1.3.6.1.4.1.6876.2.1.1.2.', '', $key);
+        $guest->vm_ident = str_replace('.1.3.6.1.4.1.6876.2.1.1.2.', '', $key);
         $guest->net = array();
-        $guest_mac = my_snmp_real_walk($ip, $credentials, "1.3.6.1.4.1.6876.2.4.1.7.".$guest->vm_id);
+        $guest_mac = my_snmp_real_walk($ip, $credentials, "1.3.6.1.4.1.6876.2.4.1.7.".$guest->vm_ident);
         if (isset($guest_mac) and is_array($guest_mac) and count($guest_mac) > 0) {
             if (!empty($guest_mac[0])) {
                 foreach ($guest_mac[0] as $k => $v) {
@@ -65,17 +65,18 @@ if (is_array($guest_name) and count($guest_name) > 0) {
                         $mk = str_replace(".1.3.6.1.4.1.6876.2.4.1.7.".$guest['id'].'.', '', $k);
                         $guest->net[$mk]['mac'] = snmp_clean($v);
                         $guest->net[$mk]['mac'] = strtolower(str_replace(" ", ":", $guest['net'][$mk]['mac']));
-                        $guest->net[$mk]['desc'] = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.4.1.4.".$guest->vm_id.".".$mk);
+                        $guest->net[$mk]['desc'] = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.4.1.4.".$guest->vm_ident.".".$mk);
                     }
                 }
             }
         }
-        $guest->uuid = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.10.".$guest->vm_id);
-        $guest->memory = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.5.".$guest->vm_id);
-        $guest->cpu = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.9.".$guest->vm_id);
-        $guest->status = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.6.".$guest->vm_id);
-        $guest->config_file = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.3.".$guest->vm_id);
+        $guest->uuid = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.10.".$guest->vm_ident);
+        $guest->memory = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.5.".$guest->vm_ident);
+        $guest->cpu = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.9.".$guest->vm_ident);
+        $guest->status = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.6.".$guest->vm_ident);
+        $guest->config_file = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.6876.2.1.1.3.".$guest->vm_ident);
         $guest->vm_group = '';
+        $guest->type = 'esx';
         $guests[] = $guest;
         unset($guest);
         unset($guest_mac);
