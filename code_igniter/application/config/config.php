2d916502fa4ef85fc89aeebb12d4d40efcdf718d
Mark Unwin <marku@opmantek.com>
2018-02-23 17:26:04 +1000

    Application version to 2.2.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if (! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20180105';
-$config['web_display_version'] = '2.1.1';
+$config['web_internal_version'] = '20180218';
+$config['web_display_version'] = '2.2';
 # $config['debug'] = false;
 /*
 |--------------------------------------------------------------------------
