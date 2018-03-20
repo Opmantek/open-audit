b35771b1c5e3c4a7440c1d9bdfc33ed8daf04163
Mark Unwin <marku@opmantek.com>
2018-03-20 17:20:01 +1000

    Updated dictionary entry for dashboards.

--- code_igniter/application/controllers/include_dictionary.php
+++ code_igniter/application/controllers/include_dictionary.php
@@ -198,7 +198,7 @@ if ($table == 'credentials') {
 }
 
 if ($table == 'dashboards') {
-    $dictionary->sentence = 'Open-AudIT Professional and Enterprises\' interactive Dashboard provides a single point of access to audit status and results.';
+    $dictionary->sentence = 'Open-AudIT Professional and Enterprises\' interactive Dashboard provides a single point of access to audit status and results. The ability to customize and create Dashboards is an Enterprise feature.';
     $dictionary->marketing = '<p>Simple, intuitive, dynamic - just some of the words used to describe Open-AudIT Enterprises\' interactive dashboard. The dashboard provides a single point of access to audit status, new software installs, device breakdowns, and fast access to missing devices.<br /><br />
     ' . $link . '<br /><br /></p>';
     $dictionary->about = '<p>Quickly view the status of devices on your network.<br /><br />
