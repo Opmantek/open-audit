bbcdd3ff4d2e8f6c74451fb0fef08946254afa1c
Mark Unwin <marku@opmantek.com>
2018-03-27 17:45:22 +1000

    Dictionary entry for /applications.

--- code_igniter/application/controllers/include_dictionary.php
+++ code_igniter/application/controllers/include_dictionary.php
@@ -54,6 +54,20 @@ if ($table == 'attachments') {
     $dictionary->columns->edited_date = $edited_date;
 }
 
+if ($table == 'applications') {
+    $dictionary->sentence = 'You can tag devices with an Application of your choosing.';
+    $dictionary->marketing = '<p>Applications are defined by you, the user and stored for Open-AudIT to use and associate with devices.<br /><br />
+    ' . $link . '<br /><br /></p>';
+    $dictionary->about = '<p>Applications are defined by you, the user and stored for Open-AudIT to use and associate with devices.<br /><br />
+    ' . $link . '<br /><br /></p>';
+    $dictionary->notes = '';
+    $dictionary->columns->id = $id;
+    $dictionary->columns->name = $name;
+    $dictionary->columns->org_id = $org_id;
+    $dictionary->columns->edited_by = $edited_by;
+    $dictionary->columns->edited_date = $edited_date;
+}
+
 if ($table == 'attributes') {
     $dictionary->sentence = 'Open-AudIT enables you to create and use your own values for certain stored fields. For example, if you have another status than those provided, just add it and use it. Simple.';
     $dictionary->marketing = '<p>Attributes are stored for Open-AudIT to use for particular fields.<br /><br />Currently, the attributes you can edit are associated with the following columns: Class, Environment, Status &amp; Type.<br /><br />
