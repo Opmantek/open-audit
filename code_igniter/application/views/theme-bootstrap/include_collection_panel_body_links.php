092fb7b4925fbe90380b6b90b0278376d68bfb87
Mark Unwin <marku@opmantek.com>
2018-04-27 15:19:11 +1000

    Use the new create_url function for links.

--- code_igniter/application/views/theme-bootstrap/include_collection_panel_body_links.php
+++ code_igniter/application/views/theme-bootstrap/include_collection_panel_body_links.php
@@ -11,29 +11,25 @@
 <?php
 if (count($this->response->meta->filter) > 0) {
     echo '<div class="panel panel-default pull-left">';
-    echo '<div class="panel-body">';
+    echo '<div class="panel-body"><h4>';
     foreach ($this->response->meta->filter as $item) {
-        if ($item->operator == '=') {
-            $label = 'label-success';
-        } else if ($item->operator == '!=') {
-            $label = 'label-danger';
-        } else {
-            $label = 'label-info';
-        }
+        $query_parameters = $this->response->meta->query_parameters;
         if ($item->operator == '=') {
             $operator = '';
         } else {
             $operator = $item->operator;
         }
-        $link = str_replace($item->name . '=' . $operator . $item->value, '', $_SERVER["REQUEST_URI"]);
-        $link = str_replace($item->name . '=' . $operator . urlencode($item->value), '', $_SERVER["REQUEST_URI"]);
-        if ($item->name == 'status' and $item->operator == '=' and $item->value == 'production') {
-            $link = $refine_link . 'status=!=""';
+        for ($i=0; $i < count($query_parameters); $i++) {
+            $parameter = $query_parameters[$i];
+            if ($parameter === $item) {
+                array_splice($query_parameters, $i, 1);
+            }
         }
-        $label = 'label-info';
-        echo '<span class="label ' . $label . '">' . $item->name . ' ' . $item->operator . ' ' . htmlentities(urldecode($item->value)) . '&nbsp;&nbsp;<a href="' . $link . '">&times;</a></span>&nbsp;';
+        $link = create_url($query_parameters);
+        echo '<span class="label label-info">' . $item->name . ' ' . $item->operator . ' ' . urldecode($item->value) . '&nbsp;&nbsp;<a href="' . $link . '">&times;</a></span>&nbsp;';
+        unset($query_parameters);
     }
-    echo '</div>';
+    echo '</h4></div>';
     echo '</div>';
 }
 
@@ -45,12 +41,26 @@ function refine($property, $value, $display = '', $align = 'left')
     if (strlen($display) > 28) {
         $display = mb_substr($display, 0, 25) . ' ...';
     }
-    $refine_link = $_SERVER["REQUEST_URI"];
-    if (strpos($refine_link, '?') === false) {
-        $refine_link .= '?';
-    } else if (strrpos($refine_link, '&') !== strlen($refine_link)-1) {
-        $refine_link .= '&';
-    }
-    echo "            <td class=\"text-" . $align . "\"><span class=\"small glyphicon glyphicon-filter\" aria-hidden=\"true\" data-html=\"true\" data-toggle=\"popover\" title=\"Refine\" data-content=\"<a href='" . $refine_link . $property . "=!=" . urlencode($value) . "'>Exclude</a><br /><a href='" . $refine_link . $property . "=" . urlencode($value) . "'>Include</a><br />\"></span><span title=\"" . htmlspecialchars($value) . "\">" . htmlspecialchars($display, REPLACE_FLAGS, CHARSET) . "</span></td>\n";
+
+    $CI = & get_instance();
+    $query_parameters = $CI->response->meta->query_parameters;
+    $item = new stdClass();
+    $item->name = $property;
+    $item->operator = '=';
+    $item->value = $value;
+    $query_parameters[] = $item;
+    $include_link = create_url($query_parameters);
+    unset($query_parameters);
+
+    $query_parameters = $CI->response->meta->query_parameters;
+    $item = new stdClass();
+    $item->name = $property;
+    $item->operator = '!=';
+    $item->value = $value;
+    $query_parameters[] = $item;
+    $exclude_link = create_url($query_parameters);
+    unset($query_parameters);
+
+    echo '            <td class="text-' . $align . '"><span class="small glyphicon glyphicon-filter" aria-hidden="true" data-html="true" data-toggle="popover" title="Refine" data-content="<a href=\'' . $exclude_link . '\'>Exclude</a><br /><a href=\'' . $include_link . '\'>Include</a>"></span><span title="' . htmlspecialchars($value) . '">' . htmlspecialchars($display, REPLACE_FLAGS, CHARSET) . "</span></td>\n";
 }
 ?>
