42ba76de3aaa91d14c462cb859ac11a5584ca7e6
Mark Unwin <marku@opmantek.com>
2018-04-27 15:20:14 +1000

    Use the new create_url function for links.

--- code_igniter/application/views/theme-bootstrap/v_devices_collection.php
+++ code_igniter/application/views/theme-bootstrap/v_devices_collection.php
@@ -160,29 +160,25 @@ if ($this->response->meta->sub_resource !== 'group') {
 <?php
 if (count($this->response->meta->filter) > 0) {
     echo '<div class="panel panel-default pull-left">';
-    echo '<div class="panel-body">';
+    echo '<div class="panel-body"><h4>';
     foreach ($this->response->meta->filter as $item) {
-        if ($item->operator == '=') {
-            $label = 'label-success';
-        } else if ($item->operator == '!=') {
-            $label = 'label-danger';
-        } else {
-            $label = 'label-info';
-        }
+        $query_parameters = $this->response->meta->query_parameters;
         if ($item->operator == '=') {
             $operator = '';
         } else {
             $operator = $item->operator;
         }
-        $link = str_replace($item->name . '=' . $operator . $item->value, '', $_SERVER["REQUEST_URI"]);
-        $link = str_replace($item->name . '=' . $operator . urlencode($item->value), '', $_SERVER["REQUEST_URI"]);
-        if (($item->name == 'status' or $item->name == 'system.status') and $item->operator == '=' and $item->value == 'production') {
-            $link = $refine_link . 'system.status=!=\'\'';
+        for ($i=0; $i < count($query_parameters); $i++) {
+            $parameter = $query_parameters[$i];
+            if ($parameter === $item) {
+                array_splice($query_parameters, $i, 1);
+            }
         }
-        $label = 'label-info';
-        echo '<span class="label ' . $label . '">' . $item->name . ' ' . $item->operator . ' ' . urldecode($item->value) . '&nbsp;&nbsp;<a href="' . $link . '">&times;</a></span>&nbsp;';
+        $link = create_url($query_parameters);
+        echo '<span class="label label-info">' . $item->name . ' ' . $item->operator . ' ' . urldecode($item->value) . '&nbsp;&nbsp;<a href="' . $link . '">&times;</a></span>&nbsp;';
+        unset($query_parameters);
     }
-    echo '</div>';
+    echo '</h4></div>';
     echo '</div>';
 }
 
