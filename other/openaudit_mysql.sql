b39cd20c0c06dfa9628b5c40ae57e28edea057af
Mark Unwin <marku@opmantek.com>
2018-06-19 13:19:41 +1000

    Reset the auto-inc number for /configuration.

--- other/openaudit_mysql.sql
+++ other/openaudit_mysql.sql
@@ -570,7 +570,7 @@ CREATE TABLE `configuration` (
   `edited_date` datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
