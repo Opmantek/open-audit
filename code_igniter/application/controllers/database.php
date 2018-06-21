2d2e0a1ea28a49af9d967a3d73f2c04e06236598
Mark Unwin <marku@opmantek.com>
2018-06-22 09:49:53 +1000

    SQL version bump to 2.2.5

--- code_igniter/application/controllers/database.php
+++ code_igniter/application/controllers/database.php
@@ -727,6 +727,11 @@ class Database extends MY_Controller
             include "db_upgrades/db_2.2.4.php";
         }
 
+        // if (($db_internal_version < '20180705') and ($this->db->platform() == 'mysql')) {
+        //     # upgrade for 2.2.5
+        //     include "db_upgrades/db_2.2.5.php";
+        // }
+
         $this->data['include'] = 'v_database_update';
         $this->data['heading'] = 'Database Upgrade';
         $this->data['success'] = "Database upgraded successfully. New database version is ".$this->config->config['display_version']." (".$this->config->config['internal_version'].")";
