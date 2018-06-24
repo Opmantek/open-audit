d1afcbbfa19276de2905bf40916d2b216c2a94d7
Mark Unwin <marku@opmantek.com>
2018-06-25 09:30:42 +1000

    DB upgrade version to 2.2.5.

--- code_igniter/application/controllers/database.php
+++ code_igniter/application/controllers/database.php
@@ -727,10 +727,10 @@ class Database extends MY_Controller
             include "db_upgrades/db_2.2.4.php";
         }
 
-        // if (($db_internal_version < '20180705') and ($this->db->platform() == 'mysql')) {
-        //     # upgrade for 2.2.5
-        //     include "db_upgrades/db_2.2.5.php";
-        // }
+        if (($db_internal_version < '20180705') and ($this->db->platform() == 'mysql')) {
+            # upgrade for 2.2.5
+            include "db_upgrades/db_2.2.5.php";
+        }
 
         $this->data['include'] = 'v_database_update';
         $this->data['heading'] = 'Database Upgrade';
