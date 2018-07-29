307e67b40f6c7a51d61f5ae614cfa4ae515e123a
Mark Unwin <marku@opmantek.com>
2018-07-30 06:09:58 +1000

    Test code to process SNMP vendor and mac addresss vendor files.

---
+++ code_igniter/application/helpers/snmp_47196_helper.php
@@ -0,0 +1,50 @@
+<?php
+if (!defined('BASEPATH')) {
+     exit('No direct script access allowed');
+}
+#
+#  Copyright 2003-2015 Opmantek Limited (www.opmantek.com)
+#
+#  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
+#
+#  This file is part of Open-AudIT.
+#
+#  Open-AudIT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Open-AudIT is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Open-AudIT (most likely in a file named LICENSE).
+#  If not, see <http://www.gnu.org/licenses/>
+#
+#  For further information on Open-AudIT or for a license other than AGPL please see
+#  www.opmantek.com or email contact@opmantek.com
+#
+# *****************************************************************************
+
+/*
+* @category  Helper
+* @package   Open-AudIT
+* @author    Mark Unwin <marku@opmantek.com>
+* @copyright 2014 Opmantek
+* @license   http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+* @version   2.2.6
+* @link      http://www.open-audit.org
+ */
+
+# Vendor HP
+
+$get_oid_details = function ($ip, $credentials, $oid) {
+    $details = new stdClass();
+    if ($oid == '1.3.6.1.4.1.47196.4.1.1.1.2') {
+        $details->model = 'Aruba HPE-2930M';
+        $details->type = 'switch';
+    }
+    return($details);
+};
