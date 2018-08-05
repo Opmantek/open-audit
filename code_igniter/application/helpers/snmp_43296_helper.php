048e29a77925244a7e07eec5046e6faa00321fda
Mark Unwin <marku@opmantek.com>
2018-08-05 19:49:24 +1000

    Initial code to retrieve line cards, modules, power supplies and ports from Exablaze devices.

--- code_igniter/application/helpers/snmp_43296_helper.php
+++ code_igniter/application/helpers/snmp_43296_helper.php
@@ -46,7 +46,7 @@ $get_oid_details = function ($ip, $credentials, $oid) {
         $details->type = 'switch';
     }
 
-    $model = my_snmp_get($ip, $credentials, "3.6.1.4.1.43296.3.1.3");
+    $model = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.43296.3.1.3");
     if (!empty($model)) {
         $details->model = $details->model . " " . $model;
     }
@@ -56,4 +56,146 @@ $get_oid_details = function ($ip, $credentials, $oid) {
     $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.43296.3.1.1");
 
     return($details);
+};
+
+$get_modules = function ($ip, $credentials, $discovery_id, $system_id) {
+
+    $modules = array();
+
+    $log = new stdClass();
+    $log->command = 'snmpwalk 1.3.6.1.4.1.43296.3.1.5.1.1';
+    $log->command_status = 'fail';
+    $log->discovery_id = $discovery_id;
+    $log->file = 'snmp_43296_helper';
+    $log->function = 'get_modules';
+    $log->ip = $ip;
+    $log->message = 'Line Card retrieval for '.$ip;
+    $log->pid = getmypid();
+    $log->severity = 7;
+    $log->system_id = $system_id;
+    $log->timestamp = date('Y-m-b H:M:i');
+    $item_start = microtime(true);
+    $log->id = discovery_log($log);
+
+    # line cards
+    $modules_list = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.5.1.1');
+    $log->command_time_to_execute = (microtime(true) - $item_start);
+    $log->command_output = 'Count is ' . @count($modules_list);
+    $log->command_status = '';
+    discovery_log($log);
+    unset($log->id, $log->command, $log->command_time_to_execute, $log->command_output);
+
+    if (!empty($modules_list)) {
+        $names = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.5.1.2');
+    }
+    if (!empty($modules_list) and !empty($names)) {
+        $types = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.5.1.3');
+    }
+    if (!empty($modules_list) and !empty($names) and !empty($types)) {
+        foreach ($modules_list as $key => $value) {
+            $module = new stdClass();
+            $index = str_replace('.1.3.6.1.4.1.43296.3.1.5.1.1.', '', $key);
+            $module->description = 'Line Card - ' . $value . ' - ' . $names['.1.3.6.1.4.1.43296.3.1.5.1.2.'.$index];
+            $module->module_index = $index;
+            $module->object_ident = $key;
+            $module->contained_in = '';
+            $module->class = '1';
+            $module->class_text = 'other';
+            $module->hardware_revision = '';
+            $module->firmware_revision = '';
+            $module->software_revision = '';
+            $module->serial = '';
+            $module->asset_ident = '';
+            $module->is_fru = '';
+            $modules[] = $module;
+        }
+    }
+    unset($modules_list, $names, $types);
+
+    # modules
+    $modules_list = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.6.1.1');
+    if (!empty($modules_list)) {
+        $names = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.6.1.2');
+    }
+    if (!empty($modules_list) and !empty($names)) {
+        $boards = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.6.1.3');
+    }
+    if (!empty($modules_list) and !empty($names) and !empty($boards)) {
+        $functions = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.1.6.1.4');
+    }
+    if (!empty($modules_list) and !empty($names) and !empty($types) and !empty($boards)) {
+        foreach ($modules_list as $key => $value) {
+            $module = new stdClass();
+            $index = str_replace('.1.3.6.1.4.1.43296.3.1.6.1.1.', '', $key);
+            $module->description = 'Module - ' . $value . ' - ' . $names['.1.3.6.1.4.1.43296.3.1.6.1.2.'.$index];
+            $module->module_index = $index;
+            $module->object_ident = $key;
+            $module->contained_in = $boards['.1.3.6.1.4.1.43296.3.1.6.1.3.'.$index];
+            $module->class = '9';
+            $module->class_text = 'module';
+            $module->hardware_revision = '';
+            $module->firmware_revision = $function['.1.3.6.1.4.1.43296.3.1.6.1.4.'.$index];
+            $module->software_revision = '';
+            $module->serial = '';
+            $module->asset_ident = '';
+            $module->is_fru = '';
+            $modules[] = $module;
+        }
+    }
+    unset($modules_list, $names, $boards, $functions);
+
+    # power supplies
+    $modules_list = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.3.1.1');
+    if (!empty($modules_list)) {
+        $names = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.3.1.2');
+    }
+    if (!empty($modules_list) and !empty($names)) {
+        foreach ($modules_list as $key => $value) {
+            $module = new stdClass();
+            $index = str_replace('.1.3.6.1.4.1.43296.3.3.1.1.', '', $key);
+            $module->description = 'Power Supply - ' . $value . ' - ' . $names['.1.3.6.1.4.1.43296.3.3.1.2.'.$index];
+            $module->module_index = $index;
+            $module->object_ident = $key;
+            $module->contained_in = '';
+            $module->class = '6';
+            $module->class_text = 'powerSupply';
+            $module->hardware_revision = '';
+            $module->firmware_revision = '';
+            $module->software_revision = '';
+            $module->serial = '';
+            $module->asset_ident = '';
+            $module->is_fru = '';
+            $modules[] = $module;
+        }
+    }
+    unset($modules_list, $names);
+
+    # ports
+    $modules_list = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.4.1.2');
+    if (!empty($modules_list)) {
+        $names = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.4.1.3');
+    }
+    if (!empty($modules_list) and !empty($names)) {
+        $speeds = my_snmp_real_walk($ip, $credentials, '1.3.6.1.4.1.43296.3.4.1.9');
+    }
+    if (!empty($modules_list) and !empty($names)) {
+        foreach ($modules_list as $key => $value) {
+            $module = new stdClass();
+            $index = str_replace('.1.3.6.1.4.1.43296.3.4.1.2.', '', $key);
+            $module->description = 'Port - ' . $value . ' - ' . $names['.1.3.6.1.4.1.43296.3.4.1.3.'.$index] . ' at ' . $speeds['.1.3.6.1.4.1.43296.3.4.1.9.'.$index] . ' Mpbs';
+            $module->module_index = $index;
+            $module->object_ident = $key;
+            $module->contained_in = '';
+            $module->class = '10';
+            $module->class_text = 'port';
+            $module->hardware_revision = '';
+            $module->firmware_revision = '';
+            $module->software_revision = '';
+            $module->serial = '';
+            $module->asset_ident = '';
+            $module->is_fru = '';
+            $modules[] = $module;
+        }
+    }
+    return($modules);
 };
