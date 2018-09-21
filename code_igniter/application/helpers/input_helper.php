0e2884c956ad646e7d62657425ae042dc006a267
Mark Unwin <marku@opmantek.com>
2018-09-21 11:24:11 +1000

    Add racks, etc to roles.

--- code_igniter/application/helpers/input_helper.php
+++ code_igniter/application/helpers/input_helper.php
@@ -66,7 +66,7 @@ if (! function_exists('set_collection')) {
     {
         $CI = & get_instance();
         $collection = @$CI->uri->segment(1);
-        $collections = array('agents','applications','attributes','charts','clouds','collectors','configuration','connections','credentials','dashboards','database','devices','discoveries','errors','fields','files','graphs','groups','invoices','invoice_items','ldap_servers','licenses','locations','logs','networks','nmis','orgs','queries','racks','reports','roles','scripts','search','sessions','summaries','tasks','users','widgets');
+        $collections = array('agents','applications','attributes','buildings','charts','clouds','collectors','configuration','connections','credentials','dashboards','database','devices','discoveries','errors','fields','files','floors','graphs','groups','invoices','invoice_items','ldap_servers','licenses','locations','logs','networks','nmis','orgs','queries','racks','rack_devices','reports','roles','rooms','rows','scripts','search','sessions','summaries','tasks','users','widgets');
         if (!empty($collection) and in_array($collection, $collections)) {
             # a valid collection
         } else {
--- code_igniter/application/views/theme-bootstrap/v_roles_read.php
+++ code_igniter/application/views/theme-bootstrap/v_roles_read.php
@@ -91,7 +91,7 @@ $item = $this->response->data[0];
 </form>
 
 <?php
-$endpoints = array('agents','applications','attributes','charts','clouds','collectors','configuration','connections','credentials','dashboards','database','devices','discoveries','errors','fields','files','graphs','groups','invoices','invoice_items','ldap_servers','licenses','locations','logs','networks','nmis','orgs','queries','racks','reports','roles','scripts','search','sessions','summaries','tasks','users','widgets');
+$endpoints = array('agents','applications','attributes','buildings','charts','clouds','collectors','configuration','connections','credentials','dashboards','database','devices','discoveries','errors','fields','files','floors','graphs','groups','invoices','invoice_items','ldap_servers','licenses','locations','logs','networks','nmis','orgs','queries','racks','rack_devices','reports','roles','rooms','rows','scripts','search','sessions','summaries','tasks','users','widgets');
 
 $permissions = array('c', 'r', 'u', 'd');
 
