2422f6f30653538787ba8e92bc85ac7a5e857195
Mark Unwin <marku@opmantek.com>
2018-12-21 11:35:33 +1000

    Alter queue table definition and assign queue roles.

--- code_igniter/application/controllers/db_upgrades/db_2.3.2.php
+++ code_igniter/application/controllers/db_upgrades/db_2.3.2.php
@@ -83,6 +83,17 @@ if (!empty($result)) {
     }
 }
 
+# queue
+$this->alter_table('queue', 'name', "ADD `name` varchar(100) NOT NULL DEFAULT '' AFTER `id`", 'add');
+$this->alter_table('queue', 'status', "ADD `status` varchar(20) NOT NULL DEFAULT '' AFTER `pid`", 'add');
+$this->alter_table('queue', 'org_id', "ADD `org_id` int(10) unsigned NOT NULL DEFAULT '1', AFTER `type`", 'add');
+
+$this->load->model('m_roles');
+$this->m_roles->update_permissions('admin', 'queue', 'crud');
+$this->m_roles->update_permissions('org_admin', 'queue', 'cr');
+$this->m_roles->update_permissions('reporter', 'queue', 'r');
+$this->m_roles->update_permissions('user', 'queue', 'r');
+
 # set our versions
 $sql = "UPDATE `configuration` SET `value` = '20181225' WHERE `name` = 'internal_version'";
 $this->db->query($sql);
