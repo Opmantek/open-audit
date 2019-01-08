91de84fe900d3fbf9aa4410662a7acda3ec21924
Mark Unwin <marku@opmantek.com>
2019-01-08 12:29:39 +1000

    Scafolding for a queue controller.

---
+++ code_igniter/application/controllers/queue.php
@@ -0,0 +1,143 @@
+<?php
+/**
+#  Copyright 2003-2015 Opmantek Limited (www.opmantek.com)
+#
+#  ALL CODE MODIFICATIONS MUST BE SENT TO CODE@OPMANTEK.COM
+#
+#  This file is part of Open-AudIT.
+#
+#  Open-AudIT is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Open-AudIT is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Open-AudIT (most likely in a file named LICENSE).
+#  If not, see <http://www.gnu.org/licenses/>
+#
+#  For further information on Open-AudIT or for a license other than AGPL please see
+#  www.opmantek.com or email contact@opmantek.com
+#
+# *****************************************************************************
+*
+* @category  Controller
+* @package   Open-AudIT
+* @author    Mark Unwin <marku@opmantek.com>
+* @copyright 2014 Opmantek
+* @license   http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+* @version   2.3.2
+* @link      http://www.open-audit.org
+*/
+
+/**
+* Base Object Queue
+*
+* @access   public
+* @category Object
+* @package  Open-AudIT
+* @author   Mark Unwin <marku@opmantek.com>
+* @license  http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+* @link     http://www.open-audit.org
+ */
+class Queue extends MY_Controller
+{
+    /**
+    * Constructor
+    *
+    * @access    public
+
+    */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_queue');
+        inputRead();
+        $this->output->url = $this->config->item('oa_web_index');
+    }
+
+    /**
+    * Index that is unused
+    *
+    * @access public
+    * @return NULL
+    */
+    public function index()
+    {
+        # Unused
+    }
+
+    /**
+    * Our remap function to override the inbuilt controller->method functionality
+    *
+    * @access public
+    * @return NULL
+    */
+    public function _remap()
+    {
+        $this->{$this->response->meta->action}();
+    }
+
+    /**
+    * Process the supplied data and create a new object
+    *
+    * @access public
+    * @return NULL
+    */
+    public function create()
+    {
+        include 'include_create.php';
+    }
+
+    /**
+    * Read a single object
+    *
+    * @access public
+    * @return NULL
+    */
+    public function read()
+    {
+        include 'include_read.php';
+    }
+
+    /**
+    * Process the supplied data and update an existing object
+    *
+    * @access public
+    * @return NULL
+    */
+    public function update()
+    {
+        include 'include_update.php';
+    }
+
+    /**
+    * Delete an existing object
+    *
+    * @access public
+    * @return NULL
+    */
+    public function delete()
+    {
+        include 'include_delete.php';
+    }
+
+    /**
+    * Collection of objects
+    *
+    * @access public
+    * @return NULL
+    */
+    public function collection()
+    {
+        include 'include_collection.php';
+    }
+
+
+}
+// End of file queue.php
+// Location: ./controllers/queue.php
