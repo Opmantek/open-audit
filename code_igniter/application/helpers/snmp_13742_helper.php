e5a24bb2752b45e57ccef22ced5aef0ef3316d61
Mark Unwin <marku@opmantek.com>
2019-03-07 10:48:53 +1000

    SNMP helper update for Raritan.

--- code_igniter/application/helpers/snmp_13742_helper.php
+++ code_igniter/application/helpers/snmp_13742_helper.php
@@ -42,6 +42,27 @@ if (!defined('BASEPATH')) {
 
 $get_oid_details = function ($ip, $credentials, $oid) {
     $details = new stdClass();
-    if ($oid == '1.3.6.1.4.1.13742.6') { $details->model = 'Raritan PDU'; $details->type = 'pdu'; }
+
+    if ($details->serial == '') {
+        $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.13742.4.1.1.2.0");
+    }
+    if ($details->model == '') {
+        $details->model = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.13742.4.1.1.12.0");
+    }
+    if (empty($details->model)) {
+        $details->model = 'Raritan PDU';
+        // if ($oid == '1.3.6.1.4.1.13742.4') { }
+        // if ($oid == '1.3.6.1.4.1.13742.6') { }
+    }
+    if (empty($details->mac_address)) {
+        snmp_set_valueretrieval(SNMP_VALUE_LIBRARY);
+        $details->mac_address = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.13742.4.1.1.6.0");
+        snmp_set_valueretrieval(SNMP_VALUE_PLAIN);
+        $details->mac_address = format_mac($details->mac_address);
+    }
+    if (empty($details->netmask)) {
+        $details->netmask = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.13742.4.1.1.4.0");
+    }
+    $details->type = 'pdu';
     return($details);
 };
