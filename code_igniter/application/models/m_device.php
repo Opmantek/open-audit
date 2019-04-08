a5e6fa4ec9588e6cb63558b58bab17e775c3a02b
Mark Unwin <marku@opmantek.com>
2019-04-08 11:43:14 +1000

    Fixed test.

--- code_igniter/application/models/m_device.php
+++ code_igniter/application/models/m_device.php
@@ -640,7 +640,7 @@ class M_device extends MY_Model
             $data = array("$details->mac_address");
             $query = $this->db->query($sql, $data);
             $row = $query->row();
-            if (count($row) > 0) {
+            if (!empty($row->id)) {
                 $details->id = $row->id;
                 $log->system_id = $details->id;
                 $message = new stdClass();
