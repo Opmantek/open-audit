b7d830e2cf9b0b1bcae55a0d7b93e14cf65a6ac0
Mark Unwin <marku@opmantek.com>
2019-06-21 14:03:07 +1000

    SQL Schema upgrade trigger added.

--- code_igniter/application/controllers/database.php
+++ code_igniter/application/controllers/database.php
@@ -783,6 +783,11 @@ class Database extends MY_Controller
             include "db_upgrades/db_3.1.0.php";
         }
 
+        if (($db_internal_version < '20190620') and ($this->db->platform() == 'mysql' or $this->db->platform() == 'mysqli')) {
+            # upgrade for 3.1.1
+            include "db_upgrades/db_3.1.1.php";
+        }
+
         $this->data['include'] = 'v_database_update';
         $this->data['heading'] = 'Database Upgrade';
         $this->data['success'] = "Database upgraded successfully. New database version is ".$this->config->config['display_version']." (".$this->config->config['internal_version'].")";
