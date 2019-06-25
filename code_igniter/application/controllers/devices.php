9fc94303d81d69b1543c05294a59758f06ae9a5e
Mark Unwin <marku@opmantek.com>
2019-06-26 08:50:56 +1000

    Provide details of working credential sets.

--- code_igniter/application/controllers/devices.php
+++ code_igniter/application/controllers/devices.php
@@ -177,6 +177,18 @@ class devices extends MY_Controller
                         }
                     }
                 }
+                // # populate the credentials associated form the last discovery
+                if (!empty($this->response->data[0]->attributes->credentials)) {
+                    $credentials = @json_decode($this->response->data[0]->attributes->credentials);
+                    if (!empty($credentials)) {
+                        $this->load->model('m_credentials');
+                        foreach ($credentials as $credential_id) {
+                            $credential = $this->m_credentials->read($credential_id);
+                            unset($credential[0]->attributes->credentials);
+                            $this->response->included = array_merge($this->response->included, $credential);
+                        }
+                    }
+                }
             }
         }
         $this->response->meta->total = count($this->response->data);
