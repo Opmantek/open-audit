489e435ae08406c29595a19ebe0d463b88ed79ec
Mark Unwin <marku@opmantek.com>
2019-07-08 08:30:46 +1000

    TODO comment check later if working as intended.

--- code_igniter/application/models/m_devices_components.php
+++ code_igniter/application/models/m_devices_components.php
@@ -882,13 +882,24 @@ class M_devices_components extends MY_Model
             return;
         }
 
+        # TODO - validate this is working correctly.
+        #        I had to add the !empty test to the below, otherwise was seeing the below when manually running audit_linux.sh
+        /*
+            <p>Severity: Notice</p>
+            <p>Message:  Trying to get property 'id' of non-object</p>
+            <p>Filename: models/m_devices_components.php</p>
+            <p>Line Number: 889</p>
+        */
+        # Line 889 (was) $data = array($db_result[$i]->id);
         if (!empty($this->config->config['delete_noncurrent_' . $table]) and strtolower($this->config->config['delete_noncurrent_' . $table]) == 'y') {
             for ($i=0; $i < count($db_result); $i++) {
                 $sql = "DELETE FROM `$table` WHERE `id` = ?";
                 $sql = $this->clean_sql($sql);
-                $data = array($db_result[$i]->id);
-                $query = $this->db->query($sql, $data);
-                unset($db_result[$i]);
+                if (!empty($db_result[$i]->id)) {
+                    $data = array($db_result[$i]->id);
+                    $query = $this->db->query($sql, $data);
+                    unset($db_result[$i]);
+                }
             }
             return;
         }
