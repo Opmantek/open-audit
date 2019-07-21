9cab36de1c5023bc85b5dec18507cedf4892f560
Mark Unwin <marku@opmantek.com>
2019-07-22 05:49:39 +1000

    Provide wearning when Windows Apache running as Local System and we have failed audit script copies in the discovery log.

--- code_igniter/application/models/m_collection.php
+++ code_igniter/application/models/m_collection.php
@@ -239,8 +239,13 @@ class M_collection extends MY_Model
             if ((string) php_uname('s') === 'Windows NT') {
                 $user = get_current_user();
                 if ($user == 'SYSTEM') {
-                    $CI->response->meta->warning = 'WARNING - Windows is running the Apache service as "Local System". This should be changed to a real user (with network access) for optimal discovery results. See the <a href="https://community.opmantek.com/display/OA/Running+Open-AudIT+Apache+Service+under+Windows" target="_blank">Open-AudIT wiki</a> for more details.';
-                    $CI->session->set_flashdata('warning', $CI->response->meta->warning);
+                    $sql = "SELECT COUNT(*) as `count` FROM `discovery_log` WHERE `discovery_id` = ? AND `file` = 'wmi_helper' AND `function` = 'copy_to_windows' AND `message` = 'Net Use' and `command_status` = 'fail'";
+                    $data = array(intval($id));
+                    $data_result = $this->run_sql($sql, $data);
+                    if ($data_result[0]->count > 0){
+                        $CI->response->meta->warning = 'WARNING - Windows is running the Apache service as "Local System". This should be changed to a real user (with network access) for optimal discovery results. See the <a href="https://community.opmantek.com/display/OA/Running+Open-AudIT+Apache+Service+under+Windows" target="_blank">Open-AudIT wiki</a> for more details.';
+                        $CI->session->set_flashdata('warning', $CI->response->meta->warning);
+                    }
                 }
             }
         }
