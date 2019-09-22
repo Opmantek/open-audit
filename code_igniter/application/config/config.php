e13eb60d6ff6ea1e9242ef9193493c11ac6dea9e
Mark Unwin <marku@opmantek.com>
2019-09-23 09:36:35 +1000

    Version 3.2.2 schema upgrade.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if (! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20190919';
-$config['web_display_version'] = '3.2.1';
+$config['web_internal_version'] = '20190923';
+$config['web_display_version'] = '3.2.2';
 $config['microtime'] = microtime(true);
 # $config['debug'] = false;
 /*
