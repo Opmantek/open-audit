25480a4f5f6b80cb1bec347f3f2b75127fc4ade0
Mark Unwin <marku@opmantek.com>
2019-09-23 10:18:53 +1000

    Fix DB upgrade script for 3.2.2.

--- code_igniter/application/controllers/database.php
+++ code_igniter/application/controllers/database.php
@@ -806,6 +806,11 @@ class Database extends MY_Controller
             include "db_upgrades/db_3.2.1.php";
         }
 
+        if (($db_internal_version < '20190923') and ($this->db->platform() == 'mysql' or $this->db->platform() == 'mysqli')) {
+            # upgrade for 3.2.2
+            include "db_upgrades/db_3.2.2.php";
+        }
+
         $this->data['include'] = 'v_database_update';
         $this->data['heading'] = 'Database Upgrade';
         $this->data['success'] = "Database upgraded successfully. New database version is ".$this->config->config['display_version']." (".$this->config->config['internal_version'].")";
