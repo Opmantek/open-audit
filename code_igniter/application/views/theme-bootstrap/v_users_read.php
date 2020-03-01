93fb32db607532f1117b8108d7193d88d7578e04
Mark Unwin <marku@opmantek.com>
2020-03-02 08:27:13 +1000

    Use internal Org List, not included on users_read template.

--- code_igniter/application/views/theme-bootstrap/v_users_read.php
+++ code_igniter/application/views/theme-bootstrap/v_users_read.php
@@ -217,7 +217,7 @@ $item = $this->response->data[0];
             </thead>
             <tbody>
         <?php
-        foreach ($this->response->included as $org) {
+        foreach ($this->orgs as $org) {
             if ($org->type == 'orgs') {
                 if ($org->id == 1) {
                     $org->attributes->parent_name = '';
