d3ef429aa771d098921d5bc6265bf22d08d8bcce
Mark Unwin <marku@opmantek.com>
2020-03-05 09:40:37 +1000

    Add os_version for 2281 SNMP.

--- code_igniter/application/helpers/snmp_2281_helper.php
+++ code_igniter/application/helpers/snmp_2281_helper.php
@@ -44,5 +44,6 @@ $get_oid_details = function ($ip, $credentials, $oid) {
     $details = new stdClass();
     $details->model =  my_snmp_get($ip, $credentials, "1.3.6.1.4.1.2281.10.1.2.10.1.1.2.127");
     $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.2281.10.1.2.10.1.1.6.127");
+    $details->os_version = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.2281.10.4.1.13.1.1.4.1");
     return($details);
 };
