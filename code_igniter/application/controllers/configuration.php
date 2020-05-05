411204319b725ca6ccd0b3ce0699f8defbadea59
Mark Unwin <marku@opmantek.com>
2020-05-06 09:17:18 +1000

    Bug - remove attempt to retrieve OrgID from configuration::read.

--- code_igniter/application/controllers/configuration.php
+++ code_igniter/application/controllers/configuration.php
@@ -103,11 +103,6 @@ class Configuration extends MY_Controller
                 $options = $this->m_discovery_scan_options->collection($this->user->id);
                 $this->response->included = array_merge($this->response->included, $options);
             }
-            if ($this->response->meta->format === 'screen') {
-                $this->response->included = array_merge($this->response->included, $this->m_orgs->collection($this->user->id));
-            } else {
-                $this->response->included = array_merge($this->response->included, $this->m_orgs->read($this->response->data[0]->attributes->org_id));
-            }
         } else {
             log_error('ERR-0002', $this->response->meta->collection . ':read');
             $this->session->set_flashdata('error', 'No object could be retrieved when ' . $this->response->meta->collection . ' called m_' . $this->response->meta->collection . '->read.');
