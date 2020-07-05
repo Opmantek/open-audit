5d96026c3becc085a6d1b88bafde20628ffc7c29
Mark Unwin <marku@opmantek.com>
2020-07-06 09:53:38 +1000

    File and DB schema version bumps to 3.4.0.

--- other/audit_esxi.sh
+++ other/audit_esxi.sh
@@ -28,7 +28,7 @@
 # @package Open-AudIT
 # @author Mark Unwin <marku@opmantek.com>
 # 
-# @version   GIT: Open-AudIT_3.3.2
+# @version   GIT: Open-AudIT_3.4.0
 
 # @copyright Copyright (c) 2014, Opmantek
 # @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
@@ -79,7 +79,7 @@ self_delete='n'
 debugging=2
 
 # Version
-version="3.3.2"
+version="3.4.0"
 
 discovery_id=""
 
