4af974ff6cfa1d6b367707df94a448637042b012
Mark Unwin <marku@opmantek.com>
2020-09-08 11:09:48 +1000

    Fix - Enable oae_location to be editable in the configuration.

--- other/openaudit_mysql.sql
+++ other/openaudit_mysql.sql
@@ -934,7 +934,7 @@ INSERT INTO `configuration` VALUES (NULL,'modules','','text','n','system','2000-
 INSERT INTO `configuration` VALUES (NULL,'nmis','n','bool','y','system','2000-01-01 00:00:00','Enable import / export to NMIS functions.');
 INSERT INTO `configuration` VALUES (NULL,'nmis_url','','text','y','system','2000-01-01 00:00:00','The web server address of NMIS.');
 INSERT INTO `configuration` VALUES (NULL,'oae_license','','text','n','system','2000-01-01 00:00:00','License status of Open-AudIT Enterprise.');
-INSERT INTO `configuration` VALUES (NULL,'oae_location','','text','n','system','2000-01-01 00:00:00','The non-default location of Open-AudIT Enterprise.');
+INSERT INTO `configuration` VALUES (NULL,'oae_location','','text','y','system','2000-01-01 00:00:00','The non-default location of Open-AudIT Enterprise.');
 INSERT INTO `configuration` VALUES (NULL,'oae_product','Open-AudIT Community','text','n','system','2000-01-01 00:00:00','The name of the installed commercial application.');
 INSERT INTO `configuration` VALUES (NULL,'oae_prompt','2015-06-01','date','n','system','2000-01-01 00:00:00','Prompt to activate a license for Open-AudIT Enterprise.');
 INSERT INTO `configuration` VALUES (NULL,'oae_url','/omk/open-audit','text','y','system','2000-01-01 00:00:00','The web server address of Open-AudIT Enterprise.');
