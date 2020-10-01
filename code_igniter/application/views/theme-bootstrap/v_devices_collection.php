b4ff477cf99b3ad5879a8ba60eb026a2ef9ea66e
Mark Unwin <marku@opmantek.com>
2020-10-01 10:48:08 +1000

    Fix - Fix (unreleased) bug in filter items for devices::collection template.

--- code_igniter/application/views/theme-bootstrap/v_devices_collection.php
+++ code_igniter/application/views/theme-bootstrap/v_devices_collection.php
@@ -168,34 +168,17 @@ if (count($this->response->meta->filter) > 0) {
     echo '<div class="panel panel-default pull-left">';
     echo '<div class="panel-body"><h4>';
     foreach ($this->response->meta->filter as $item) {
-        $query_parameters = $this->response->meta->query_parameters;
         if ($item->operator == '=') {
             $operator = '';
         } else {
             $operator = $item->operator;
         }
-        for ($i=0; $i < count($query_parameters); $i++) {
-            $parameter = $query_parameters[$i];
-            if ($parameter === $item) {
-                array_splice($query_parameters, $i, 1);
-            }
+        $search = $item->name . '=' . $operator . $item->value;
+        $replace = '';
+        $link = '?' . str_replace($search, $replace, $this->response->meta->query_string);
+        if (stripos($link, '?&') === 0) {
+            $link = str_replace('?&', '?', $link);
         }
-        if ($item->name == 'system.status') {
-            $found = false;
-            foreach ($this->response->meta->query_parameters as $parameter) {
-                if ($parameter->name == 'system.status') {
-                    $found = true;
-                }
-            }
-            if (!$found) {
-                $parameter = new stdClass();
-                $parameter->name = 'system.status';
-                $parameter->operator = '!=';
-                $parameter->value = '';
-                $query_parameters[] = $parameter;
-            }
-        }
-        $link = create_url($query_parameters);
         echo '<span class="label label-info">' . htmlentities($item->name) . ' ' . htmlentities($item->operator) . ' ' . htmlentities(urldecode($item->value)) . '&nbsp;&nbsp;<a href="' . $link . '">&times;</a></span>&nbsp;';
         unset($query_parameters);
     }
