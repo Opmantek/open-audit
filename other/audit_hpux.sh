6d6d28723017f9afdd9ac413f3dc70d591615170
Mark Unwin <marku@opmantek.com>
2021-01-05 15:30:37 +1000

    File version bump to 3.5.3.

--- other/audit_hpux.sh
+++ other/audit_hpux.sh
@@ -28,7 +28,7 @@
 # @package Open-AudIT
 # @author Mark Unwin <marku@opmantek.com> and others
 # 
-# @version   GIT: Open-AudIT_3.5.2
+# @version   GIT: Open-AudIT_3.5.3
 
 # @copyright Copyright (c) 2014, Opmantek
 # @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
@@ -50,7 +50,7 @@ url="http://localhost/open-audit/index.php/input/devices"
 debugging=1
 
 # Version
-version="3.5.2"
+version="3.5.3"
 
 discovery_id=""
 help=""
