1ccf0813120dd1182edb37b27e4350b17ceff3d9
Mark Unwin <marku@opmantek.com>
2021-01-05 15:23:57 +1000

    DB Schema update to 3.5.3.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if ( ! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20201115';
-$config['web_display_version'] = '3.5.2';
+$config['web_internal_version'] = '20210104';
+$config['web_display_version'] = '3.5.3';
 $config['microtime'] = microtime(true);
 // $config['debug'] = false;
 /*
