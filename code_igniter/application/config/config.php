018b8bdc1807b6057de55ecba3b8f165669f6a16
Mark Unwin <marku@opmantek.com>
2021-02-01 16:38:22 +1000

    Version bump in config to 3.5.4.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if ( ! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20210104';
-$config['web_display_version'] = '3.5.3';
+$config['web_internal_version'] = '20210126';
+$config['web_display_version'] = '3.5.4';
 $config['microtime'] = microtime(true);
 // $config['debug'] = false;
 /*
