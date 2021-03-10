5d2971f7d24e84f389efe1f554e35ed8383c87b8
Mark Unwin <marku@opmantek.com>
2021-03-10 14:37:23 +1000

    Dashboards Read, populate all available widgets so can be displayed on template and selectable.

--- code_igniter/application/controllers/dashboards.php
+++ code_igniter/application/controllers/dashboards.php
@@ -116,10 +116,8 @@ class Dashboards extends MY_Controller
             $this->load->model('m_orgs');
             $this->response->dictionary = $this->{'m_'.$this->response->meta->collection}->dictionary();
             $this->load->model('m_widgets');
+            # Need all widgets for dashboards_read to be able to select another widget
             $this->response->included = array_merge($this->response->included, $this->m_widgets->collection($this->user->id));
-            #foreach ($this->response->data[0]->attributes->options->widgets as $widget) {
-            #    $this->response->included = array_merge($this->response->included, $this->m_widgets->read($widget->widget_id));
-            #}
             if ($this->response->meta->format === 'screen') {
                 $this->response->included = array_merge($this->response->included, $this->m_orgs->collection($this->user->id));
             } else {
