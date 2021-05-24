6a475ba70e32c3e135edcae1cd86587325b842a0
Mark Unwin <marku@opmantek.com>
2021-05-24 11:02:46 +1000

    Version to 4.1.1 in database schema and upgrade.

--- code_igniter/application/controllers/database.php
+++ code_igniter/application/controllers/database.php
@@ -901,6 +901,11 @@ class Database extends MY_Controller
             include "db_upgrades/db_3.5.4.php";
         }
 
+        if (($db_internal_version < '20210512') and ($this->db->platform() == 'mysql' or $this->db->platform() == 'mysqli')) {
+            # upgrade for 4.1.1
+            include "db_upgrades/db_4.1.1.php";
+        }
+
         $this->data['include'] = 'v_database_update';
         $this->data['heading'] = 'Database Upgrade';
         $this->data['success'] = "Database upgraded successfully. New database version is ".$this->config->config['display_version']." (".$this->config->config['internal_version'].")";
