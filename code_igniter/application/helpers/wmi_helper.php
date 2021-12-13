5f6617c65ac1299bcc89e7f1a094ae545eb65f7a
Mark Unwin <marku@opmantek.com>
2021-12-13 16:21:14 +1100

    File version bump to 4.3.1.

--- code_igniter/application/helpers/wmi_helper.php
+++ code_igniter/application/helpers/wmi_helper.php
@@ -32,7 +32,7 @@ if (!defined('BASEPATH')) {
  * @package Open-AudIT
  * @author Mark Unwin <marku@opmantek.com>
  *
- * @version   GIT: Open-AudIT_4.3.0
+ * @version   GIT: Open-AudIT_4.3.1
  * @license http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
  */
 
