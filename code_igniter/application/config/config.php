8761e04a3f94fa6175be0ef398e5533b31753429
Mark Unwin <marku@opmantek.com>
2022-01-27 08:08:02 +1000

    Version increases to 4.3.2.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if ( ! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20211213';
-$config['web_display_version'] = '4.3.1';
+$config['web_internal_version'] = '20220126';
+$config['web_display_version'] = '4.3.2';
 $config['microtime'] = microtime(true);
 // $config['debug'] = false;
 /*
