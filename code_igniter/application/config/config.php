a01fd35a8ba8edcf7b9cb8ac3353b8a4904d043f
Mark Unwin <marku@opmantek.com>
2022-04-22 10:39:45 +1000

    Trigger 4.3.3 upgrade.

--- code_igniter/application/config/config.php
+++ code_igniter/application/config/config.php
@@ -2,8 +2,8 @@
 if ( ! defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-$config['web_internal_version'] = '20220126';
-$config['web_display_version'] = '4.3.2';
+$config['web_internal_version'] = '20220321';
+$config['web_display_version'] = '4.3.3';
 $config['microtime'] = microtime(true);
 // $config['debug'] = false;
 /*
