f2da1a21f09219c4e97409ffb867c67a6bc5c6e2
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-04-27 13:50:54 +1000

    Buildings collection.

---
+++ app/Models/BuildingsModel.php
@@ -0,0 +1,252 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class BuildingsModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('buildings');
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $properties[] = 'locations.id as `locations.id`';
+        $properties[] = 'locations.name as `locations.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'buildings.org_id = orgs.id', 'left');
+        $this->builder->join('locations', 'buildings.location_id = locations.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'buildings');
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('buildings', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        $return = array();
+        $locations = $this->listUser();
+        $return['locations'] = $locations;
+        return $return;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'buildings.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $properties[] = 'locations.id as `locations.id`';
+        $properties[] = 'locations.name as `locations.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'buildings.org_id = orgs.id', 'left');
+        $this->builder->join('locations', 'buildings.location_id = locations.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'buildings');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'buildings');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('buildings')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        // Accept our client data
+        $data = $this->updateFieldData('buildings', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item for buildings
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'buildings';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'orgs.name', 'locations.name', 'description', 'edited_by', 'edited_date');
+        $dictionary->attributes->create = array('name', 'org_id', 'location_id'); # We MUST have each of these present and assigned a value
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection); # All field names for this table
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection); # The meta data about all fields - name, type, max_length, primary_key, nullable, default
+        $dictionary->attributes->update = $this->updateFields($collection); # We MAY update any of these listed fields
+
+        $dictionary->about =  '<p>Your buildings help refine exactly where your assets are located.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '<p>Buildings go inside your Locations and contain Floors.<br /><br /></p>';
+
+        $dictionary->product = 'professional';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->location_id = 'The location of the building. Links to <code>locations.id</code>.';
+        $dictionary->columns->options = 'unused';
+        $dictionary->columns->notes = 'unused';
+        $dictionary->columns->tags = 'unused';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
