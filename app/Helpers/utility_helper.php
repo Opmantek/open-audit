65339ba96b68936be5452bd38710d3c2a2bc5f28
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-02 14:06:35 +1000

    Add a test for a null ID in format_data because of database result with ID null, that is a valid row that contains (for example) 0 devices linked.

--- app/Helpers/utility_helper.php
+++ app/Helpers/utility_helper.php
@@ -36,6 +36,10 @@ function format_data($result, $type)
     $item->attributes = new \stdClass();
     $item->type = $type;
 
+    if (@$result[0]->id === null) {
+        return array();
+    }
+
     # format the attributes
     // $db = \Config\Database::connect();
     // $fields = $db->getFieldData($type);
