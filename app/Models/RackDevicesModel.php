2b40a3f4fd02da8e1129fa8de35190441e084707
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-24 15:08:48 +1000

    Racks and RackDevices models.

---
+++ app/Models/RackDevicesModel.php
@@ -0,0 +1,255 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class RackDevicesModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('rack_devices');
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $properties[] = "devices.name as `devices.name`";
+        $properties[] = "devices.id as `devices.id`";
+        $properties[] = "racks.name as `racks.name`";
+        $properties[] = "racks.id as `racks.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        $this->builder->join('devices', $resp->meta->collection . '.device_id = devices.id', 'left');
+        $this->builder->join('racks', $resp->meta->collection . '.rack_id = racks.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('rack_devices', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'rack_devices.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'rack_devices.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'rack_devices');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'rack_devices');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('rack_devices')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        // Accept our client data
+        $data = $this->updateFieldData('rack_devices', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'applications';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'racks.name', 'devices.name', 'orgs.name', 'edited_by', 'edited_date');
+        $dictionary->attributes->create = array('device_id', 'rack_id', 'position'); # We MUST have each of these present and assigned a value
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection); # All field names for this table
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection); # The meta data about all fields - name, type, max_length, primary_key, nullable, default
+        $dictionary->attributes->update = $this->updateFields($collection); # We MAY update any of these listed fields
+
+        $dictionary->about = '<p>These are the devices that live within a rack.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '<p>Your racks help refine exactly where your devices are located.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->product = 'enterprise';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->rack_id = 'The rack this device is located in. Links to <code>racks.id</code>.';
+        $dictionary->columns->device_id = 'The device within Open-AudIT. Links to <code>devices.id</code>.';
+        $dictionary->columns->position = 'The RU positon of the top of this device.';
+        $dictionary->columns->height = 'The number of RUs this device occupies.';
+        $dictionary->columns->width = 'The width of this device.';
+        $dictionary->columns->orientation = 'The orientation of this device.';
+        $dictionary->columns->type = 'The type of device.';
+        $dictionary->columns->options = 'unused';
+        $dictionary->columns->notes = 'unused';
+        $dictionary->columns->tags = 'unused';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
---
+++ app/Models/RacksModel.php
@@ -0,0 +1,248 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class RacksModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('racks');
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('racks', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        $included = array();
+        $properties = array();
+        $properties[] = 'rack_devices.*';
+        $properties[] = 'devices.name as `devices.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('devices', 'racks.device_id = devices.id', 'left');
+        $this->builder->where('rack_id', $id);
+        $query = $this->builder->get();
+        $included['rack_devices'] = format_data($query->getResult(), 'rack_devices');
+        return $included;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'racks.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'racks.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'racks');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'racks');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('racks')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        // Accept our client data
+        $data = $this->updateFieldData('racks', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'racks';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'description', 'orgs.name', 'edited_by', 'edited_date');
+        $dictionary->attributes->create = array('name','org_id'); # We MUST have each of these present and assigned a value
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection); # All field names for this table
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection); # The meta data about all fields - name, type, max_length, primary_key, nullable, default
+        $dictionary->attributes->update = $this->updateFields($collection); # We MAY update any of these listed fields
+
+        $dictionary->about = '<p>Applications are defined by you, the user and stored for Open-AudIT to use and associate with devices.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '';
+
+        $dictionary->product = 'enterprise';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
