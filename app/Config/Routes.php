84d8de367e83133efb5d40c468fae44421c1f66a
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-26 13:13:22 +1000

    Add route for input/log from a collector discovery.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -129,6 +129,7 @@ $routes->cli('test', 'Cli::test', ['as' => 'test']);
 $routes->cli('upgrade', 'Cli::upgrade', ['as' => 'upgrade']);
 
 $routes->post('input/devices', 'Input::devices');
+$routes->post('input/logs', 'Input::logs');
 $routes->get('queue/start', 'Queue::start');
 
 
