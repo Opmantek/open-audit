d046c839e1775113afbaca4aab06800a582aa6ed
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-26 14:38:50 +1000

    Add the model for Discovery Scan Options.

---
+++ app/Models/DiscoveryScanOptionsModel.php
@@ -0,0 +1,253 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class DiscoveryScanOptionsModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('discovery_scan_options');
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('discovery_scan_options', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'discovery_scan_options.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'discovery_scan_options.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'discovery_scan_options');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'discovery_scan_options');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('discovery_scan_options')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        // Accept our client data
+        $data = $this->updateFieldData('discovery_scan_options', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'applications';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'description', 'orgs.name', 'edited_by', 'edited_date');
+        $dictionary->attributes->create = array('name','org_id'); # We MUST have each of these present and assigned a value
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection); # All field names for this table
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection); # The meta data about all fields - name, type, max_length, primary_key, nullable, default
+        $dictionary->attributes->update = $this->updateFields($collection); # We MAY update any of these listed fields
+
+        $dictionary->about = '<p>Scanning options allow you to easily apply a set of options to a discovery.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '<p>Nmap timing details are found on the bottom of this linked page <a href="https://nmap.org/book/man-performance.html" target="_blank">https://nmap.org/book/man-performance.html</a>. From that page:<br /><br /><em><p>If you are on a decent broadband or ethernet connection, I would recommend always using -T4 (Aggressive). Some people love -T5 (Insane) though it is too aggressive for my taste. People sometimes specify -T2 (Polite) because they think it is less likely to crash hosts or because they consider themselves to be polite in general. They often don\'t realize just how slow -T2 really is. Their scan may take ten times longer than a default scan. Machine crashes and bandwidth problems are rare with the default timing options -T3 (Normal) and so I normally recommend that for cautious scanners. Omitting version detection is far more effective than playing with timing values at reducing these problems.</em><footer>Gordon \'Fyodor\' Lyon</footer><br /><br /></p>';
+
+        $dictionary->product = 'professional';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->ping = 'Should we ping the device before attempting to scan it? If it does not respond to the ping, skip this device.';
+        $dictionary->columns->service_version = 'When we receive an open port, should we attempt to test for the version of the service currently running upon it? This assists in confirming actual running services.';
+        $dictionary->columns->filtered = 'Should we consider a filtered port to be an open port - and therefore flag this IP as having a device attached?';
+        $dictionary->columns->{'open|filtered'} = 'Should we consider an open|filtered port to be an open port - and therefore flag this IP as having a device attached?';
+        $dictionary->columns->timing = 'The standard Nmap timing options. We usually use -T4 (Aggressive) as this is recommended for a decent broadband or ethernet connection.';
+        $dictionary->columns->timeout = 'The number of seconds to try and communicate with the target IP.';
+        $dictionary->columns->nmap_tcp_ports = 'The top 10, 100 or 1000 (or none) TCP ports commonly in use according to Nmap.';
+        $dictionary->columns->nmap_udp_ports = 'The top 10, 100 or 1000 (or none) UDP ports commonly in use according to Nmap.';
+        $dictionary->columns->tcp_ports = 'Any specific TCP ports you wish tested (comma seperated, no spaces).';
+        $dictionary->columns->udp_ports = 'Any specific UDP ports you wish tested (comma seperated, no spaces).';
+        $dictionary->columns->exclude_tcp_ports = 'Any TCP ports (comma seperated, no spaces) you wish to exclude from this discovery. Only available when using Nmap 7+.';
+        $dictionary->columns->exclude_udp_ports = 'Any UDP ports (comma seperated, no spaces) you wish to exclude from this discovery. Only available when using Nmap 7+.';
+        $dictionary->columns->exclude_ip = 'Specifies a comma-separated list of targets (no spaces) to be excluded from the scan. The list you pass in uses normal Nmap syntax, so it can include hostnames, CIDR netblocks, octet ranges, etc.';
+        $dictionary->columns->ssh_ports = 'If any of these (comma seperated, no spaces) ports are detected, assume SSH is running on them and use them for auditing. No need to add this port to the Custom TCP ports - it will be added automatically.';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
