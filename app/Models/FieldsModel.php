f48beb6197053ee870a93924bedfeb87df37103e
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-29 09:41:51 +1000

    Fields controller, model and views.

---
+++ app/Models/FieldsModel.php
@@ -0,0 +1,244 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class FieldsModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('fields');
+        # Use the below to execute BaseModel::__construct
+        # parent::__construct();
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('fields', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'fields.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'fields.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'fields');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'fields');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('fields')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        $data = $this->updateFieldData('fields', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'fields';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'type', 'placement', 'orgs.name');
+        $dictionary->attributes->create = array('name','org_id','type'); # We MUST have each of these present and assigned a value
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection); # All field names for this table
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection); # The meta data about all fields - name, type, max_length, primary_key, nullable, default
+        $dictionary->attributes->update = $this->updateFields($collection); # We MAY update any of these listed fields
+
+        $dictionary->about = '<p>Open-AudIT can store information in custom fields that are associated with each device.<br /><br />Once an Additional Field has been created it can be used in queries and groups just like any other attribute in the database.<br /><br />' . $instance->dictionary->link. '<br /><br /></p>';
+
+        $dictionary->notes = '<p>If you select a List type, the "values" field will be enabled. You should put a comma separated list of selectable values here. These will display in a dropdown box whenever the field is edited.<br /><br />Like most other attributes, Fields can be bulk edited. Use the Bulk Edit function as you normally would and you will see the Fields are available for input.<br /><br />You can edit the value of the Field for each individual device in the group you have assigned the field to. View the device details screen, open the section containing the field and (if you have sufficient access) you can click on the field value to edit it.<br /><br /></p>';
+
+        $dictionary->product = 'community';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->group_id = 'The group of devices this field applies to. To apply it to all devices, use the All Devices group. Links to <code>groups.id</code>.';
+        $dictionary->columns->type = 'This can be either a varchar (a text field) or a list (a list of values that can be selected).';
+        $dictionary->columns->values = 'A comma separated list of values, one of which can be selected.';
+        $dictionary->columns->placement = 'Where on the Device Details screen you would like to see this field appear.';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
---
+++ app/Views/fieldsCreateform.php
@@ -0,0 +1,81 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+include 'shared/create_functions.php';
+?>
+        <main class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <?= create_card_header($meta->collection, $meta->icon, $user); ?>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <form class="form-horizontal" method="post" action="<?= url_to($meta->collection.'Create') ?>">
+                                <input type="hidden" value="<?= $meta->access_token ?>" id="data[access_token]" name="data[access_token]" />
+
+                                <?= create_text_field('data[attributes][name]', __('Name'), $dictionary->attributes->create) ?>
+                                <?= create_select('data[attributes][org_id]', __('Organisation'), $orgs, $dictionary->attributes->create) ?>
+
+                                <div class="row" style="padding-top:16px;">
+                                    <div class="offset-2 col-8">
+                                        <label class="form-label" for="data[attributes][type]"><?= __('Type'); ?> <span style="color: #dc3545;">*</span></label>
+                                        <select class="form-select" name="data[attributes][type]" id="data[attributes][type]" required>
+                                            <option value="">Choose</option>
+                                            <option value="varchar"><?= __('Varchar'); ?></option>
+                                            <option value="list"><?= __('List'); ?></option>
+                                            <option value="date"><?= __('Date'); ?></option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <?= create_text_field('data[attributes][values]', __('Values'), $dictionary->attributes->create) ?>
+
+                                <br>
+                                <div class="row">
+                                    <div class="offset-2 col-8">
+                                        <label for="submit" class="form-label">&nbsp;</label>
+                                        <button id="submit" name="submit" type="submit" class="btn btn-primary"><?= __('Submit'); ?></button>
+                                        <input type="hidden" name="placement" id="placment" value="system">
+                                        <input type="hidden" name="group_id" id="group_id" value="1">
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="offset-2 col-8">
+                                <?php if (! empty($dictionary->about)) {
+                                    echo "<h4 class=\"text-center\">About</h4><br>";
+                                    echo html_entity_decode($dictionary->about);
+                                } ?>
+                                <?php if (! empty($dictionary->notes)) {
+                                    echo "<h4 class=\"text-center\">Notes</h4><br>";
+                                    echo html_entity_decode($dictionary->notes);
+                                } ?>
+                                <h4 class="text-center">Fields</h4><br>
+                                <?php foreach ($dictionary->columns as $key => $value) {
+                                    echo "<code>$key:</code> " . html_entity_decode($value) . "<br><br>";
+                                } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+<script>
+$(document).ready(function () {
+    $("#data\\[attributes\\]\\[values\\]").prop('disabled', true);
+
+    $(document).on('change', "#data\\[attributes\\]\\[type\\]", function () {
+        if ($("#data\\[attributes\\]\\[type\\]").val() == 'list') {
+            $("#data\\[attributes\\]\\[values\\]").prop('disabled', false);
+        } else {
+            $("#data\\[attributes\\]\\[values\\]").prop('disabled', true);
+        }
+    });
+});
+</script>
