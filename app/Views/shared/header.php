a8acbe661ce7ea16e53b0b55ec44beaf6cc87ff7
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-29 16:08:53 +1000

    Better breadcrumb naming.

--- app/Views/shared/header.php
+++ app/Views/shared/header.php
@@ -565,7 +565,7 @@ if (!empty($config->modules)) {
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="<?= url_to('home') ?>" class="link-secondary">Home</a></li>
-                                <li class="breadcrumb-item"><a href="<?= url_to($meta->collection.'Collection') ?>" class="link-secondary"><?= @ucfirst($meta->collection) ?></a></li>
+                                <li class="breadcrumb-item"><a href="<?= url_to($meta->collection.'Collection') ?>" class="link-secondary"><?= @ucwords(str_replace('_', ' ', $meta->collection)) ?></a></li>
                             <?php if ($meta->action === 'read' and !empty($name)) { ?>
                                 <li class="breadcrumb-item"><a href="<?= url_to($meta->collection.'Read', $meta->id) ?>" class="link-secondary"><?= $name ?></a></li>
                             <?php } ?>
