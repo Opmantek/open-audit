1188dc8e397b62bfbda97e8a779331a49f5735d3
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-29 11:11:39 +1000

    Add Groups controller, model and views.

---
+++ app/Models/GroupsModel.php
@@ -0,0 +1,291 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class GroupsModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('groups');
+        # Use the below to execute BaseModel::__construct
+        # parent::__construct();
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('groups', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Run the SQL definition and return the provided properties or the system.id list
+     * @param  integer $id The ID of the group
+     * @return array       An array of standard formatted devices, or an empty array
+     */
+    public function execute(int $id = 0, object $user = null): array
+    {
+        $instance = & get_instance();
+        $properties = config('Openaudit')->devices_default_group_columns;
+        $result = $this->builder->getWhere(['id' => intval($id)])->getResult();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        if (empty($result)) {
+            log_message('error', 'Failed to retrieve group');
+            return array();
+        }
+        $group = $result[0];
+        $sql = trim($group->sql);
+        if (strpos($sql, ';') === strlen($sql)-1) {
+            $sql = substr($sql, 0, strlen($sql)-1);
+            $sql = trim($sql);
+        }
+        $properties_array = explode(',', $properties);
+        $properties_new_array = array();
+        foreach ($properties_array as $property) {
+            $properties_new_array[] = "{$property} AS `{$property}`";
+        }
+        $properties = implode(', ', $properties_new_array);
+        $sql = str_ireplace('SELECT DISTINCT(devices.id) FROM devices', "SELECT {$properties} FROM devices", $sql);
+        $filter = "devices.org_id IN ({$user->org_list})";
+        if (!empty($instance->resp->meta->requestor)) {
+            $filter = "devices.org_id IN ({$user->org_list}) AND devices.oae_manage = 'y'";
+        }
+        $sql = str_ireplace('WHERE @filter', "WHERE {$filter}", $sql);
+        $sql .= ' ' . $instance->resp->meta->groupby . ' ' .
+                $instance->resp->meta->sort;
+        if (!empty($instance->resp->meta->limit)) {
+            $sql .= ' LIMIT ' . $instance->resp->meta->limit;
+        }
+                
+        $query = $this->db->query($sql);
+        log_message('debug', str_replace("\n", " ", (string)$this->db->getLastQuery()));
+        $result = format_data($query->getResult(), 'devices');
+        return $result;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'groups.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'fields.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'groups');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'groups');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('groups')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        $data = $this->updateFieldData('groups', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'fields';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'description', 'orgs.name');
+        $dictionary->attributes->create = array('name','org_id','sql');
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection);
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection);
+        $dictionary->attributes->update = $this->updateFields($collection);
+
+        $dictionary->about = '<p>Groups are used as simple lists of devices that match the required conditions. If requested using JSON they return a list of system.id\'s only. If requested using the web interface, they return the standard column attribute list.<br /><br />' . $instance->dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '<p>The SELECT section of your SQL <em>must</em> only contain <code>DISTINCT(system.id)</code>.<br /><br />The WHERE section of your SQL <em>must</em> contain <code>WHERE @filter</code> so Open-AudIT knows to restrict your query to the appropriate Orgs. SQL not containing this condition will result in the group failing to be created.<br /><br />An example for SQL to select all devices running the Debian OS - <code>SELECT DISTINCT(system.id) FROM system WHERE @filter AND system.os_family = \'Debian\'</code><br /><br /></p>';
+
+        $dictionary->product = 'community';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->sql = 'Your SQL to select devices that will populate this group.';
+        $dictionary->columns->link = 'unused';
+        $dictionary->columns->expose = 'Should we expose this group in the web interface.';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
