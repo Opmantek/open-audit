55fc52538c3a864ba66d6432aa9d94fb53ba6715
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-29 13:20:57 +1000

    Add FilesModel.

---
+++ app/Models/FilesModel.php
@@ -0,0 +1,247 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class FilesModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('files');
+        # Use the below to execute BaseModel::__construct
+        # parent::__construct();
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data->path = str_replace("'", '', $data->path);
+        $data->path = str_replace('"', '', $data->path);
+        $data->path = str_replace(';', '', $data->path);
+        $data->path = str_replace("\n", '', $data->path);
+        $data = $this->createFieldData('files', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'files.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'files.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'files');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'files');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('files')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        $data = $this->updateFieldData('files', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'files';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'description', 'path', 'orgs.name');
+        $dictionary->attributes->create = array('name','org_id','path');
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection);
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection);
+        $dictionary->attributes->update = $this->updateFields($collection);
+
+        $dictionary->about = '<p>Open-AudIT can retrieve details about a file or directory of files and monitor these files for changes as per other attributes in the Open-AudIT database.<br /><br />This feature works out of the box for Linux Open-AudIT servers, but needs a change to the service account name under a Windows Open-AudIT server.<br /><br />Supported clients are Windows and Linux.<br /><br />' . $CI->temp_dictionary->link . '<br /><br /></p>';
+
+        $dictionary->notes = '<p>See the note about enabling this feature when running a Windows Open-AudIT server on the wiki, here - <a href="https://community.opmantek.com/display/OA/files#Files-EnablingtheFeatureUnderWindows">https://community.opmantek.com/display/OA/files#Files-EnablingtheFeatureUnderWindows</a>.<br /><br /></p>';
+
+
+        $dictionary->product = 'enterprise';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->description = $instance->dictionary->description;
+        $dictionary->columns->path = 'The path to the file or directory.';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
