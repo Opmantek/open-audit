c7bfac21c2186fc749e666bb806b35011a8158ba
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-05-31 15:13:43 +1000

    Controller and Model for Summaries.

---
+++ app/Controllers/Summaries.php
@@ -0,0 +1,67 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+/**
+ * PHP version 7.4
+ *
+ * @category  Controller
+ * @package   Open-AudIT\Controller
+ * @author    Mark Unwin <mark.unwin@firstwave.com>
+ * @copyright 2023 FirstWave
+ * @license   http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+ * @version   GIT: Open-AudIT_5.0.0
+ * @link      http://www.open-audit.org
+ */
+
+/**
+ * Base Object Summaries
+ *
+ * @access   public
+ * @category Object
+ * @package  Open-AudIT\Controller\Summaries
+ * @author   Mark Unwin <mark.unwin@firstwave.com>
+ * @license  http://www.gnu.org/licenses/agpl-3.0.html aGPL v3
+ * @link     http://www.open-audit.org
+ */
+class Summaries extends BaseController
+{
+
+    /**
+     * Execute a query
+     *
+     * @access public
+     * @return void
+     */
+    public function execute($id)
+    {
+        $query = $this->summariesModel->read($this->resp->meta->id);
+        $this->resp->meta->name = $query[0]->attributes->name;
+        $this->resp->data = $this->summariesModel->execute($this->resp->meta->id, $this->user);
+        $this->resp->meta->total = count($this->resp->data);
+        $this->resp->meta->filtered = count($this->resp->data);
+        if ($this->resp->meta->format !== 'screen') {
+            output($this);
+            return true;
+        } else {
+            if (empty($this->resp->data)) {
+                \Config\Services::session()->setFlashdata('error', 'No data returned when running summary.');
+                return redirect()->route($this->resp->meta->collection.'Collection');
+            } else {
+                return view('shared/header', [
+                    'config' => $this->config,
+                    'dictionary' => $this->queriesModel->dictionary(),
+                    'meta' => filter_response($this->resp->meta),
+                    'orgs' => filter_response($this->orgsUser),
+                    'queries' => filter_response($this->queriesUser),
+                    'roles' => filter_response($this->roles),
+                    'user' => filter_response($this->user)]) .
+                    view($this->resp->meta->collection . ucfirst($this->resp->meta->action), ['data' => filter_response($this->resp->data), 'meta' => filter_response($this->resp->meta)]);
+            }
+        }
+    }
+}
---
+++ app/Models/SummariesModel.php
@@ -0,0 +1,329 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use stdClass;
+
+class SummariesModel extends BaseModel
+{
+
+    public function __construct()
+    {
+        $this->db = db_connect();
+        $this->builder = $this->db->table('summaries');
+        # Use the below to execute BaseModel::__construct
+        # parent::__construct();
+    }
+
+    /**
+     * Read the collection from the database
+     *
+     * @param  $resp object An object containing the properties, filter, sort and limit as passed by the user
+     *
+     * @return array        An array of formatted entries
+     */
+    public function collection(object $resp): array
+    {
+        $properties = $resp->meta->properties;
+        $properties[] = "orgs.name as `orgs.name`";
+        $properties[] = "orgs.id as `orgs.id`";
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', $resp->meta->collection . '.org_id = orgs.id', 'left');
+        foreach ($resp->meta->filter as $filter) {
+            if (in_array($filter->operator, ['!=', '>=', '<=', '=', '>', '<'])) {
+                $this->builder->{$filter->function}($filter->name . ' ' . $filter->operator, $filter->value);
+            } else {
+                $this->builder->{$filter->function}($filter->name, $filter->value);
+            }
+        }
+        $this->builder->orderBy($resp->meta->sort);
+        $this->builder->limit($resp->meta->limit, $resp->meta->offset);
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), $resp->meta->collection);
+    }
+
+    /**
+     * Create an individual item in the database
+     *
+     * @param  object $data The data attributes
+     *
+     * @return int|false    The Integer ID of the newly created item, or false
+     */
+    public function create($data = null)
+    {
+        if (empty($data)) {
+            return false;
+        }
+        $data = $this->createFieldData('summaries', $data);
+        $this->builder->insert($data);
+        if ($error = $this->sqlError($this->db->error())) {
+            \Config\Services::session()->setFlashdata('error', json_encode($error));
+            return false;
+        }
+        return ($this->db->insertID());
+    }
+
+    /**
+     * Delete an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return bool    true || false depending on success
+     */
+    public function delete($id = null, bool $purge = false): bool
+    {
+        $this->builder->delete(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        if ($this->db->affectedRows() !== 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public function execute(int $id = 0): array
+    {
+        $instance = & get_instance();
+        $limit_lower = 0;
+        $limit_upper = 8888888888;
+        $dashboard = $this->builder->getWhere(['id' => intval($id)])->getResult();
+
+        if ($dashboard[0]->table == 'orgs') {
+            $org_id = 'id';
+        } else {
+            $org_id = 'org_id';
+        }
+        $tables = ' field audit_log bios change_log credential disk dns edit_log file ip log memory module monitor motherboard netstat network nmap optical partition pagefile print_queue processor purchase route san scsi service server server_item share software software_key sound task user user_group variable video vm windows ';
+        $filter = '';
+        if (!empty($CI->response->meta->filter)) {
+            foreach ($CI->response->meta->filter as $filter_entry) {
+                $filter .= ' AND ' . $filter_entry->name . ' ' . $filter_entry->operator . ' ' . '"' . $filter_entry->value . '"';
+            }
+        }
+        if (stripos($tables, $dashboard[0]->table) !== false) {
+            $sql = "SELECT " . $dashboard[0]->id . " AS `id`, COUNT(*) AS `count`, " . $dashboard[0]->table . "." . $dashboard[0]->column . " AS `name` FROM devices LEFT JOIN `" . $dashboard[0]->table . "` ON (devices.id = " . $dashboard[0]->table . ".device_id and " . $dashboard[0]->table . ".current = 'y') WHERE " . $dashboard[0]->table . "." . $dashboard[0]->column . " IS NOT NULL AND " . $dashboard[0]->table . "." . $dashboard[0]->column . " != '' AND devices.org_id IN (" . $instance->user->org_list . ")" . $filter . " GROUP BY " . $dashboard[0]->table . "." . $dashboard[0]->column;
+        } else {
+            $sql = "SELECT " . $dashboard[0]->id . " AS `id`, COUNT(*) AS `count`, " . $dashboard[0]->column . " AS `name` FROM `" . $dashboard[0]->table . "` WHERE `$org_id` IN (" . $instance->user->org_list . ")" . $filter . " GROUP BY `" . $dashboard[0]->column . "`";
+        }
+
+        $result = $this->db->query($sql)->getResult();
+        // log_message('debug', str_replace("\n", " ", (string)$this->db->getLastQuery()));
+        $result = format_data($result, 'summaries');
+
+        switch ($dashboard[0]->table) {
+            case 'networks':
+                $collection = 'networks';
+                break;
+
+            case 'orgs':
+                $collection = 'orgs';
+                break;
+
+            case 'devices':
+                $collection = 'devices';
+                break;
+            
+            default:
+                $collection = 'devices';
+                break;
+        }
+        if (!empty($dashboard[0]->extra_columns)) {
+            $properties = 'devices.id,devices.icon,devices.type,devices.name,devices.domain,devices.ip,devices.os_family,devices.status,' . $dashboard[0]->extra_columns;
+        } else {
+            $properties = 'devices.id,devices.icon,devices.type,devices.name,devices.domain,devices.ip,devices.os_family,devices.status';
+        }
+        $link = url_to($collection.'Collection') . '?' . $dashboard[0]->table . '.' . $dashboard[0]->column . '=';
+        for ($i=0; $i < count($result); $i++) {
+            $result[$i]->attributes->link = $link . urlencode($result[$i]->attributes->name) . '&properties=' . $properties;
+        }
+        if (!empty($set_count)) {
+            if ($limit_upper == 8888888888) {
+                $CI->response->meta->filtered = count($result);
+            }
+            $CI->response->meta->total = count($result);
+        }
+        if ($limit_upper != 8888888888) {
+            $result = array_slice($result, $limit_lower, $limit_upper);
+            if (!empty($set_count)) {
+                $CI->response->meta->filtered = count($result);
+            }
+        }
+        if (empty($result)) {
+            $result = array();
+            $item = new stdClass();
+            $item->id = 0;
+            $item->type = 'summaries';
+            $item->attributes = new stdClass();
+            $item->attributes->id = 0;
+            $item->attributes->count = 0;
+            $item->attributes->name = '';
+            $item->attributes->link = '';
+            $result[] = $item;
+            unset($item);
+        }
+
+        return ($result);
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedRead(int $id = 0): array
+    {
+        return array();
+    }
+
+    /**
+     * Return an array containing arrays of related items to be stored in resp->included
+     *
+     * @param  int $id The ID of the requested item
+     * @return array  An array of anything needed for screen output
+     */
+    public function includedCreateForm(int $id = 0): array
+    {
+        return array();
+    }
+
+
+    /**
+     * Read the entire collection from the database that the user is allowed to read
+     *
+     * @return array  An array of formatted entries
+     */
+    public function listUser($where = array()): array
+    {
+        $instance = & get_instance();
+        $org_list = array_unique(array_merge($instance->user->orgs, $instance->orgsModel->getUserDescendants($instance->user->orgs, $instance->orgs)));
+        $org_list[] = 1;
+        $org_list = array_unique($org_list);
+
+        $properties = array();
+        $properties[] = 'summaries.*';
+        $properties[] = 'orgs.name as `orgs.name`';
+        $this->builder->select($properties, false);
+        $this->builder->join('orgs', 'summaries.org_id = orgs.id', 'left');
+        $this->builder->whereIn('orgs.id', $org_list);
+        if (!empty($where[0]) and !empty($where[1])) {
+            $this->builder->where($where[0], $where[1]);
+        }
+        if (!empty($where[2]) and !empty($where[3])) {
+            $this->builder->where($where[2], $where[3]);
+        }
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'summaries');
+    }
+
+    /**
+     * Read the entire collection from the database
+     *
+     * @return array  An array of all entries
+     */
+    public function listAll(): array
+    {
+        $query = $this->builder->get();
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return $query->getResult();
+    }
+
+    /**
+     * Read an individual item from the database, by ID
+     *
+     * @param  int $id The ID of the requested item
+     *
+     * @return array   The array containing the requested item
+     */
+    public function read(int $id = 0): array
+    {
+        $query = $this->builder->getWhere(['id' => intval($id)]);
+        if ($this->sqlError($this->db->error())) {
+            return array();
+        }
+        return format_data($query->getResult(), 'attributes');
+    }
+
+    /**
+     * Reset a table
+     *
+     * @return bool Did it work or not?
+     */
+    public function reset(string $table = ''): bool
+    {
+        if ($this->tableReset('attributes')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Update an individual item in the database
+     *
+     * @param  object  $data The data attributes
+     *
+     * @return bool    true || false depending on success
+     */
+    public function update($id = null, $data = null): bool
+    {
+        $data = $this->updateFieldData('summaries', $data);
+        $this->builder->where('id', intval($id));
+        $this->builder->update($data);
+        if ($this->sqlError($this->db->error())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * The dictionary item
+     *
+     * @return object  The stdClass object containing the dictionary
+     */
+    public function dictionary(): object
+    {
+        $instance = & get_instance();
+
+        $collection = 'summaries';
+        $dictionary = new stdClass();
+        $dictionary->table = $collection;
+        $dictionary->columns = new stdClass();
+
+        $dictionary->attributes = new stdClass();
+        $dictionary->attributes->collection = array('id', 'name', 'orgs.name');
+        $dictionary->attributes->create = array('name','org_id','table','column','menu_category');
+        $dictionary->attributes->fields = $this->db->getFieldNames($collection);
+        $dictionary->attributes->fieldsMeta = $this->db->getFieldData($collection);
+        $dictionary->attributes->update = $this->updateFields($collection);
+
+        $dictionary->about = '<p>Attributes are stored for Open-AudIT to use for particular fields.</p>';
+
+        $dictionary->notes = '<p>If you add a device type, to display the associated icon you will have to manually copy the .svg formatted file to the directory:<br /><em>Linux</em>: /usr/local/open-audit/www/open-audit/device_images<br /><em>Windows</em>: c:\xampp\htdocs\open-audit\device_images<br /><br />If you add a location type, to display the associated icon you will have to manually copy the 32x32px icon to the directory:<br /><em>Linux</em>: /usr/local/open-audit/www/open-audit/images/map_icons<br /><em>Windows</em>: c:\xampp\htdocs\open-audit\images\map_icons</p><p>When the <i>resource</i> is a \'device\', valid <i>types</i> are: \'class\', \'environment\', \'status\' and \'type\'. If the <i>resource</i> is \'locations\' or \'orgs\' the only valid <i>type</i> is \'type\'. If the <i>resource</i> is a \'query\' the only valid <i>type</i> is \'menu_category\'.</p>';
+
+        $dictionary->product = 'community';
+        $dictionary->columns->id = $instance->dictionary->id;
+        $dictionary->columns->name = $instance->dictionary->name;
+        $dictionary->columns->org_id = $instance->dictionary->org_id;
+        $dictionary->columns->table = 'The specific database table.';
+        $dictionary->columns->column = 'The specific database column from the specified database table.';
+        $dictionary->columns->extra_columns = 'The fully qualified table.column. Multiple may be provided, separated by a comma (no spaces).';
+        $dictionary->columns->menu_display = 'Should we expose this query in the list of reports under the Reports menu in the web interface.';
+        $dictionary->columns->menu_category = 'Which sub-menu should we display this query in.';
+        $dictionary->columns->edited_by = $instance->dictionary->edited_by;
+        $dictionary->columns->edited_date = $instance->dictionary->edited_date;
+        return $dictionary;
+    }
+}
