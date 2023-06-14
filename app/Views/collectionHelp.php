2e6bdcf4a0f52e6dce3cd895649f4aab02133e27
Mark Unwin <mark.unwin@firstwavecloud.com>
2023-06-14 12:18:43 +1000

    Format the Collection name for display in the template helper collection_functions.

--- app/Views/collectionHelp.php
+++ app/Views/collectionHelp.php
@@ -6,12 +6,12 @@ include 'shared/collection_functions.php';
         <main class="container-fluid">
             <div class="card">
                 <div class="card-header">
-                    <?= collection_card_header($meta->collection, $meta->icon, $user, 'About ' . ucwords($meta->collection)); ?>
+                    <?= collection_card_header($meta->collection, $meta->icon, $user, 'About ' . ucwords(str_replace('_', ' ', $meta->collection))); ?>
                 </div>
                 <div class="card-body text-center">
                     <div class="row">
                         <div class="col-8 offset-2 text-center">
-                            <h1><?= ucfirst($meta->collection) ?></h1>
+                            <h1><?= ucwords(str_replace('_', ' ', $meta->collection)) ?></h1>
                             <br>
                             <?= $dictionary->about ?>
                             <p>For more detailed information, check the Open-AudIT <a href="https://community.opmantek.com/display/OA/<?= $meta->collection ?>">Knowledge Base</a>.<br><br></p>
