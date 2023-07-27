399229c7935567bafb634273290af97c79708450
Mark Unwin <marku@opmantek.com>
2023-07-27 10:39:35 +1000

    Refactor routes.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -42,22 +42,15 @@ $routes->collections = array('applications','attributes','baselines',
 $routes->get('/', 'Collections::collection', ['filter' => \App\Filters\Session::class, 'as' => 'home']);
 
 # These will match and then take precedence over the below route array
+
+$routes->get('charts', 'Collections::collection', ['filter' => \App\Filters\Session::class, 'as' => 'chartsCollection']);
+$routes->get('charts/(:any)', 'Charts::read/$1', ['filter' => \App\Filters\Session::class, 'as' => 'chartsRead']);
+
 // TODO - Make this URL the same format as the two below (:num)/(:any) not (:any)/(:num)
 $routes->get('components/create/(:any)/(:num)', 'Components::createForm/$1/$2', ['filter' => \App\Filters\Session::class, 'as' => 'componentsCreateForm']);
 // TODO - Both below, we have to specify the url thus: components/1/image?components.type=image because we cannot cater to the type (yet) in the response_helper->response_get_permission_id
 $routes->delete('components/(:num)/(:any)', 'Components::delete/$1/$2', ['filter' => \App\Filters\Session::class, 'as' => 'componentsDelete']);
 $routes->get('components/(:num)/(:any)/download', 'Components::download/$1/$2');
-
-$routes->post('devices', 'Devices::create', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreate']);
-
-$routes->get('charts', 'Collections::collection', ['filter' => \App\Filters\Session::class, 'as' => 'chartsCollection']);
-$routes->get('charts/(:any)', 'Charts::read/$1', ['filter' => \App\Filters\Session::class, 'as' => 'chartsRead']);
-
-$routes->get('devices/create/example', 'devices::createExampleForm', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreateExampleForm']);
-$routes->get('devices/delete/example', 'devices::deleteExampleForm', ['filter' => \App\Filters\Session::class, 'as' => 'devicesDeleteExampleForm']);
-$routes->post('devices/create/example', 'devices::createExample', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreateExample']);
-$routes->post('devices/delete/example', 'devices::deleteExample', ['filter' => \App\Filters\Session::class, 'as' => 'devicesDeleteExample']);
-$routes->delete('devices/delete/example', 'devices::deleteExample', ['filter' => \App\Filters\Session::class]);
 
 $routes->get('database/update', 'Database::update/get', ['filter' => \App\Filters\Session::class, 'as' => 'databaseUpdate']);
 $routes->post('database/update', 'Database::update/post', ['filter' => \App\Filters\Session::class]);
@@ -66,38 +59,47 @@ $routes->get('database/help', 'Database::help', ['filter' => \App\Filters\Sessio
 $routes->get('database/(:any)', 'Collections::read/$1', ['filter' => \App\Filters\Session::class, 'as' => 'databaseRead']);
 $routes->get('database', 'Collections::collection', ['filter' => \App\Filters\Session::class, 'as' => 'databaseCollection']);
 
+$routes->post('devices', 'Devices::create', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreate']);
+$routes->get('devices/create/example', 'devices::createExampleForm', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreateExampleForm']);
+$routes->get('devices/delete/example', 'devices::deleteExampleForm', ['filter' => \App\Filters\Session::class, 'as' => 'devicesDeleteExampleForm']);
+$routes->post('devices/create/example', 'devices::createExample', ['filter' => \App\Filters\Session::class, 'as' => 'devicesCreateExample']);
+$routes->post('devices/delete/example', 'devices::deleteExample', ['filter' => \App\Filters\Session::class, 'as' => 'devicesDeleteExample']);
+$routes->delete('devices/delete/example', 'devices::deleteExample', ['filter' => \App\Filters\Session::class]);
+$routes->get('devices/importnmis', 'Devices::importNMISForm', ['filter' => \App\Filters\Session::class, 'as' => 'devicesImportNMISForm']);
+$routes->post('devices/importnmis', 'Devices::importNMIS', ['filter' => \App\Filters\Session::class, 'as' => 'devicesImportNMIS']);
+
+$routes->post('graph/reset', 'Collections::reset', ['filter' => \App\Filters\Session::class, 'as' => 'graphReset']);
+
+$routes->get('about', 'Help::about', ['filter' => \App\Filters\Session::class, 'as' => 'about']);
+$routes->get('features', 'Help::features', ['filter' => \App\Filters\Session::class, 'as' => 'features']);
+$routes->get('support', 'Help::support', ['filter' => \App\Filters\Session::class, 'as' => 'support']);
+
+$routes->post('input/devices', 'Input::devices');
+
+// TODO - Make the HTML template provide the complete list of fields and update that, rather than loop through and find the provided field and remove it
+//        That way we can use the standard 'update' function as per other collections.
+$routes->delete('integrations/(:num)/fields/(:num)', 'Integrations::update/$1', ['filter' => \App\Filters\Session::class]);
+$routes->post('integrations_log/reset', 'Collections::reset', ['filter' => \App\Filters\Session::class, 'as' => 'integrations_logReset']);
+
 $routes->get('logon', 'Logon::createForm', ['as' => 'logon']);
 $routes->get('logoff', 'Logon::delete', ['as' => 'logoff']);
 $routes->get('login', 'Logon::createForm');
 $routes->post('logon', 'Logon::create');
 $routes->get('logout', 'Logon::delete');
 
-$routes->post('search', 'Search::create', ['filter' => \App\Filters\Session::class, 'as' => 'searchCreate']);
+$routes->get('reports', 'Reports::Collection', ['filter' => \App\Filters\Session::class, 'as' => 'reportsCollection']);
 
 $routes->get('scripts/(:any)/download', 'Scripts::download/$1');
 
-$routes->get('about', 'Help::about', ['filter' => \App\Filters\Session::class, 'as' => 'about']);
-$routes->get('features', 'Help::features', ['filter' => \App\Filters\Session::class, 'as' => 'features']);
-$routes->get('support', 'Help::support', ['filter' => \App\Filters\Session::class, 'as' => 'support']);
-
-$routes->cli('rotateLogs', 'Cli::rotateLogs', ['as' => 'rotateLogs']);
-$routes->cli('test', 'Cli::test', ['as' => 'test']);
-$routes->cli('upgrade', 'Cli::upgrade', ['as' => 'upgrade']);
-
-$routes->post('input/devices', 'Input::devices');
-$routes->cli('queue/start', 'Queue::start');
-
-$routes->get('reports', 'Reports::Collection', ['filter' => \App\Filters\Session::class, 'as' => 'reportsCollection']);
+$routes->post('search', 'Search::create', ['filter' => \App\Filters\Session::class, 'as' => 'searchCreate']);
 
 $routes->post('util/subnet_size', 'Util::subnetSize');
 $routes->post('util/google', 'Util::google');
 
-// TODO - Make the HTML template provide the complete list of fields and update that, rather than loop through and find the provided field and remove it
-//        That way we can use the standard 'update' function as per other collections.
-$routes->delete('integrations/(:num)/fields/(:num)', 'Integrations::update/$1', ['filter' => \App\Filters\Session::class]);
-
-$routes->post('graph/reset', 'Collections::reset', ['filter' => \App\Filters\Session::class, 'as' => 'graphReset']);
-$routes->post('integrations_log/reset', 'Collections::reset', ['filter' => \App\Filters\Session::class, 'as' => 'integrations_logReset']);
+$routes->cli('queue/start', 'Queue::start');
+$routes->cli('rotateLogs', 'Cli::rotateLogs', ['as' => 'rotateLogs']);
+$routes->cli('test', 'Cli::test', ['as' => 'test']);
+# $routes->cli('upgrade', 'Cli::upgrade', ['as' => 'upgrade']);
 
 foreach ($routes->collections as $collection) {
     // Account for users editing the config and including a space character
