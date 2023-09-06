1c484e4f422a113b8ce1bbfb22b45fdd086a4f0a
Mark Unwin <marku@opmantek.com>
2023-09-06 16:10:40 +1000

    locationsRead template improvements.

--- app/Views/locationsRead.php
+++ app/Views/locationsRead.php
@@ -2,6 +2,25 @@
 # Copyright © 2023 FirstWave. All Rights Reserved.
 # SPDX-License-Identifier: AGPL-3.0-or-later
 include 'shared/read_functions.php';
+$disabled = 'disabled';
+if (!empty(config('Openaudit')->maps_api_key) and (config('Openaudit')->product === 'professional' or config('Openaudit')->product === 'enterprise')) {
+    $disabled = '';
+}
+#$disabled = 'disabled';
+#config('Openaudit')->maps_api_key = '';
+if ($user->toolbar_style === 'icontext') {
+    $latlong = '<li class="nav-item" role="presentation"><button type="button" id="locations_latlong" name="locations_latlong" class="locations_latlong btn btn-default"><span style="margin-right:6px;" class="fa-solid fa-globe text-success"></span>' . __('Get Lat/Long') . '</button></li>';
+
+    $geocode = '<li class="nav-item" role="presentation"><button type="button" id="locations_geocode" name="locations_geocode" class="locations_geocode btn btn-default"><span style="margin-right:6px;" class="fa-solid fa-map text-success"></span>' . __('Create Geocode') . '</button></li>';
+} else if ($user->toolbar_style === 'icon') {
+    $latlong = '<li class="nav-item" role="presentation"><button type="button" id="locations_latlong" name="locations_latlong" class="locations_latlong btn btn-default"><span style="margin-right:6px;" class="fa-solid fa-globe text-success"></span></button></li>';
+
+    $geocode = '<li class="nav-item" role="presentation"><button type="button" id="locations_geocode" name="locations_geocode" class="locations_geocode btn btn-default"><span style="margin-right:6px;" class="fa-solid fa-map text-success"></span></button></li>';
+} else {
+    $latlong = '<li class="nav-item" role="presentation"><button type="button" id="locations_latlong" name="locations_latlong" class="locations_latlong btn btn-default">' . __('Get Lat/Long') . '</button></li>';
+
+    $geocode = '<li class="nav-item" role="presentation"><button type="button" id="locations_geocode" name="locations_geocode" class="locations_geocode btn btn-default">' . __('Create Geocode') . '</button></li>';
+}
 ?>
         <main class="container-fluid">
             <div class="card">
@@ -9,19 +28,28 @@ include 'shared/read_functions.php';
                     <?= read_card_header($meta->collection, $meta->id, $meta->icon, $user, $resource->name) ?>
                 </div>
                 <div class="card-body">
+                    <div class="row text-center">
+                        <div class="col-4 offset-4">
+                            <ul class="nav nav-pills nav-fill" id="myTab" role="tablist">
+                                <?= $latlong ?>
+                                <?= $geocode ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <br/>
                     <div class="row">
                         <div class="col-4">
                             <?= read_field('name', $resource->name, $dictionary->columns->name, $update) ?>
                             <?= read_select('org_id', $resource->org_id, $dictionary->columns->org_id, $update, __('Organisation'), $orgs) ?>
                             <?= read_field('description', $resource->description, $dictionary->columns->description, $update) ?>
                             <?= read_select('type', $resource->type, $dictionary->columns->type, $update, __('Type'), $included['types']) ?>
-                            <?php $attributes = array('room', 'suite', 'level', 'address');
+                            <?php $attributes = array('room', 'suite', 'level', 'address', 'suburb', 'district', 'region', 'area');
                             foreach ($attributes as $attribute) {
                                 echo read_field($attribute, $resource->{$attribute}, $dictionary->columns->{$attribute}, $update) . "\n";
                             } ?>
                         </div>
                         <div class="col-4">
-                            <?php $attributes = array('city', 'state', 'postcode', 'country', 'phone', 'latitude', 'longitude');
+                            <?php $attributes = array('latitude', 'longitude', 'geo', 'city', 'state', 'postcode', 'country', 'phone', 'external_ident', 'geo');
                             foreach ($attributes as $attribute) {
                                 echo read_field($attribute, $resource->{$attribute}, $dictionary->columns->{$attribute}, $update) . "\n";
                             } ?>
@@ -31,6 +59,14 @@ include 'shared/read_functions.php';
                         <div class="col-4">
                             <br>
                             <div class="offset-2 col-8">
+                                <?php if ($disabled === 'disabled') { ?>
+                                <div class="container-fluid">
+                                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                        <?= __('No Google Maps API Key present, you will not be able to display the map') ?>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                </div>
+                                <?php } ?>
                                 <?php if (!empty($dictionary->about)) { ?>
                                     <h4 class="text-center"><?= __('About') ?></h4><br>
                                     <?= $dictionary->about ?>
@@ -52,3 +88,83 @@ include 'shared/read_functions.php';
                 </div>
             </div>
         </main>
+
+<?php if ($disabled === '') { ?>
+<script src="https://maps.google.com/maps/api/js?key=<?= config('Openaudit')->maps_api_key ?>"></script>
+
+<script>
+window.onload = function () {
+    /* Get Lat/Long from Google Maps API on update locations page */
+    $(document).ready(function () {
+        $(document).on('click', '.locations_latlong', function (e) {
+            var address = "";
+            if ($('#geo').val() > "") {
+                address = $('#geo').val();
+            } else {
+                address = $('#address').val() + ", " + $('#city').val() + ", " + $('#state').val() + ", " + $('#postcode').val() + ", " + $('#country').val();
+            }
+            $('#latitude').val('');
+            $('#longitude').val('');
+            var geocoder = new google.maps.Geocoder();
+            if (geocoder) {
+                geocoder.geocode({'address': address}, function (results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        $('#longitude').val(results[0].geometry.location.lng());
+                        $('.edit*[data-attribute="longitude"]').click();
+                        $('#latitude').val(results[0].geometry.location.lat());
+                        $('.edit*[data-attribute="latitude"]').click();
+                    }
+                });
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $(document).on('click', '.locations_geocode', function (e) {
+            console.log("Clicked locations geocode");
+            var address = $('#address').val() + ", " + $('#city').val() + ", " + $('#state').val() + ", " + $('#postcode').val() + ", " + $('#country').val();
+            console.log(address);
+            $('#geo').val('');
+            // $('#geo').attr("disabled", false);
+            $('#geo').val(address);
+            $('.edit*[data-attribute="geo"]').click();
+
+        });
+    });
+}
+</script>
+<?php } ?>
+
+<?php if ($disabled === 'disabled') { ?>
+<script>
+window.onload = function () {
+    /* Get Lat/Long from Google Maps API on update locations page */
+    $(document).ready(function () {
+        header = "<?= __('License Required') ?>";
+        body = "<?= __('This feature requires a Professional or an Enterprise license.') ?>";
+        <?php if (empty(config('Openaudit')->maps_api_key)) { ?>
+            header = "<?= __('API Key required.') ?>";
+            body = "<?= __('A Google Maps API Key is required for this function.') ?>";
+        <?php } ?>
+        $(document).on('click', '.locations_latlong', function (e) {
+            $("#liveToastFailure-header").text(header);
+            $("#liveToastFailure-body").text(body);
+            var toastElList = [].slice.call(document.querySelectorAll('.toast-failure'));
+            var toastList = toastElList.map(function(toastEl) {
+                return new bootstrap.Toast(toastEl)
+            });
+            toastList.forEach(toast => toast.show());
+        });
+        $(document).on('click', '.locations_geocode', function (e) {
+            $("#liveToastFailure-header").text(header);
+            $("#liveToastFailure-body").text(body);
+            var toastElList = [].slice.call(document.querySelectorAll('.toast-failure'));
+            var toastList = toastElList.map(function(toastEl) {
+                return new bootstrap.Toast(toastEl)
+            });
+            toastList.forEach(toast => toast.show());
+        });
+    });
+}
+</script>
+<?php } ?>
