9258f1dfc0ea80a85bb64784ca60e2e051382f81
Mark Unwin <marku@opmantek.com>
2023-09-21 12:57:01 +1000

    Make Routes.php more closely match the shipped latest version of that file.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,27 +1,12 @@
 <?php
 
-namespace Config;
-
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
+use CodeIgniter\Router\RouteCollection;
 
 // Load the system's routing file first, so that the app and ENVIRONMENT
 // can override as needed.
 if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
     require SYSTEMPATH . 'Config/Routes.php';
 }
-
-/*
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
- */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-$routes->setAutoRoute(false);
 
 /*
  * --------------------------------------------------------------------
