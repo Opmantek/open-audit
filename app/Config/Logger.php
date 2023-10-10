b8d80b9e73f915abdf872783e1456ff300e220cc
Mark Unwin <marku@opmantek.com>
2023-10-11 09:20:03 +1000

    Log at level 7 by default.

--- app/Config/Logger.php
+++ app/Config/Logger.php
@@ -38,7 +38,7 @@ class Logger extends BaseConfig
      *
      * @var array|int
      */
-    public $threshold = (ENVIRONMENT === 'production') ? 4 : 9;
+    public $threshold = (ENVIRONMENT === 'production') ? 7 : 9;
 
     /**
      * --------------------------------------------------------------------------
