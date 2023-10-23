98684d4a558bb27a2a47465256b7cbb5c9b2be85
Mark Unwin <marku@opmantek.com>
2023-10-23 13:32:26 +1000

    Fix license header on issue_3 template.

--- app/Views/discovery_issues/issue_3.php
+++ app/Views/discovery_issues/issue_3.php
@@ -1,39 +1,7 @@
-%# VERSION=1.628.0
-<%
-#
-# THIS SOFTWARE IS NOT PART OF Open-AudIT AND IS COPYRIGHTED, PROTECTED AND LICENSED
-# BY FIRSTWAVE.
-#
-# YOU MUST NOT MODIFY OR DISTRIBUTE THIS CODE
-#
-# This code is NOT Open Source
-#
-# IT IS IMPORTANT THAT YOU HAVE READ CAREFULLY AND UNDERSTOOD THE END USER
-# LICENSE AGREEMENT THAT WAS SUPPLIED WITH THIS SOFTWARE.   BY USING THE
-# SOFTWARE  YOU ACKNOWLEDGE THAT (1) YOU HAVE READ AND REVIEWED THE LICENSE
-# AGREEMENT IN ITS ENTIRETY, (2) YOU AGREE TO BE BOUND BY THE AGREEMENT, (3)
-# THE INDIVIDUAL USING THE SOFTWARE HAS THE POWER, AUTHORITY AND LEGAL RIGHT
-# TO ENTER INTO THIS AGREEMENT ON BEHALF OF YOU (AS AN INDIVIDUAL IF ON YOUR
-# OWN BEHALF OR FOR THE ENTITY THAT EMPLOYS YOU )) AND, (4) BY SUCH USE, THIS
-# AGREEMENT CONSTITUTES BINDING AND ENFORCEABLE OBLIGATION BETWEEN YOU AND
-# FIRSTWAVE.
-#
-# Firstwave is a passionate, committed open source software company - we really
-# are.  This particular piece of code was taken from a commercial module and
-# thus we can't legally supply it under GPL. It is supplied in good faith as
-# source code so you can get more out of Open-AudIT.  According to the license
-# agreement you can not modify or distribute this code, but please let us know
-# if you want to and we will certainly help - in most cases just by emailing
-# you a different agreement that better suits what you want to do but covers
-# Firstwave legally too.
-#
-# contact Firstwave by emailing code@opmantek.com
-#
-# All licenses for all software obtained from Firstwave (GPL and commercial)
-# are viewable at https://firstwave.com/legal/licensing/
-#
-# *****************************************************************************
-%>
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+?>
 <h1>ERROR: Failed to save ADMIN$/winexesvc.exe - NT_STATUS_ACCESS_DENIED</h1>
 <br />
 <p>When Open-AudIT on Linux   discovers a Windows machine, in order to execute a remote command we use <a href="https://sourceforge.net/p/winexe/wiki/Home/">Winexe</a>. This code relys on the great work done by the <a hre="https://www.samba.org/">Samba</a> team.</p>
