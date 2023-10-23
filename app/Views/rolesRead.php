4a016a967664f07bb8cd70a8a48f3ce2a0d2e661
Mark Unwin <marku@opmantek.com>
2023-10-23 17:12:10 +1000

    Make the roles template use tabs.

--- app/Views/rolesRead.php
+++ app/Views/rolesRead.php
@@ -5,6 +5,17 @@ include 'shared/read_functions.php';
 $permissions = array('c','r','u','d');
 $endpoints = array('applications','attributes','baselines','clouds','clusters','components','configuration','connections','credentials','dashboards','database','devices','discoveries','discovery_scan_options','fields','files','groups','integrations','ldap_servers','licenses','locations','logs','networks','orgs','queries','racks','rack_devices','roles','rules','scripts','search','summaries','tasks','users','widgets');
 $item_permissions = $resource->permissions;
+$style = @$user->toolbar_style;
+if ($style === 'icontext') {
+    $details_button = '<li class="nav-item" role="presentation"><a href="#details" class="nav-link" id="details-tab"><span style="margin-right:6px;" class="fa fa-eye text-success"></span>' . __('Details') . '</a></li>';
+    $permissions_button = '<li class="nav-item" role="presentation"><a href="#permissions" class="nav-link" id="permissions-tab"><span style="margin-right:6px;" class="fa fa-user-shield text-primary" ></span>' . __('Permissions') . '</a></li>';
+} else if ($style === 'icon') {
+    $details_button = '<li class="nav-item" role="presentation"><a href="#details" class="nav-link" id="details-tab"><span style="margin-right:6px;" title="' . __('Details') . '" class="fa fa-eye text-success"></span></a></li>';
+    $permissions_button = '<li class="nav-item" role="presentation"><a href="#permissions" class="nav-link" id="permissions-tab"><span style="margin-right:6px;" title="' . __('Permissions') . '" class="fa fa-user-shield text-primary"></span></a></li>';
+} else {
+    $details_button = '<li class="nav-item" role="presentation"><a href="#details" class="nav-link" id="details-tab">' . __('Details') . '</a></li>';
+    $permissions_button = '<li class="nav-item" role="presentation"><a href="#permissions" class="nav-link" id="permissions-tab">' . __('Permissions') . '</a></li>';
+}
 ?>
         <main class="container-fluid">
             <div class="card">
@@ -12,94 +23,114 @@ $item_permissions = $resource->permissions;
                     <?= read_card_header($meta->collection, $meta->id, $meta->icon, $user, $resource->name) ?>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-6">
-                            <?= read_field('name', $resource->name, $dictionary->columns->name, $update) ?>
-                            <?= read_field('description', $resource->description, $dictionary->columns->description, $update) ?>
-                            <?= read_field('ad_group', $resource->ad_group, $dictionary->columns->ad_group, $update, __('AD Group')) ?>
-                            <?= read_field('edited_by', $resource->edited_by, $dictionary->columns->edited_by, false) ?>
-                            <?= read_field('edited_date', $resource->edited_date, $dictionary->columns->edited_date, false) ?>
-                        </div>
 
-                        <div class="col-6">
-                            <br>
-                            <div class="offset-2 col-8">
-                                <?php if (!empty($dictionary->about)) { ?>
-                                    <h4 class="text-center"><?= __('About') ?></h4><br>
-                                    <?= $dictionary->about ?>
-                                <?php } ?>
-                                <?php if (!empty($dictionary->notes)) { ?>
-                                    <h4 class="text-center"><?= __('Notes') ?></h4><br>
-                                    <?= $dictionary->notes ?>
-                                <?php } ?>
-                                <?php if (!empty($dictionary->columns)) { ?>
-                                    <?php $fields = array('name', 'description', 'ad_group', 'edited_by', 'edited_date') ?>
-                                <h4 class="text-center"><?= __('Fields') ?></h4><br>
-                                    <?php foreach ($fields as $key) { ?>
-                                    <code><?= $key ?>: </code><?= $dictionary->columns->{$key} ?><br><br>
-                                    <?php } ?>
-                                <?php } ?>
+                    <div class="row text-center">
+                        <div class="col-8 offset-2" style="background-color: rgba(var(--bs-body-color-rgb), 0.03);">
+                            <ul class="nav nav-pills nav-fill" id="myTab" role="tablist">
+                                <?= $details_button ?>
+                                <?= $permissions_button ?>
+                            </ul>
+                        </div>
+                    </div>
+                    <br/>
+
+                    <div class="tab-content">
+                        <div class="tab-pane" id="details" role="tabpanel" tabindex="0" aria-labelledby="details">
+                            <div class="row">
+                                <div class="col-6">
+                                    <?= read_field('name', $resource->name, $dictionary->columns->name, $update) ?>
+                                    <?= read_field('description', $resource->description, $dictionary->columns->description, $update) ?>
+                                    <?= read_field('ad_group', $resource->ad_group, $dictionary->columns->ad_group, $update, __('AD Group')) ?>
+                                    <?= read_field('edited_by', $resource->edited_by, $dictionary->columns->edited_by, false) ?>
+                                    <?= read_field('edited_date', $resource->edited_date, $dictionary->columns->edited_date, false) ?>
+                                </div>
+
+                                <div class="col-6">
+                                    <br>
+                                    <div class="offset-2 col-8">
+                                        <?php if (!empty($dictionary->about)) { ?>
+                                            <h4 class="text-center"><?= __('About') ?></h4><br>
+                                            <?= $dictionary->about ?>
+                                        <?php } ?>
+                                        <?php if (!empty($dictionary->notes)) { ?>
+                                            <h4 class="text-center"><?= __('Notes') ?></h4><br>
+                                            <?= $dictionary->notes ?>
+                                        <?php } ?>
+                                        <?php if (!empty($dictionary->columns)) { ?>
+                                            <?php $fields = array('name', 'description', 'ad_group', 'edited_by', 'edited_date') ?>
+                                        <h4 class="text-center"><?= __('Fields') ?></h4><br>
+                                            <?php foreach ($fields as $key) { ?>
+                                            <code><?= $key ?>: </code><?= $dictionary->columns->{$key} ?><br><br>
+                                            <?php } ?>
+                                        <?php } ?>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <br>
-        </main>
 
-
-        <div class="container-fluid">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row" style="height:46px;">
-                        <div class="col-12 clearfix">
-                            <h6 style="padding-top:10px;"><?= __('Role Permissions') ?></h6>
+                    <div class="tab-content">
+                                <div class="tab-pane" id="permissions" role="tabpanel" tabindex="0" aria-labelledby="permissions">
+                            <table class="table <?= $GLOBALS['table'] ?> table-striped dataTable" id="table_result">
+                                <thead>
+                                    <tr>
+                                        <th>Endpoint</th>
+                                        <th class="text-center"><?= __('Create') ?>&nbsp;<input type="checkbox" name="select_c" id="select_c"/></th>
+                                        <th class="text-center"><?= __('Read') ?>&nbsp;<input type="checkbox" name="select_c" id="select_r"/></th>
+                                        <th class="text-center"><?= __('Update') ?>&nbsp;<input type="checkbox" name="select_c" id="select_u"/></th>
+                                        <th class="text-center"><?= __('Delete') ?>&nbsp;<input type="checkbox" name="select_c" id="select_d"/></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php foreach ($endpoints as $endpoint) { ?>
+                                    <tr><td><?= $endpoint ?></td>
+                                        <?php foreach ($permissions as $permission) {
+                                            $checked = '';
+                                            if (!empty($item_permissions->{$endpoint}) and strpos($item_permissions->{$endpoint}, $permission) !== false) {
+                                                $checked = 'checked';
+                                            } else {
+                                                $checked = '';
+                                            }
+                                            $disabled = 'disabled';
+                                            if ($update) {
+                                                $disabled = '';
+                                            } ?>
+                                        <td class="text-center"><input data-permission="<?= $permission ?>" name="permission.<?= $endpoint ?>" type="checkbox" value="<?= $permission ?>" <?= $checked ?> <?= $disabled ?>></td>
+                                        <?php } ?>
+                                    </tr>
+                                <?php } ?>
+                                </tbody>
+                            </table>
+                            <?php if ($update) { ?>
+                                <button id="send" name="send" type="button" class="btn btn-primary"><?= __('Submit') ?></button>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-                    <table class="table <?= $GLOBALS['table'] ?> table-striped dataTable" id="table_result">
-                        <thead>
-                            <tr>
-                                <th>Endpoint</th>
-                                <th class="text-center"><?= __('Create') ?>&nbsp;<input type="checkbox" name="select_c" id="select_c"/></th>
-                                <th class="text-center"><?= __('Read') ?>&nbsp;<input type="checkbox" name="select_c" id="select_r"/></th>
-                                <th class="text-center"><?= __('Update') ?>&nbsp;<input type="checkbox" name="select_c" id="select_u"/></th>
-                                <th class="text-center"><?= __('Delete') ?>&nbsp;<input type="checkbox" name="select_c" id="select_d"/></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($endpoints as $endpoint) { ?>
-                            <tr><td><?= $endpoint ?></td>
-                                <?php foreach ($permissions as $permission) {
-                                    $checked = '';
-                                    if (!empty($item_permissions->{$endpoint}) and strpos($item_permissions->{$endpoint}, $permission) !== false) {
-                                        $checked = 'checked';
-                                    } else {
-                                        $checked = '';
-                                    }
-                                    $disabled = 'disabled';
-                                    if ($update) {
-                                        $disabled = '';
-                                    } ?>
-                                <td class="text-center"><input data-permission="<?= $permission ?>" name="permission.<?= $endpoint ?>" type="checkbox" value="<?= $permission ?>" <?= $checked ?> <?= $disabled ?>></td>
-                                <?php } ?>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                    <?php if ($update) { ?>
-                        <button id="send" name="send" type="button" class="btn btn-primary"><?= __('Submit') ?></button>
-                    <?php } ?>
-                </div>
             </div>
-        </div>
+        </main>
 
 
 
 <script {csp-script-nonce}>
 window.onload = function () {
     $(document).ready(function() {
+
+        var hash = window.location.hash;
+        if (hash == "") {
+            hash = "#details"
+        }
+        hash && $('ul.nav.nav-pills a[href="' + hash + '"]').tab('show');
+
+        $('ul.nav.nav-pills a').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+            window.location.hash = this.hash;
+        });
+
+        $(".nav-link").click(function(e) {
+            window.scrollTo(0, 0);
+        });
 
         $("#select_c").click( function() {
             $(':checkbox').each(
