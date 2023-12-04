b308c59a4215ff4af7558b75c2da6d2324f13b6a
Mark Unwin <marku@opmantek.com>
2023-12-04 15:54:07 +1000

    In DiscoveryLogModel, ALWAYS grab a new timestamp from MySQL for each log. This is a change from using the PHP timestamp and removes the need to set 'timezone' in the app code.

--- app/Models/DiscoveryLogModel.php
+++ app/Models/DiscoveryLogModel.php
@@ -136,14 +136,8 @@ class DiscoveryLogModel extends BaseModel
         $data->command_status = (!empty($data->command_status)) ? $data->command_status : '';
         $data->command_time_to_execute = (!empty($data->command_time_to_execute)) ? $data->command_time_to_execute : 0;
         $data->command_output = (!empty($data->command_output)) ? $data->command_output : '';
-        if (empty($data->timestamp)) {
-            $data->timestamp = date('Y-m-d H:i:s');
-            #log_message('debug', 'timetamp is empty, log is: ' . $data->message . ' :: ' . $data->command);
-        } else {
-            #log_message('debug', 'timetamp is NOT empty, log is: ' . $data->message . ' :: ' . $data->command);
-        }
-        // NOTE - Not using the above as it causes timestamp -> id order issues. May affect Collectors?
-        $data->timestamp = date('Y-m-d H:i:s');
+        $sql = "SELECT NOW() as `timestamp`";
+        $data->timestamp = $this->db->query($sql)->getResult()[0]->timestamp;
 
         if (!empty($data->message) and stripos($data->message, 'Collector - Starting discovery') !== false and !empty($data->discovery_id)) {
             // Special clear of local discovery logs if start of a Collector discovery
