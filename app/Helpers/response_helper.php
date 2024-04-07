35b4065420b85aa4f2f9051c293c68293a27abbd
Mark Unwin <marku@opmantek.com>
2024-04-08 09:47:18 +1000

    In response_helper, remove not required baselinespolicies -> baselines_policies collection. Add some TODO's.

--- app/Helpers/response_helper.php
+++ app/Helpers/response_helper.php
@@ -80,10 +80,6 @@ if (!function_exists('response_create')) {
             $request->header('Accept')
         );
 
-        if ($response->meta->collection === 'baselinesresults') {
-            $response->meta->collection = 'baselines_results';
-        }
-
         if ($response->meta->collection === 'collections') {
             $response->meta->collection = strtolower(html_entity_decode(urldecode($uri->getSegment(1))));
             if (empty($response->meta->collection)) {
@@ -863,7 +859,9 @@ if (!function_exists('response_get_id')) {
                 return null;
             }
             $db = db_connect();
+            // TODO - have we already validate the action before calling this function? If so, remove
             $actions = response_valid_actions();
+            // TODO - have we already validate the collection before calling this function? If so, remove
             $collections = response_valid_collections();
             $no_org_id = array('chart', 'configuration', 'reports', 'roles');
             if (!in_array($id, $actions)) {
@@ -877,6 +875,7 @@ if (!function_exists('response_get_id')) {
                         log_message('debug', "ID to Name match in database (Provided ID: $id).");
                     }
                 } else if ($collection === 'baselines_policies') {
+                    // TODO - We have org_id in baselines_policies - remove this
                     // baselines_policies.baseline_id -> baselines.id -> baselines.org_id
                     $sql = "SELECT baselines_policies.id FROM baselines_policies LEFT JOIN baselines ON (baselines_policies.baseline_id = baselines.id) WHERE baselines_policies.name LIKE ? AND baselines.org_id IN ({$org_list}) ORDER BY id DESC LIMIT 1";
                     $result = $db->query($sql, [$id])->getResult();
@@ -888,6 +887,7 @@ if (!function_exists('response_get_id')) {
                         $id = null;
                     }
                 } else if ($collection === 'collectors') {
+                    // TODO - remove this
                     // $sql = "SELECT id FROM collectors WHERE uuid = ? ORDER BY id DESC LIMIT 1";
                     // $result = $db->query($sql, [$id])->getResult();
                     // if (!empty($result)) {
@@ -898,6 +898,7 @@ if (!function_exists('response_get_id')) {
                     //     $id = null;
                     // }
                 } else if ($collection === 'devices') {
+                    // TODO - remove this
                     // devices
                     $sql = "SELECT id FROM devices WHERE name LIKE ? AND org_id IN ({$org_list}) ORDER BY id DESC LIMIT 1";
                     $result = $db->query($sql, [$id])->getResult();
