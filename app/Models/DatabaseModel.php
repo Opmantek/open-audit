82243a23eaa10e03561708dc5207940ca2ddacab
Mark Unwin <marku@opmantek.com>
2024-05-02 10:45:26 +1000

    Upgrade schema and version to 5.2.1.

--- app/Models/DatabaseModel.php
+++ app/Models/DatabaseModel.php
@@ -341,6 +341,10 @@ class DatabaseModel extends BaseModel
             include "db_upgrades/db_5.2.0.php";
         }
 
+        if (intval(config('Openaudit')->internal_version) < 20240620) {
+            include "db_upgrades/db_5.2.1.php";
+        }
+
         $instance = & get_instance();
         $instance->data = $output;
         return true;
