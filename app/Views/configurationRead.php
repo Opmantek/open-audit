85219fa4f01c869cb7d4314f277a5051d45ae764
Mark Unwin <mark.unwin@gmail.com>
2024-08-02 10:56:03 +1000

    Fix ConfigurationRead for any fields named *password* to correct set the placeholder.

--- app/Views/configurationRead.php
+++ app/Views/configurationRead.php
@@ -27,7 +27,11 @@ if ($resource->name === 'feature_agents_advanced') {
                             <?= read_field('editable', $resource->editable, $dictionary->columns->editable, false) ?>
                             <?= read_field('type', $resource->type, $dictionary->columns->type, false) ?>
                             <?php if ($resource->type !== 'bool') { ?>
-                                <?= read_field('value', html_entity_decode($resource->value), $dictionary->columns->value, $update, '', '', '', $resource->type) ?>
+                                <?php if (strpos($resource->name, 'password') === false) { ?>
+                                    <?= read_field('value', html_entity_decode($resource->value), $dictionary->columns->value, $update, '', '', '', $resource->type) ?>
+                                <?php } else { ?>
+                                    <?= read_field('value', '', $dictionary->columns->value, $update, '', '', '', 'password') ?>
+                                <?php } ?>
                             <?php } ?>
                             <?php if ($resource->type === 'bool') { ?>
                                 <?= read_select('value', $resource->value, $dictionary->columns->value, $update, '', array()) ?>
@@ -65,6 +69,15 @@ window.onload = function () {
     $(document).ready(function() {
         $("#button_create").remove();
         $("#button_delete").remove();
+
+        <?php if (strpos($resource->name, 'password') !== false) {
+            if ($resource->value !== '') { ?>
+                $("#value").attr("placeholder", "<?= __("removed from display, but has been set") ?>");
+            <?php } else { ?>
+                $("#value").attr("placeholder", "<?= __("has not been set") ?>");
+            <?php }
+        } ?>
+
     });
 }
 </script>
