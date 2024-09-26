53a97e65c36c357aa173620111184d2d0ebd43b0
Mark Unwin <mark.unwin@gmail.com>
2024-09-26 12:48:32 +1000

    Additional SNMP OIDs for Dell PowerConnect serial and service tag (stored in manufacturer_code).

--- app/Helpers/snmp_674_helper.php
+++ app/Helpers/snmp_674_helper.php
@@ -20,6 +20,10 @@ $get_oid_details = function ($ip, $credentials, $oid) {
     if (empty($details->serial)) {
         $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.2.1.0");
     }
+    if (empty($details->serial)) {
+        $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.2");
+    }
+    $details->manufacturer_code = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.4");
     $details->os_name = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.6.0");
     $details->os_version = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.14.0");
     $temp = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.1.4");
