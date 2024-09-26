002d8a04deb65851ac932e493006462296b02892
Mark Unwin <mark.unwin@gmail.com>
2024-09-26 14:50:47 +1000

    Additional info for Dell PowerConnect switches (serial, service_tag, os_version).

--- app/Helpers/snmp_674_helper.php
+++ app/Helpers/snmp_674_helper.php
@@ -13,6 +13,7 @@ $get_oid_details = function ($ip, $credentials, $oid) {
     if (empty($details->model)) {
         $details->model = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.12.0");
     }
+
     $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.2.1.1.11.0");
     if (empty($details->serial)) {
         $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.2.0");
@@ -21,14 +22,38 @@ $get_oid_details = function ($ip, $credentials, $oid) {
         $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.2.1.0");
     }
     if (empty($details->serial)) {
-        $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.2");
+        $details->serial = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.2.1");
     }
-    $details->manufacturer_code = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.4");
-    $details->os_name = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.6.0");
-    $details->os_version = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.14.0");
+    $details->service_tag = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10895.3000.1.2.100.8.1.4.1");
+    if (empty($details->service_tag)) {
+        unset($details->service_tag);
+    }
+
     $temp = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.1.4");
     if (!empty($temp)) {
         $details->manufacturer = $temp;
     }
+
+    $details->os_version = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.14.0");
+    if (empty($details->os_version)) {
+        $details->os_version = my_snmp_get($ip, $credentials, '1.3.6.1.2.1.47.1.1.1.1.10.2');
+    }
+    if (empty($details->os_version)) {
+        $details->os_version = my_snmp_get($ip, $credentials, '1.3.6.1.2.1.47.1.1.1.1.10.1');
+    }
+    if (empty($details->os_version)) {
+        # PowerConnect 6248, 3.3.18.1, VxWorks 6.5
+        $sysDescr = my_snmp_get($ip, $credentials, '1.3.6.1.2.1.1.1.0');
+        if (stripos($sysDescr, 'VXWorks') !== false and substr_count($sysDescr, ', ') === 2) {
+            $split = explode(',', $sysDescr);
+            $details->os_version = trim($split[1]);
+        }
+    }
+    if (empty($details->os_version)) {
+        unset($details->os_version);
+    }
+
+    $details->os_name = my_snmp_get($ip, $credentials, "1.3.6.1.4.1.674.10892.5.1.3.6.0");
+
     return($details);
 };
