fd74717455fbff37569c21b352775c45f5d1fcac
Mark Unwin <mark.unwin@gmail.com>
2024-10-04 11:29:42 +1000

    Minor improvements to queriesExecute template.

--- app/Views/queriesExecute.php
+++ app/Views/queriesExecute.php
@@ -15,10 +15,14 @@ include 'shared/read_functions.php';
                         <thead>
                             <tr>
                                 <?php foreach ($data[0]->attributes as $key => $value) {
-                                    if (strrpos($key, 'ip_padded') === strlen($key)-9) {
+                                    if (strrpos($key, 'ip_padded') === strlen($key) - 9) {
                                         continue;
                                     }
-                                    if (strpos($key, '.id') !== false) {
+                                    if ($key === 'devices.icon') {
+                                        echo '<th class="text-center">' . __('Icon') . "</th>\n";
+                                    } elseif ($key === 'devices.id') {
+                                        echo '<th class="text-center">' . __('Details') . "</th>\n";
+                                    } elseif (strpos($key, '.id') !== false) {
                                         echo '<th class="text-center">' . collection_column_name($key) . "</th>\n";
                                     } else {
                                         echo '<th>' . collection_column_name($key) . "</th>\n";
@@ -31,7 +35,7 @@ include 'shared/read_functions.php';
                                 foreach ($data as $item) {
                                     echo "<tr>\n";
                                     foreach ($data[0]->attributes as $key => $value) {
-                                        if (strrpos($key, 'ip_padded') === strlen($key)-9) {
+                                        if (strrpos($key, 'ip_padded') === strlen($key) - 9) {
                                             continue;
                                         }
                                         if ($key === 'devices.id') {
@@ -41,13 +45,15 @@ include 'shared/read_functions.php';
                                             echo collection_button_read($temp[0], $item->attributes->{$key});
                                         } elseif ($key === 'link') {
                                             echo "<td><a href=\"" . base_url() . 'index.php/' . $item->attributes->{$key} . "\" role=\"button\" class=\"btn btn-sm btn-primary\"><span style=\"width:1rem;\" title=\"" . __('View') . "\" class=\"fa fa-eye\" aria-hidden=\"true\"></span></td>";
-                                        } elseif ((strrpos($key, 'ip') === strlen($key)-2)) {
+                                        } elseif ((strrpos($key, 'ip') === strlen($key) - 2)) {
                                             $padded_key = substr($key, 0, strpos($key, 'ip')) . 'ip_padded';
                                             if (!empty($item->attributes->{$padded_key})) {
                                                 echo '            <td><span style="display:none;">' . $item->attributes->{$padded_key} . '</span>' . $item->attributes->{$key} . "</td>\n";
                                             } else {
                                                 echo "<td>" . $item->attributes->{$key} . "</td>\n";
                                             }
+                                        } elseif ($key === 'devices.icon') {
+                                            echo "<td class=\"text-center\"><img style=\"width:40px\" src=\"" . base_url() . "device_images/" . $item->attributes->{$key} . ".svg\" alt=\"\"></td>\n";
                                         } else {
                                             // echo "<td>" . $item->attributes->{$key} . "</td>\n";
                                             echo "<td>" . nl2br($item->attributes->{$key}) . "</td>\n";
