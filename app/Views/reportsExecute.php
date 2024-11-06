46aebc82412985f9c7bc903bf329ea99b3e06882
Mark Unwin <mark.unwin@gmail.com>
2024-11-07 09:54:28 +1000

    Fix - Use correct link in reportsExecute template (to devices).

--- app/Views/reportsExecute.php
+++ app/Views/reportsExecute.php
@@ -54,7 +54,7 @@ include 'shared/read_functions.php';
                                                 <span class="<?= $item->attributes->audit_class ?>" title="<?= $item->attributes->audit_text ?>"></span>
                                             </td>
                                         <?php }
-                                        echo "                                    " . collection_button_read($meta->collection, $item->id) . "\n";
+                                        echo "                                    " . collection_button_read('devices', $item->id) . "\n";
                                         foreach ($meta->data_order as $key) {
                                             $key = str_replace('devices.', '', $key);
                                             if ($key === 'id' or $key === 'orgs.id') {
