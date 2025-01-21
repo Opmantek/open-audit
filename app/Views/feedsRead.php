ada33529779296c55424ff4af38d204f58e73cc5
Mark Unwin <mark.unwin@gmail.com>
2025-01-21 12:37:41 +1000

    feedsRead template changes.

--- app/Views/feedsRead.php
+++ app/Views/feedsRead.php
@@ -4,7 +4,11 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 include 'shared/read_functions.php';
 
-$name = !empty($data[0]->attributes->name) ? ucwords($data[0]->attributes->type) . ' :: ' . html_entity_decode($data[0]->attributes->name) : '';
+if ($data[0]->attributes->type !== 'advertisement') {
+    $name = !empty($data[0]->attributes->name) ? ucwords($data[0]->attributes->type) . ' :: ' . html_entity_decode($data[0]->attributes->name) : '';
+} else {
+    $name = html_entity_decode($data[0]->attributes->name);
+}
 $description = !empty($data[0]->attributes->description) ? html_entity_decode($data[0]->attributes->description) . '<br><br>' : '';
 $short = !empty($data[0]->attributes->short) ? html_entity_decode($data[0]->attributes->short) . '<br><br>' : '';
 $body = !empty($data[0]->attributes->body) ? $data[0]->attributes->body : '';
@@ -14,6 +18,14 @@ $actioned = !empty($data[0]->attributes->actioned) ? $data[0]->attributes->actio
 $link = !empty($data[0]->attributes->link) ? $data[0]->attributes->link : '';
 
 switch ($data[0]->attributes->type) {
+    case 'advertisement':
+        $body = htmlspecialchars_decode($data[0]->attributes->body);
+        $body = html_entity_decode($body);
+        $name .= "<span class=\"clearfix float-end\"><button id=\"myButton\" class=\"btn btn-success\">" . __('Download') . "</button>";
+        $name .= "<br><a id=\"link\" class=\"visually-hidden\" href=\"" . $link . "\" target=\"_blank\">" . $link . "</a>";
+        $actioned = '';
+        break;
+
     case 'config':
         if (strpos($user->permissions['configuration'], 'c') !== false and strpos($user->permissions['configuration'], 'u') !== false) {
             $name .= "<span class=\"clearfix float-end\"><button class=\"btn btn-primary\">" . __('Activate') . "</button>";
@@ -66,6 +78,7 @@ switch ($data[0]->attributes->type) {
                     </div>
                 </div>
             </div>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br>
             <pre>
                 <?= json_encode($data[0], JSON_PRETTY_PRINT) ?>
             </pre>
