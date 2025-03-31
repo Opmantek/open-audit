10032a5081b9b37053a9644aacc0ac84f24c98fa
Mark Unwin <mark.unwin@gmail.com>
2025-03-31 13:52:52 +1000

    task: news: Comment out any news code.

--- app/Helpers/response_helper.php
+++ app/Helpers/response_helper.php
@@ -430,23 +430,23 @@ if (!function_exists('response_create')) {
             $response->logs = $instance->response->logs;
         }
 
-        if (!empty($config->feature_news) and $config->feature_news === 'n' and in_array($response->meta->action, ['collection', 'execute']) and $response->meta->format === 'html') {
-            $reminder_days = (!empty($config->feature_news_remind_days)) ? intval($config->feature_news_remind_days) : 30;
-            $last_request_date = (!empty($config->feature_news_last_request_date)) ? strtotime("+" . $reminder_days . " days", strtotime($config->feature_news_last_request_date)) : strtotime('2001-01-01');
-            $today = strtotime(date('Y-m-d'));
-            if ($last_request_date < $today) {
-                $conf = model('ConfigurationModel');
-                $conf_data = $conf->readName('feature_news');
-                $conf_id = (!empty($conf_data[0]->id)) ? intval($conf_data[0]->id) : 0;
-                $_SESSION['success'] = 'Why not enable \'news\'? Keep up-to-date with the latest fixes, announcements, query updates, software versions and more. Click <a href="' . url_to('configurationRead', $conf_id) . '">here</a> to enable.';
-                $db = db_connect();
-                $sql = "UPDATE `configuration` SET `value` = ? WHERE `name` = 'feature_news_last_request_date'";
-                $result = $db->query($sql, [date('Y-m-d')]);
-                unset($conf);
-                unset($conf_data);
-                unset($conf_id);
-            }
-        }
+        // if (!empty($config->feature_news) and $config->feature_news === 'n' and in_array($response->meta->action, ['collection', 'execute']) and $response->meta->format === 'html') {
+        //     $reminder_days = (!empty($config->feature_news_remind_days)) ? intval($config->feature_news_remind_days) : 30;
+        //     $last_request_date = (!empty($config->feature_news_last_request_date)) ? strtotime("+" . $reminder_days . " days", strtotime($config->feature_news_last_request_date)) : strtotime('2001-01-01');
+        //     $today = strtotime(date('Y-m-d'));
+        //     if ($last_request_date < $today) {
+        //         $conf = model('ConfigurationModel');
+        //         $conf_data = $conf->readName('feature_news');
+        //         $conf_id = (!empty($conf_data[0]->id)) ? intval($conf_data[0]->id) : 0;
+        //         $_SESSION['success'] = 'Why not enable \'news\'? Keep up-to-date with the latest fixes, announcements, query updates, software versions and more. Click <a href="' . url_to('configurationRead', $conf_id) . '">here</a> to enable.';
+        //         $db = db_connect();
+        //         $sql = "UPDATE `configuration` SET `value` = ? WHERE `name` = 'feature_news_last_request_date'";
+        //         $result = $db->query($sql, [date('Y-m-d')]);
+        //         unset($conf);
+        //         unset($conf_data);
+        //         unset($conf_id);
+        //     }
+        // }
 
         # Enterprise
         $license_config = false;
