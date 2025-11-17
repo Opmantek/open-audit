949d58599984188c9ae6efcd86c288460facc38b
Mark Unwin <mark.unwin@gmail.com>
2025-11-17 13:40:34 +1000

    task: vulnerabilities: Remove Create button (vendors) from collection and Execute button on read templates.

--- app/Views/shared/collection_functions.php
+++ app/Views/shared/collection_functions.php
@@ -127,13 +127,15 @@ function collection_card_header(string $collection = '', string $icon = '', ?obj
         $import_csv_button = "<button id=\"request_vulnerability\"  data-bs-toggle=\"modal\" data-bs-target=\"#requestVulnerabilityModal\" style=\"margin-right:6px;\" role=\"button\" class=\"btn btn-light mb-2\" title=\"" . __('Request Vulnerability') . "\"><span class=\"icon-rss text-oa-primary\"></span>" . __('Request Vulnerability') . "</button>";
 
 
-        $create_button = "<a role=\"button\" href=\"" . url_to('vendorsCollection') . "\" id=\"vendors\" style=\"margin-right:6px;\" role=\"button\" class=\"btn btn-light mb-2\" title=\"" . __('Vendor Report') . "\"><span class=\"icon-store text-oa-primary\"></span>" . __('Vendor Report') . "</button>";
+        // $create_button = "<a role=\"button\" href=\"" . url_to('vendorsCollection') . "\" id=\"vendors\" style=\"margin-right:6px;\" role=\"button\" class=\"btn btn-light mb-2\" title=\"" . __('Vendor Report') . "\"><span class=\"icon-store text-oa-primary\"></span>" . __('Vendor Report') . "</button>";
+
+        $create_button = '';
 
     }
 
     $return = "<div class=\"row\">
                         <div class=\"col-3 clearfix\">
-                                <h6 style=\"padding-top:10px;\"><span class=\"{$icon} oa-icon\"></span>{$label}</h6>
+                                <h5 style=\"padding-top:10px;\"><span class=\"{$icon} oa-icon\"></span>{$label}</h5>
                         </div>
                         <div class=\"col-9 clearfix\">
                             <div class=\"btn-group btn-group-sm float-end\" role=\"group\" id=\"oa_panel_buttons\">
