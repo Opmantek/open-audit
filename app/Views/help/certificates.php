86659da6258fe27e68d10eb13492712d9cc9a17e
Mark Unwin <mark.unwin@gmail.com>
2025-11-25 22:42:17 +1000

    improve: certificates: Help page and image.

---
+++ app/Views/help/certificates.php
@@ -0,0 +1,7 @@
+<?php
+# Copyright © 2023 FirstWave. All Rights Reserved.
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+$intro = '<p>' . __('You can now select a certificate found during an audit and mark it to make management (renewal, responsibility, et al) easier. And obviously report on it. I’m thinking a report at the start of each month detailing which certificates will expire in the next 45 days, including who need to renew them (if they aren’t auto-renew). Easy to do in a couple of clicks.') . '<br><br></p>';
+
+$body = '<br>';
