ffbfdd3bf065b12073f2acdef7a20352b2624736
Mark Unwin <mark.unwin@gmail.com>
2025-12-11 14:51:23 +1000

    Fix widget link for Unknown Devices.

--- app/Models/db_upgrades/db_6.0.1.php
+++ app/Models/db_upgrades/db_6.0.1.php
@@ -8,22 +8,29 @@ $result = $db->query($sql);
 $output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
 log_message('info', (string)$db->getLastQuery());
 
+// Fix bad URL for link
+$sql = "UPDATE `widgets` SET `link` = 'devices?devices.type=in(\'unknown\',\'unclassified\')' WHERE `name` = 'Unknown Devices'";
+$result = $db->query($sql);
+$output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
+log_message('info', (string)$db->getLastQuery());
+
+// Set to daily between 12 and 4am
 $sql = "UPDATE `tasks` SET `minute` = FLOOR(RAND()*(55-5)+5), `hour` = FLOOR(RAND()*(3)+1) WHERE `name` = 'Vulnerability Retrieval'";
 $result = $db->query($sql);
 $output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
 log_message('info', (string)$db->getLastQuery());
 
+// Set daily after above, between 4 and 7 am
 $sql = "UPDATE `tasks` SET `minute` = FLOOR(RAND()*(55-5)+5), `hour` = FLOOR(RAND()*(3)+3) WHERE `name` = 'Run All Vulnerabilities'";
 $result = $db->query($sql);
 $output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
 log_message('info', (string)$db->getLastQuery());
 
+// Not used
 $sql = "DELETE FROM tasks WHERE name = 'Vendor Retrieval'";
 $result = $db->query($sql);
 $output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
 log_message('info', (string)$db->getLastQuery());
-
-
 
 // set our versions
 $sql = "UPDATE `configuration` SET `value` = '20251130' WHERE `name` = 'internal_version'";
