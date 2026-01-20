be35ed619851cf5445d55a1f04fcf4f8a83f5382
Mark Unwin <mark.unwin@gmail.com>
2026-01-20 11:00:46 +1000

    task: gui: Add PHP file for root redirect to open-audit.

---
+++ index-root.php
@@ -0,0 +1,11 @@
+<?php
+    if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+        $uri = 'https://';
+    } else {
+        $uri = 'http://';
+    }
+    $uri .= $_SERVER['HTTP_HOST'];
+    header('Location: '.$uri.'/open-audit/');
+    exit;
+?>
+Something is wrong with the Open-AudIT installation :-(
