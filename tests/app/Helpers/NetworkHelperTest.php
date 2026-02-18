ebdb6556811ba1474714faa992d88b406fb8fb1b
Adam Dyson <adam.dyson@firstwave.com>
2026-02-18 17:20:12 +1100

    improvement: testing: reorganise test case

--- tests/app/Helpers/NetworkHelperTest.php
+++ tests/app/Helpers/NetworkHelperTest.php
@@ -38,12 +38,36 @@ class NetworkHelperTest extends CIUnitTestCase
         $this->assertSame($expected, is_private_ip($ipAddress));
     }
 
+    public static function privateIpDataProvider(): array
+    {
+        return [
+            [true, '192.168.1.1'],
+            [true, '10.10.10.10'],
+            [false, '110.2.3.5'],
+            [false, '123'],
+            [false, 'abc'],
+            [false, 'bad.address.with.0'],
+        ];
+    }
+
     /**
      * @dataProvider ipAddressToDbDataProvider
      */
     public function testIpAddressToDb(string $expected, string $ipAddress): void
     {
         $this->assertSame($expected, ip_address_to_db($ipAddress));
+    }
+
+    public static function ipAddressToDbDataProvider(): array
+    {
+        return [
+            ['192.168.001.001', '192.168.1.1'],
+            ['010.010.010.010', '10.10.10.10'],
+            ['110.002.003.005', '110.2.3.5'],
+            ['123', '123'],
+            ['abc', 'abc'],
+            ['000.000.000.000', 'bad.address.with.0'],
+        ];
     }
 
     /**
@@ -54,12 +78,36 @@ class NetworkHelperTest extends CIUnitTestCase
         $this->assertSame($expected, ip_address_from_db($ipAddress));
     }
 
+    public static function ipAddressFromDbDataProvider(): array
+    {
+        return [
+            ['192.168.1.1', '192.168.001.001'],
+            ['10.10.10.10', '010.010.010.010'],
+            ['110.2.3.5', '110.002.003.005'],
+            ['123', '123'],
+            ['abc', 'abc'],
+            ['0.0.0.0', 'bad.address.with.0'],
+        ];
+    }
+
     /**
      * @dataProvider searchIpToDbDataProvider
      */
     public function testSearchIpAToDb(string $expected, string $ipAddress): void
     {
         $this->assertSame($expected, search_ip_to_db($ipAddress));
+    }
+
+    public static function searchIpToDbDataProvider(): array
+    {
+        return [
+            ['192.168.001.001', '192.168.1.1'],
+            ['010.010.010.010', '10.10.10.10'],
+            ['110.002.003.005', '110.2.3.5'],
+            ['', '123'],
+            ['', 'abc'],
+            ['bad.ess.ith.000', 'bad.address.with.0'],
+        ];
     }
 
     public function testIsSsl(): void
@@ -78,53 +126,5 @@ class NetworkHelperTest extends CIUnitTestCase
         $this->assertTrue(check_ip('::1'));
         $this->assertTrue(check_ip('2001:db8:0:0:0:ff00:42:8329'));
         $this->assertTrue(check_ip('foobar:8329')); // Probably not an expected outcome
-    }
-
-    public static function privateIpDataProvider(): array
-    {
-        return [
-            [true, '192.168.1.1'],
-            [true, '10.10.10.10'],
-            [false, '110.2.3.5'],
-            [false, '123'],
-            [false, 'abc'],
-            [false, 'bad.address.with.0'],
-        ];
-    }
-
-    public static function ipAddressToDbDataProvider(): array
-    {
-        return [
-            ['192.168.001.001', '192.168.1.1'],
-            ['010.010.010.010', '10.10.10.10'],
-            ['110.002.003.005', '110.2.3.5'],
-            ['123', '123'],
-            ['abc', 'abc'],
-            ['000.000.000.000', 'bad.address.with.0'],
-        ];
-    }
-
-    public static function ipAddressFromDbDataProvider(): array
-    {
-        return [
-            ['192.168.1.1', '192.168.001.001'],
-            ['10.10.10.10', '010.010.010.010'],
-            ['110.2.3.5', '110.002.003.005'],
-            ['123', '123'],
-            ['abc', 'abc'],
-            ['0.0.0.0', 'bad.address.with.0'],
-        ];
-    }
-
-    public static function searchIpToDbDataProvider(): array
-    {
-        return [
-            ['192.168.001.001', '192.168.1.1'],
-            ['010.010.010.010', '10.10.10.10'],
-            ['110.002.003.005', '110.2.3.5'],
-            ['', '123'],
-            ['', 'abc'],
-            ['bad.ess.ith.000', 'bad.address.with.0'],
-        ];
     }
 }
