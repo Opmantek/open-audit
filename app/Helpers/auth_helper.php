b82df2c5bcbfaf21d19f187d1961bca712e6f5d1
Mark Unwin <mark.unwin@gmail.com>
2026-02-19 08:00:55 +1000

    task: auth: Merge entra functions.

--- app/Helpers/auth_helper.php
+++ app/Helpers/auth_helper.php
@@ -5,6 +5,107 @@
 
 declare(strict_types=1);
 
+function entra_redirect(object $auth): string
+{
+    $session = \Config\Services::session();
+    $redir = (!empty($auth->redirect_uri)) ? $auth->redirect_uri : base_url() . 'index.php/logon/entra/auth';
+    $provider = new \TheNetworg\OAuth2\Client\Provider\Azure([
+        'clientId'          => $auth->client_ident,
+        'clientSecret'      => $auth->client_secret,
+        'redirectUri'       => $redir,
+        'scopes'            => ['openid'],
+        'defaultEndPointVersion' => '2.0'
+    ]);
+    $options = [
+        'scopes' => ['openid']
+    ];
+    $provider->defaultEndPointVersion = TheNetworg\OAuth2\Client\Provider\Azure::ENDPOINT_VERSION_2_0;
+    $provider->tenant = $auth->tenant;
+    log_message('debug', 'Requesting baseGraphUri');
+    $baseGraphUri = $provider->getRootMicrosoftGraphUri(null);
+    log_message('debug', 'baseGraphUri: ' . json_encode($baseGraphUri));
+    $provider->scope = 'openid profile email offline_access ' . $baseGraphUri . '/User.Read';
+    log_message('debug', 'Requesting getAuthorizationUrl.');
+    $authUrl = $provider->getAuthorizationUrl(['scope' => $provider->scope]);
+    log_message('debug', 'authUrl: ' . $authUrl);
+    log_message('debug', 'Requesting getState.');
+    $_SESSION['oauth2state'] = $provider->getState();
+    log_message('debug', 'oauth2state: ' . $_SESSION['oauth2state']);
+    $session->set('oauth2state', $_SESSION['oauth2state']);
+    return $authUrl;
+
+    #$authorizationUrl = $provider->getAuthorizationUrl(['scope' => $provider->scope]);
+
+    #$_SESSION['OAuth2.state'] = $provider->getState();
+}
+
+function entra_auth(object $auth, string $ip = ''): string
+{
+    $session = \Config\Services::session();
+    log_message('debug', json_encode($_GET));
+    if (empty($_GET['code']) or empty($_GET['state'])) {
+        log_message('error', 'Missing GET items for Entra from ' . $ip);
+        $session->set('oauth2state', '');
+        return site_url('logon');
+    }
+    if (($_GET['state'] !== $_SESSION['oauth2state']) and ($_GET['state'] !== $session->get('oauth2state'))) {
+        log_message('info', 'Invalid credentials from ' . $ip . ' using okta.');
+        $session->set('oauth2state', '');
+        return site_url('logon');
+    }
+    $redir = (!empty($auth->redirect_uri)) ? $auth->redirect_uri : base_url() . 'index.php/logon/entra/auth';
+    $provider = new \TheNetworg\OAuth2\Client\Provider\Azure([
+        'clientId'          => $auth->client_ident,
+        'clientSecret'      => $auth->client_secret,
+        'redirectUri'       => $redir,
+        'scopes'            => ['openid'],
+        'defaultEndPointVersion' => '2.0'
+    ]);
+    $provider->defaultEndPointVersion = TheNetworg\OAuth2\Client\Provider\Azure::ENDPOINT_VERSION_2_0;
+    $provider->tenant = $auth->tenant;
+    $baseGraphUri = $provider->getRootMicrosoftGraphUri(null);
+    $provider->scope = 'openid profile email offline_access ' . $baseGraphUri . '/User.Read';
+    log_message('debug', 'Requesting getAccessToken');
+    try {
+        $token = $provider->getAccessToken('authorization_code', [
+            'scope' => $provider->scope,
+            'code' => $_GET['code'],
+        ]);
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+        log_message('warning', 'Could not getAccessToken from Entra for ' . $ip . '. ' . $e->getMessage());
+        return site_url('logon');
+    }
+    try {
+        $user = $provider->getResourceOwner($token);
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+        log_message('warning', 'Could not getResourceOwner from Entra for ' . $ip . '. ' . $e->getMessage());
+        return site_url('logon');
+    }
+    // $me = $provider->get($provider->getRootMicrosoftGraphUri($token) . '/v1.0/me', $token);
+    // log_message('debug', 'Me: ' . json_encode($me, JSON_PRETTY_PRINT));
+    $user = $user->toArray();
+    log_message('debug', json_encode($user, JSON_PRETTY_PRINT));
+    $db = db_connect();
+    $sql = "SELECT * FROM users WHERE name = ? ORDER BY `id` LIMIT 1";
+    $users = $db->query($sql, [$user['preferred_username']])->getResult();
+    $dbuser = (!empty($users[0])) ? $users[0] : null;
+    if (!empty($dbuser->id)) {
+        if (empty($dbuser->full_name) and !empty($user['name'])) {
+            $sql = "UPDATE users SET full_name = ? WHERE id = ?";
+            $db->query($sql, [$user['name'], $dbuser->id]);
+        }
+        if (empty($dbuser->email) and !empty($user['email'])) {
+            $sql = "UPDATE users SET email = ? WHERE id = ?";
+            $db->query($sql, [$user['email'], $dbuser->id]);
+        }
+        $session->set('user_id', $dbuser->id);
+        log_message('info', 'Valid credentials for ' . $dbuser->name . ' from ' . $ip . ' using azure.');
+        return url_to('home');
+    }
+    log_message('warning', 'Azure used for login but user \'' . $user['preferred_username'] . '\' does not exist in Open-AudIT.');
+    return site_url('logon');
+}
+
 function github_redirect(object $auth): string
 {
     $session = \Config\Services::session();
