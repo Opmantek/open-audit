b2fe52c232ec31f037e2f516a9355dd917484726
Adam Dyson <adam.dyson@firstwave.com>
2026-03-27 11:39:04 +1100

    task: datatable: refactored the refresh feature, to work with the current bootstrap layout generation

--- public/js/datatables.extra.js
+++ public/js/datatables.extra.js
@@ -1,16 +1,8 @@
 (function ($) {
 
-    // Override the default layout, shouldn't change the look but will
-    // allow the refresh button to be injected, when enabled
-    $.extend(true, $.fn.dataTable.defaults, {
-        dom: '<"d-flex justify-content-between"<"dt-left"l><"dt-right d-flex align-items-center gap-2"fR>>tip'
-    });
-
-    const defaults = {
+    var defaults = {
+        text: '<i class="icon-refresh-cw text-oa-primary"></i> Refresh',
         className: 'btn btn-light btn-sm',
-        iconClass: 'icon-refresh-cw text-oa-primary',
-        text: "Refresh",
-        tooltip: 'Refresh table data',
         spinClass: 'dt-refresh-spin',
         autoDisable: true,
         resetPaging: true,
@@ -18,88 +10,84 @@
         autoRefreshInterval: 5000
     };
 
-    $.fn.dataTable.ext.feature.push({
-        cFeature: 'R',
-        fnInit: function (settings) {
-            const optsFromInit = settings.oInit.refreshButton;
-            if (! optsFromInit) {
-                return null;
-            }
-            var api = new $.fn.dataTable.Api(settings);
-            var opts = $.extend(
-                {},
-                defaults,
-                settings.oInit.refreshButton || {}
-            );
+    $.fn.dataTable.ext.buttons.refresh = function (table, config) {
 
-            var container = $('<div class="dt-refresh me-2"></div>');
-            var button = $(`
-                <button role="button" class="${opts.className}" title="${opts.tooltip}">
-                    <i class="${opts.iconClass}"></i><span>${opts.text}</span>
-                </button>
-            `);
+        var options = $.extend({}, defaults, config);
+        var pollTimer = null;
 
-            var icon = button.find('i');
-            var pollTimer = null;
-
-            function startPolling() {
-                if (!pollTimer) {
-                    pollTimer = setInterval(() => {
-                        if (api.data().count() === 0) {
-                            icon.addClass(opts.spinClass);
-                            if (opts.autoDisable) {
-                                button.prop('disabled', true);
-                            }
-                            api.ajax.reload(null, opts.resetPaging);
-                        } else {
-                            stopPolling();
-                        }
-                    }, opts.autoRefreshInterval);
-                }
-            }
-
-            function stopPolling() {
-                if (pollTimer) {
-                    clearInterval(pollTimer);
-                    pollTimer = null;
-                    icon.removeClass(opts.spinClass);
-                    if (opts.autoDisable) {
-                        button.prop('disabled', false);
-                    }
-                }
-            }
-
-            button.on('click', function () {
-                if (opts.autoDisable) {
-                    button.prop('disabled', true);
-                }
-
-                icon.addClass(opts.spinClass);
-                api.ajax.reload(null, opts.resetPaging);
-            });
-
-            api.on('processing.dt', function (e, settings, processing) {
-                if (!processing) {
-                    icon.removeClass(opts.spinClass);
-                    if (opts.autoDisable) {
-                        button.prop('disabled', false);
-                    }
-
-                    if (opts.autoRefreshIfEmpty && api.data().count() === 0) {
-                        startPolling();
+        function startPolling(node) {
+            if (!pollTimer) {
+                pollTimer = setInterval(() => {
+                    if (table.data().count() === 0) {
+                        triggerReload(node);
                     } else {
-                        stopPolling();
+                        stopPolling(node);
                     }
-                }
-            });
-
-            if (opts.autoRefreshIfEmpty && api.data().count() === 0) {
-                startPolling();
+                }, options.autoRefreshInterval);
             }
-
-            container.append(button);
-
-            return container[0];
         }
-    });
+
+        function stopPolling(node) {
+            if (pollTimer) {
+                clearInterval(pollTimer);
+                pollTimer = null;
+                stopSpin(node);
+            }
+        }
+
+        function startSpin(node) {
+            var icon = $(node).find('i');
+            icon.addClass(options.spinClass);
+            if (options.autoDisable) $(node).prop('disabled', true);
+        }
+
+        function stopSpin(node) {
+            var icon = $(node).find('i');
+            icon.removeClass(options.spinClass);
+            if (options.autoDisable) $(node).prop('disabled', false);
+        }
+
+        function triggerReload(node) {
+            startSpin(node);
+
+            table.ajax.reload(() => {
+                stopSpin(node);
+
+                if (options.autoRefreshIfEmpty && table.data().count() === 0) {
+                    startPolling(node);
+                } else {
+                    stopPolling(node);
+                }
+            }, options.resetPaging);
+        }
+
+        table.on('processing.dt', function (e, s, processing) {
+            var node = table.button('.buttons-refresh').node();
+
+            if (!node) return;
+
+            if (processing) {
+                startSpin(node);
+            } else {
+                stopSpin(node);
+            }
+        });
+
+        table.on('init.dt', function () {
+            var node = table.button('.buttons-refresh').node();
+
+            if (options.autoRefreshIfEmpty && table.data().count() === 0) {
+                startPolling(node);
+            }
+        });
+
+        return {
+            text: options.text,
+            className: options.className + ' buttons-refresh',
+            action: function (e, dt, node) {
+                triggerReload(node);
+            }
+        };
+    };
+
 })(jQuery);
