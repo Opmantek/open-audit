24ebc8d4f8d17d6ee0da63c258ad7b5acf08753f
Adam Dyson <adam.dyson@firstwave.com>
2026-04-23 16:20:31 +1000

    task: network: added a helper class and tests to deal with inconsistencies in Nmap data

---
+++ app/Libraries/Network/Nmap/NmapHostHelper.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Libraries\Network\Nmap;
+
+final class NmapHostHelper
+{
+    /**
+     * Retrieve all addresses or filter by type.
+     *
+     * This function will return all addresses or filter them based on the `addrtype`
+     * (e.g., 'ipv4', 'ipv6', 'mac'). It normalizes the addresses if a single
+     * address is provided as an associative array.
+     *
+     * @param array $host The host array containing address information.
+     * @param string|null $type The optional address type to filter by (e.g., 'ipv4', 'ipv6', 'mac').
+     * @return array A list of addresses (empty array if none found).
+     */
+    public static function getAddresses(array $host, ?string $type = null): array
+    {
+        if (empty($host['address']) || ! is_array($host['address'])) {
+            return [];
+        }
+
+        $addresses = array_is_list($host['address']) ? $host['address'] : [$host['address']];
+
+        if ($type !== null) {
+            return array_values(array_filter($addresses, function(array $address) use ($type) {
+                return isset($address['addrtype']) && $address['addrtype'] === $type;
+            }));
+        }
+
+        return $addresses;
+    }
+
+    /**
+     * Retrieve the first IPv4 address from the host.
+     *
+     * @param array $host The host array containing address information.
+     * @return array The first IPv4 address or an empty array if not found.
+     */
+    public static function getIpv4Address(array $host): array
+    {
+        return self::getAddresses($host, 'ipv4')[0] ?? [];
+    }
+
+    /**
+     * Retrieve the first IPv6 address from the host.
+     *
+     * @param array $host The host array containing address information.
+     * @return array The first IPv6 address or an empty array if not found.
+     */
+    public static function getIpv6Address(array $host): array
+    {
+        return self::getAddresses($host, 'ipv6')[0] ?? [];
+    }
+
+    /**
+     * Retrieve the first MAC address from the host.
+     *
+     * @param array $host The host array containing address information.
+     * @return array The first MAC address or an empty array if not found.
+     */
+    public static function getMacAddress(array $host): array
+    {
+        return self::getAddresses($host, 'mac')[0] ?? [];
+    }
+}
---
+++ tests/Unit/App/Libraries/Network/Nmap/NmapHostHelperTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenAuditTest\Unit\App\Libraries\Network\Nmap;
+
+use App\Libraries\Network\Nmap\NmapHostHelper;
+use PHPUnit\Framework\TestCase;
+
+final class NmapHostHelperTest extends TestCase
+{
+    public function testGetAddressesAllTypes()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $addresses = NmapHostHelper::getAddresses($host);
+
+        $this->assertCount(3, $addresses);
+        $this->assertEquals('192.168.1.1', $addresses[0]['addr']);
+        $this->assertEquals('fe80::1', $addresses[1]['addr']);
+        $this->assertEquals('00:14:22:01:23:45', $addresses[2]['addr']);
+    }
+
+    public function testGetAddressesByIpv4()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $ipv4Addresses = NmapHostHelper::getAddresses($host, 'ipv4');
+
+        $this->assertCount(1, $ipv4Addresses);
+        $this->assertEquals('192.168.1.1', $ipv4Addresses[0]['addr']);
+    }
+
+    public function testGetAddressesByIpv6()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $ipv6Addresses = NmapHostHelper::getAddresses($host, 'ipv6');
+
+        $this->assertCount(1, $ipv6Addresses);
+        $this->assertEquals('fe80::1', $ipv6Addresses[0]['addr']);
+    }
+
+    public function testGetAddressesByMac()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $macAddresses = NmapHostHelper::getAddresses($host, 'mac');
+
+        $this->assertCount(1, $macAddresses);
+        $this->assertEquals('00:14:22:01:23:45', $macAddresses[0]['addr']);
+    }
+
+    public function testGetIpv4Address()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6']
+            ]
+        ];
+
+        $ipv4 = NmapHostHelper::getIpv4Address($host);
+
+        $this->assertEquals('192.168.1.1', $ipv4['addr']);
+    }
+
+    public function testGetIpv4AddressNoIpv4()
+    {
+        $host = [
+            'address' => [
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $ipv4 = NmapHostHelper::getIpv4Address($host);
+
+        $this->assertEmpty($ipv4); // Should return an empty array
+    }
+
+    public function testGetIpv6Address()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6']
+            ]
+        ];
+
+        $ipv6 = NmapHostHelper::getIpv6Address($host);
+
+        $this->assertEquals('fe80::1', $ipv6['addr']);
+    }
+
+    public function testGetMacAddress()
+    {
+        $host = [
+            'address' => [
+                ['addr' => '192.168.1.1', 'addrtype' => 'ipv4'],
+                ['addr' => 'fe80::1', 'addrtype' => 'ipv6'],
+                ['addr' => '00:14:22:01:23:45', 'addrtype' => 'mac']
+            ]
+        ];
+
+        $mac = NmapHostHelper::getMacAddress($host);
+
+        $this->assertEquals('00:14:22:01:23:45', $mac['addr']);
+    }
+
+    public function testNoAddresses()
+    {
+        $host = [];
+
+        $addresses = NmapHostHelper::getAddresses($host);
+        $ipv4 = NmapHostHelper::getIpv4Address($host);
+        $ipv6 = NmapHostHelper::getIpv6Address($host);
+        $mac = NmapHostHelper::getMacAddress($host);
+
+        $this->assertEmpty($addresses);
+        $this->assertEmpty($ipv4);
+        $this->assertEmpty($ipv6);
+        $this->assertEmpty($mac);
+    }
+}
