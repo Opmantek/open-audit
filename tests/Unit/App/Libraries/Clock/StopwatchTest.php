9fad37b12357154e87e03b72dc8701b292595d45
Adam Dyson <adam.dyson@firstwave.com>
2026-04-23 13:09:44 +1000

    task: misc: added stopwatch class and tests for capturing elapsed time

---
+++ tests/Unit/App/Libraries/Clock/StopwatchTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenAuditTest\Unit\App\Libraries\Clock;
+
+use App\Libraries\Clock\Stopwatch;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+use RuntimeException;
+
+final class StopwatchTest extends TestCase
+{
+    private Stopwatch $stopwatch;
+
+    protected function setUp(): void
+    {
+        $this->stopwatch = new Stopwatch();
+    }
+
+    /**
+     * @dataProvider elapsedTimeProvider
+     */
+    public function testElapsedTime(int $startTime, int $endTime, string $expected)
+    {
+        $this->setProperty($this->stopwatch, 'startTime', $startTime);
+        $this->setProperty($this->stopwatch, 'endTime', $endTime);
+
+        $this->assertSame($expected, $this->stopwatch->getElapsedTime());
+    }
+
+    public function elapsedTimeProvider(): array
+    {
+        return [
+            'testElapsedTimeInSeconds' => [
+                'startTime' => 0,
+                'endTime'   => 3,
+                'expected'  => '3 seconds'
+            ],
+            'testElapsedTimeInMinutesAndSeconds' => [
+                'startTime' => 0,
+                'endTime'   => 65,
+                'expected'  => '1 minute, 5 seconds'
+            ],
+            'testElapsedTimeWithMultipleUnits' => [
+                'startTime' => 0,
+                'endTime'   => 3665,
+                'expected'  => '1 hour, 1 minute, 5 seconds'
+            ],
+            'testElapsedTimeWithDaysHoursMinutesSeconds' => [
+                'startTime' => 0,
+                'endTime'   => 90062,
+                'expected'  => '1 day, 1 hour, 1 minute, 2 seconds'
+            ],
+            'testZeroElapsedTime' => [
+                'startTime' => 0,
+                'endTime'   => 0,
+                'expected'  => '0 seconds'
+            ],
+        ];
+    }
+
+    public function testCannotStartWhenRunning()
+    {
+        $this->stopwatch->start();
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Stopwatch is already running.');
+
+        $this->stopwatch->start();
+    }
+
+    public function testCannotStopWhenNotRunning()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Stopwatch is not running.');
+        $this->stopwatch->stop();
+    }
+
+    private function setProperty($object, $propertyName, $value)
+    {
+        $reflection = new ReflectionClass($object);
+        $property = $reflection->getProperty($propertyName);
+        $property->setValue($object, $value);
+    }
+}
