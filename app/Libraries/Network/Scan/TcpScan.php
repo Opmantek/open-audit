f258082fd6b165ca10ebe504944b457aa80d3a1f
Adam Dyson <adam.dyson@firstwave.com>
2026-04-29 12:55:34 +1000

    task: network: updated TcpScan to use SubnetHelper

--- app/Libraries/Network/Scan/TcpScan.php
+++ app/Libraries/Network/Scan/TcpScan.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Libraries\Network\Scan;
 
-use App\Libraries\Network\Helper\IpAddressHelper;
+use App\Libraries\Network\Helper\SubnetHelper;
 
 final class TcpScan extends AbstractAsyncSocketScan
 {
@@ -14,7 +14,7 @@ final class TcpScan extends AbstractAsyncSocketScan
     protected function initialize(): array
     {
         $targets = iterator_to_array(
-            IpAddressHelper::generateIpV4($this->options->targets),
+            SubnetHelper::expand($this->options->targets),
             false
         );
 
