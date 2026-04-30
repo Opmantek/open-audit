75d17342d947ba314680a3582eebd08cdad1c162
Adam Dyson <adam.dyson@firstwave.com>
2026-04-30 14:55:39 +1000

    task: network: updated isPingAvailable

--- app/Libraries/Network/Scan/PingScan.php
+++ app/Libraries/Network/Scan/PingScan.php
@@ -169,7 +169,7 @@ final class PingScan extends AbstractAsyncSocketScan
      */
     private function isPingAvailable(): bool
     {
-        $process = new Process(['ping', '-V']);
+        $process = new Process(['ping', '127.0.0.1']);
         $process->run();
 
         return $process->isSuccessful();
