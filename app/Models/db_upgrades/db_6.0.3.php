02c11b4db9a0d6572907445cddf8cc289aec3fff
Mark Unwin <mark.unwin@gmail.com>
2026-05-07 10:52:23 +1000

    task: database: Set versions in upgrade.

--- app/Models/db_upgrades/db_6.0.3.php
+++ app/Models/db_upgrades/db_6.0.3.php
@@ -276,6 +276,17 @@ if (config('Openaudit')->uuid === '79ef9ada-f4fb-11f0-a02e-000c294b745f') {
     log_message('info', (string)$db->getLastQuery());
 }
 
+// set our versions
+$sql = "UPDATE `configuration` SET `value` = '20260218' WHERE `name` = 'internal_version'";
+$db->query($sql);
+$output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
+log_message('info', (string)$db->getLastQuery());
+
+$sql = "UPDATE `configuration` SET `value` = '6.0.3' WHERE `name` = 'display_version'";
+$db->query($sql);
+$output .= str_replace("\n", " ", (string)$db->getLastQuery()) . "\n\n";
+log_message('info', (string)$db->getLastQuery());
+
 $output .= "Upgrade database to 6.0.3 completed.\n\n";
 config('Openaudit')->internal_version = 20260218;
 config('Openaudit')->display_version = '6.0.3';
